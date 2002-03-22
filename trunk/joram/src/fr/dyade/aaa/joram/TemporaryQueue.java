a2e7507409d906345d01cd958927ec5633a13d2d
joram <joram@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2002-03-22 10:24:06 +0000

    missing throw JMSException added in delete() method

--- trunk/joram/src/fr/dyade/aaa/joram/TemporaryQueue.java
+++ trunk/joram/src/fr/dyade/aaa/joram/TemporaryQueue.java
@@ -75,13 +75,9 @@ public class TemporaryQueue extends Queue implements javax.jms.TemporaryQueue
       sess = (Session) cnx.sessions.get(i);
       for (int j = 0; j < sess.consumers.size(); j++) {
         cons = (MessageConsumer) sess.consumers.get(j);
-        if (agentId.equals(cons.destName)) {
-          JMSException jE = new JMSException("Subscribers still exist"
-                                             + " for this temp. queue.");
-
-          if (JoramTracing.dbgClient.isLoggable(BasicLevel.WARN))
-            JoramTracing.dbgClient.log(BasicLevel.WARN, jE);
-        }
+        if (agentId.equals(cons.destName))
+          throw new JMSException("Consumers still exist for this temp."
+                                 + " queue.");
       }
     }
     // Sending the request to the server:
