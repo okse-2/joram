530946ae0253975d7581dc5f66c673ffea168ffa
fmaistre <fmaistre@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2003-03-20 09:14:54 +0000

    no message

--- trunk/tests/src/org/objectweb/jtests/providers/admin/JoramAdmin.java
+++ trunk/tests/src/org/objectweb/jtests/providers/admin/JoramAdmin.java
@@ -3,133 +3,159 @@ package org.objectweb.jtests.providers.admin;
 import org.objectweb.jtests.jms.admin.Admin;
 
 import javax.naming.*;
-import fr.dyade.aaa.joram.*;
-import javax.jms.Queue;
-import javax.jms.Topic;
+import fr.dyade.aaa.joram.admin.AdminItf;
+import fr.dyade.aaa.joram.admin.AdminImpl;
+import javax.jms.*;
 import java.util.*;
 import java.io.*;
 
-public class JoramAdmin implements Admin {
-  
-    private String name = "JORAM";
-    InitialContext ictx = null;
-    fr.dyade.aaa.joram.admin.Admin admin;
+public class JoramAdmin implements Admin
+{
+  private String name = "JORAM";
+  InitialContext ictx = null;
+  AdminItf admin;
 
-    public JoramAdmin() {
-	try {
-	    Properties props = new Properties();
-	    props.setProperty("java.naming.factory.initial", "fr.dyade.aaa.jndi.NamingContextFactory");
-	    props.setProperty("java.naming.factory.host", "localhost");
-	    props.setProperty("java.naming.provider.url", "http://localhost:16400");
-	    ictx = new InitialContext (props);
-	    admin = new fr.dyade.aaa.joram.admin.Admin("localhost", 16010, 
-						       "root", "root", 100);  
-	    try {
-		admin.getUser("anonymous");
-	    } catch (fr.dyade.aaa.joram.admin.AdminException e) {
-		admin.createUser("anonymous", "anonymous");
-	    }
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public JoramAdmin()
+  {
+    try {
+      Properties props = new Properties();
+      props.setProperty("java.naming.factory.initial",
+                        "fr.dyade.aaa.jndi2.client.NamingContextFactory");
+      props.setProperty("java.naming.factory.host", "localhost");
+      props.setProperty("java.naming.provider.url", "http://localhost:16400");
+
+      ictx = new InitialContext (props);
+      admin = new AdminImpl();
+      admin.connect("root", "root", 100);  
+
+      admin.createUser("anonymous", "anonymous", 0);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 
-    public String getName() {
-	return name;
-    }
+  public String getName()
+  {
+    return name;
+  }
 
-    public InitialContext createInitialContext() throws NamingException {
-	return ictx;
-    }
+  public InitialContext createInitialContext() throws NamingException
+  {
+    return ictx;
+  }
   
-    public void createConnectionFactory(String name) {
-	try {
-	    fr.dyade.aaa.joram.ConnectionFactory cf = admin.createConnectionFactory();
-	    ictx.rebind(name, cf);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void createConnectionFactory(String name)
+  {
+    try {
+      ConnectionFactory cf = admin.createConnectionFactory();
+      ictx.rebind(name, cf);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
   
-    public void createQueueConnectionFactory(String name) {
-	try {
-	    fr.dyade.aaa.joram.QueueConnectionFactory qcf = admin.createQueueConnectionFactory();
-	    ictx.rebind(name, qcf);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void createQueueConnectionFactory(String name)
+  {
+    try {
+      QueueConnectionFactory cf = admin.createQueueConnectionFactory();
+      ictx.rebind(name, cf);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
   
-    public void createTopicConnectionFactory(String name) {
-	try {
-	    fr.dyade.aaa.joram.TopicConnectionFactory tcf = admin.createTopicConnectionFactory();
-	    ictx.rebind(name, tcf);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void createTopicConnectionFactory(String name)
+  {
+    try {
+      TopicConnectionFactory cf = admin.createTopicConnectionFactory();
+      ictx.rebind(name, cf);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
  
-    public void createQueue(String name) {
-	try {
-	    fr.dyade.aaa.joram.Queue queue = admin.createQueue(name);
-	    admin.setFreeWriting(name);
-	    admin.setFreeReading(name);
-	    ictx.rebind(name, queue);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void createQueue(String name)
+  {
+    try {
+      Queue queue = admin.createQueue(0);
+      ictx.rebind(name, queue);
+      admin.setFreeWriting(queue);
+      admin.setFreeReading(queue);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
   
-    public void createTopic(String name) {
-	try {
-	    fr.dyade.aaa.joram.Topic topic = admin.createTopic(name);
-	    admin.setFreeWriting(name);
-	    admin.setFreeReading(name);
-	    ictx.rebind(name, topic);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void createTopic(String name)
+  {
+    try {
+      Topic topic = admin.createTopic(0);
+      ictx.rebind(name, topic);
+      admin.setFreeWriting(topic);
+      admin.setFreeReading(topic);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-    public void deleteQueue(String name) {
-	try {
-	    admin.deleteDestination(name);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void deleteQueue(String name)
+  {
+    try {
+      Queue queue = (Queue) ictx.lookup(name);
+      admin.deleteDestination(queue);
+      ictx.unbind(name);
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void deleteTopic(String name)
+  {
+    try {
+      Topic topic = (Topic) ictx.lookup(name);
+      admin.deleteDestination(topic);
+      ictx.unbind(name);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
  
-    public void deleteTopic(String name) {
-	try {
-	    admin.deleteDestination(name);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void deleteConnectionFactory(String name)
+  {
+    try {
+      ictx.unbind(name);
     }
-
-    public void deleteConnectionFactory(String name) {
-	try {
-	    ictx.unbind(name);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+    catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public void deleteTopicConnectionFactory(String name) {
-	try {
-	    ictx.unbind(name);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+  public void deleteTopicConnectionFactory(String name)
+  {
+    try {
+      ictx.unbind(name);
     }
-
-    public void deleteQueueConnectionFactory(String name) {
-	try {
-	    ictx.unbind(name);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+    catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
+  public void deleteQueueConnectionFactory(String name)
+  {
+    try {
+      ictx.unbind(name);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
