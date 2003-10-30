41edb26f36c248ababc6a19da34d58fa5a86e0f6
fmaistre <fmaistre@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2003-10-30 16:45:19 +0000

    no message

--- joram/samples/bin/windows/admin.bat
+++ joram/samples/bin/windows/admin.bat
@@ -5,6 +5,7 @@ echo == Launching the graphical administration tool ==
 call setHome
 
 set JORAM_LIBS=%JORAM_HOME%\lib
+set CONFIG_ENV=%JORAM_HOME%\samples\config
 
 set CLASSPATH=%JORAM_LIBS%\joramgui.jar
 set CLASSPATH=%CLASSPATH%;%JORAM_LIBS%\joram.jar
@@ -13,5 +14,6 @@ set CLASSPATH=%CLASSPATH%;%JORAM_LIBS%\jms.jar
 set CLASSPATH=%CLASSPATH%;%JORAM_LIBS%\jndi.jar
 set CLASSPATH=%CLASSPATH%;%JORAM_LIBS%\log4j.jar
 set CLASSPATH=%CLASSPATH%;%JORAM_LIBS%\ow_monolog.jar
+set CLASSPATH=%CLASSPATH%;%CONFIG_ENV%
 
 %JAVA_HOME%\bin\java org.objectweb.joram.client.tools.admin.AdminTool
