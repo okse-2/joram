73a120b94d069b52963d4a0f6290de7258167f42
fmaistre <fmaistre@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2003-11-20 14:34:43 +0000

    no message

--- joram/samples/bin/windows/setHome.bat
+++ joram/samples/bin/windows/setHome.bat
@@ -3,4 +3,4 @@
 :: Set JORAM_HOME to point to your Joram directory
 :: (containing the samples and lib directories)
 
-set JORAM_HOME=D:\BOULOT\BAC_A_SABLE\CVS_objectweb\JORAM\joramCurrent
+set JORAM_HOME=C:\joram
