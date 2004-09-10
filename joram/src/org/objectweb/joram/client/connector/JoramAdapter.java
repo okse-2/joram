3e5ac79919a501d168d5bfcb909290641d1f9928
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2004-09-10 11:07:13 +0000

    setObjectName to the bean before register.

--- joram/src/org/objectweb/joram/client/connector/JoramAdapter.java
+++ joram/src/org/objectweb/joram/client/connector/JoramAdapter.java
@@ -893,6 +893,7 @@ public class JoramAdapter implements javax.resource.spi.ResourceAdapter,
                                          + "name=JORAMlocalServer,"
                                          + "J2EEServer="
                                          + serverName);
+          ((LocalServer) bean).setObjectName(jmsResourceON.toString());
           mbs.registerMBean(bean, jmsResourceON);
         }
       }
