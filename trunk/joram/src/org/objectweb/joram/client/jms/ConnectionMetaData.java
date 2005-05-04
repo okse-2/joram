ce25e47d1a0f92d96bf8aecaa20a176dc2b7f0ec
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2005-05-04 12:57:58 +0000

    Updates providerVersion to 4.2.3

--- trunk/joram/src/org/objectweb/joram/client/jms/ConnectionMetaData.java
+++ trunk/joram/src/org/objectweb/joram/client/jms/ConnectionMetaData.java
@@ -39,7 +39,7 @@ public class ConnectionMetaData implements javax.jms.ConnectionMetaData
   public final static String jmsVersion = "1.1";
   public final static int providerMajorVersion = 4;
   public final static int providerMinorVersion = 2;
-  public final static String providerVersion = "4.2.2";
+  public final static String providerVersion = "4.2.3";
   public final static Vector jmsxProperties = new Vector();
 
   static
