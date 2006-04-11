08880d1f60282da5e876936deb22ccbe6a8bacdd
nobody <nobody@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2006-04-11 08:29:08 +0000

    This commit was manufactured by cvs2svn to create tag 'JORAM_4_3_16'.

---
+++ tags/JORAM_4_3_16/joram/src/com/scalagent/kjoram/QueueConnection.java
@@ -0,0 +1,100 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2001 - ScalAgent Distributed Technologies
+ * Copyright (C) 1996 - Dyade
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): Frederic Maistre (INRIA)
+ * Contributor(s): Nicolas Tachker (ScalAgent)
+ */
+package com.scalagent.kjoram;
+
+import com.scalagent.kjoram.excepts.JMSException;
+import com.scalagent.kjoram.excepts.IllegalStateException;
+
+
+public class QueueConnection extends Connection
+{
+  /**
+   * Creates a <code>QueueConnection</code> instance.
+   *
+   * @param factoryParameters  The factory parameters.
+   * @param connectionImpl  The actual connection to wrap.
+   *
+   * @exception JMSSecurityException  If the user identification is incorrect.
+   * @exception IllegalStateException  If the server is not listening.
+   */
+  public QueueConnection(FactoryParameters factoryParameters,
+                         ConnectionItf connectionImpl) throws JMSException
+  {
+    super(factoryParameters, connectionImpl);
+  }
+
+
+  /**
+   * API method.
+   * 
+   * @exception IllegalStateException  If the connection is closed.
+   * @exception InvalidSelectorException  If the selector syntax is wrong.
+   * @exception InvalidDestinationException  If the target destination does
+   *              not exist.
+   * @exception JMSException  If the method fails for any other reason.
+   */
+  public ConnectionConsumer
+         createConnectionConsumer(Queue queue, String selector,
+                                  ServerSessionPool sessionPool,
+                                  int maxMessages) throws JMSException
+  {
+    if (closed)
+      throw new IllegalStateException("Forbidden call on a closed"
+                                      + " connection.");
+
+    return new ConnectionConsumer(this, (Queue) queue, selector,
+                                  sessionPool, maxMessages);
+  }
+
+  /**
+   * API method.
+   * 
+   * @exception IllegalStateException  If the connection is closed.
+   * @exception JMSException  In case of an invalid acknowledge mode.
+   */
+  public QueueSession
+         createQueueSession(boolean transacted, int acknowledgeMode)
+         throws JMSException
+  {
+    if (closed)
+      throw new IllegalStateException("Forbidden call on a closed"
+                                      + " connection.");
+
+    return new QueueSession(this, transacted, acknowledgeMode);
+  }
+
+  /** 
+   * API method.
+   *
+   * @exception IllegalStateException  Systematically.
+   */
+  public ConnectionConsumer
+         createDurableConnectionConsumer(Topic topic, String name,
+                                         String selector,
+                                         ServerSessionPool sessPool,
+                                         int maxMessages) throws JMSException
+  {
+    throw new IllegalStateException("Forbidden call on a QueueConnection.");
+  }
+}
