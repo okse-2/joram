4919dfef42407c3245347755e6554e2598859bca
feliot <feliot@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2006-06-07 08:53:30 +0000

    fx header

--- joram/src/org/objectweb/joram/client/jms/MultiSessionConsumer.java
+++ joram/src/org/objectweb/joram/client/jms/MultiSessionConsumer.java
@@ -1,8 +1,24 @@
 /*
- * Created on 24 mai 2006
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2001 - 2006 ScalAgent Distributed Technologies
+ * Copyright (C) 1996 - 2000 Dyade
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
  */
 package org.objectweb.joram.client.jms;
 
@@ -27,10 +43,6 @@ import fr.dyade.aaa.util.Queue;
  * because the session pool can hang if there is no more
  * available ServerSession.
  * 
- * @author feliot
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class MultiSessionConsumer extends MessageConsumerListener
   implements javax.jms.ConnectionConsumer{
