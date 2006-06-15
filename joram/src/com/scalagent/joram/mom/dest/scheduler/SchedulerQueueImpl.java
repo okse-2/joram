2c1f66e5a5bc1cceee504dc473020ed25e90724f
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2006-06-15 10:36:47 +0000

    Changes the creation and initialization process.

--- joram/src/com/scalagent/joram/mom/dest/scheduler/SchedulerQueueImpl.java
+++ joram/src/com/scalagent/joram/mom/dest/scheduler/SchedulerQueueImpl.java
@@ -1,7 +1,30 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2005 - 2006 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
 package com.scalagent.joram.mom.dest.scheduler;
 
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.Properties;
 
 import javax.jms.JMSException;
 import javax.jms.MessageFormatException;
@@ -10,8 +33,6 @@ import org.objectweb.joram.mom.dest.QueueImpl;
 import org.objectweb.joram.mom.notifications.ClientMessages;
 import org.objectweb.joram.shared.excepts.MessageValueException;
 import org.objectweb.joram.shared.messages.Message;
-import org.objectweb.util.monolog.api.BasicLevel;
-import org.objectweb.util.monolog.api.Logger;
 
 import com.scalagent.scheduler.AddConditionListener;
 import com.scalagent.scheduler.Condition;
@@ -25,38 +46,49 @@ import fr.dyade.aaa.agent.Debug;
 import fr.dyade.aaa.agent.Notification;
 import fr.dyade.aaa.agent.UnknownNotificationException;
 
+import org.objectweb.util.monolog.api.BasicLevel;
+import org.objectweb.util.monolog.api.Logger;
+
 public class SchedulerQueueImpl extends QueueImpl {
-  public static Logger logger = Debug
-      .getLogger("com.scalagent.joram.scheduler.SchedulerQueueImpl");
+  public static Logger logger = Debug.getLogger("com.scalagent.joram.scheduler.SchedulerQueueImpl");
 
   public static final String SCHEDULE_DATE = "scheduleDate";
-
-  public SchedulerQueueImpl(AgentId destId, AgentId adminId) {
-    super(destId, adminId);
+    
+  /**
+   * Constructs a <code>SchedulerQueueImpl</code> instance.
+   *
+   * @param destId   Identifier of the agent hosting the queue.
+   * @param adminId  Identifier of the administrator of the queue.
+   * @param prop     The initial set of properties.
+   */
+  public SchedulerQueueImpl(AgentId destId, AgentId adminId, Properties prop) {
+    super(destId, adminId, prop);
     if (logger.isLoggable(BasicLevel.DEBUG))
-      logger.log(BasicLevel.DEBUG, "SchedulerQueueImpl.<init>(" + destId + ','
-          + adminId + ')');
+      logger.log(BasicLevel.DEBUG, 
+                 "SchedulerQueueImpl.<init>(" + destId + ',' + adminId + ')');
   }
 
   protected void doProcess(ClientMessages not) {
     if (logger.isLoggable(BasicLevel.DEBUG))
-      logger.log(BasicLevel.DEBUG, "SchedulerQueueImpl.doProcess(" + not + ')');
+      logger.log(BasicLevel.DEBUG, 
+                 "SchedulerQueueImpl.doProcess(" + not + ')');
     super.doProcess(not);
     Message msg;
-    for (Enumeration msgs = not.getMessages().elements(); msgs
-        .hasMoreElements();) {
+    for (Enumeration msgs = not.getMessages().elements();
+         msgs.hasMoreElements();) {
       msg = (Message) msgs.nextElement();
-      long scheduleDate = getScheduleDate(msg);
-      if (scheduleDate < 0) return;
-      //DF: to improve
-      // two notifs are necessary to subscribe
-      Channel.sendTo(Scheduler.getDefault(), new AddConditionListener(msg
-          .getIdentifier()));
-      Channel.sendTo(Scheduler.getDefault(), new ScheduleEvent(msg
-          .getIdentifier(), new Date(scheduleDate)));
+        long scheduleDate = getScheduleDate(msg);
+        if (scheduleDate < 0) return;
+        //DF: to improve
+        // two notifs are necessary to subscribe
+        Channel.sendTo(Scheduler.getDefault(),
+                       new AddConditionListener(msg.getIdentifier()));
+        Channel.sendTo(Scheduler.getDefault(),
+                       new ScheduleEvent(msg.getIdentifier(),
+                                         new Date(scheduleDate)));
     }
   }
-  
+
   private static long getScheduleDate(Message msg) {
     Object scheduleDateValue = msg.getObjectProperty(SCHEDULE_DATE);
     if (scheduleDateValue == null) return -1;
@@ -64,8 +96,7 @@ public class SchedulerQueueImpl extends QueueImpl {
       return ((Long)scheduleDateValue).longValue();
     } catch (Exception exc) {
       if (logger.isLoggable(BasicLevel.WARN))
-        logger.log(BasicLevel.WARN, 
-          "Scheduled message error", exc);
+        logger.log(BasicLevel.WARN, "Scheduled message error", exc);
       return -1;
     }
   }
@@ -98,8 +129,8 @@ public class SchedulerQueueImpl extends QueueImpl {
 
   protected boolean checkDelivery(Message msg) {
     if (logger.isLoggable(BasicLevel.DEBUG))
-      logger.log(BasicLevel.DEBUG, "SchedulerQueueImpl.checkDelivery(" + msg
-          + ')');
+      logger.log(BasicLevel.DEBUG,
+                 "SchedulerQueueImpl.checkDelivery(" + msg + ')');
     long scheduleDate = getScheduleDate(msg);
     if (scheduleDate < 0) {
       return true;
@@ -109,4 +140,3 @@ public class SchedulerQueueImpl extends QueueImpl {
     }
   }
 }
-
