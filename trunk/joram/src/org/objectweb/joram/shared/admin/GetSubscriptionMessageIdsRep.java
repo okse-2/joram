224cfd60facfd7c8937a52122f824a09ea154f8b
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2006-12-05 08:55:18 +0000

    minor changes.

--- trunk/joram/src/org/objectweb/joram/shared/admin/GetSubscriptionMessageIdsRep.java
+++ trunk/joram/src/org/objectweb/joram/shared/admin/GetSubscriptionMessageIdsRep.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2003 - ScalAgent Distributed Technologies
+ * Copyright (C) 2003 - 2006 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -9,7 +9,7 @@
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY oGetSubscriptionMessageIdsr FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
@@ -17,14 +17,12 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): ScalAgent DT
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s):
  */
 package org.objectweb.joram.shared.admin;
 
-public class GetSubscriptionMessageIdsRep
-    extends AdminReply {
-
+public class GetSubscriptionMessageIdsRep extends AdminReply {
   private String[] ids;
 
   public GetSubscriptionMessageIdsRep(
