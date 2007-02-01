cc9f2042517fa18b1b5f77fd42dcd24a68170b99
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2007-02-01 09:12:21 +0000

    Bug fix in handling of byte array as object.

--- trunk/joram/src/org/objectweb/joram/client/jms/StreamMessage.java
+++ trunk/joram/src/org/objectweb/joram/client/jms/StreamMessage.java
@@ -864,7 +864,12 @@ public final class StreamMessage extends Message implements javax.jms.StreamMess
       } else if (type == NULL) {
         return null;
       } else if (type == BYTES) {
-        byte[] b = new byte[inputStream.available()];
+        int available = inputStream.readInt();
+
+        if (available == -1) return null;
+        if (available == 0) return new byte[0];
+
+        byte[] b = new byte[available];
         inputStream.read(b);
         return b;
       } else
