56e4fae156d3377dff26da3ba7ca7b4254cdeb09
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2007-06-22 13:32:13 +0000

    Implements generic toString method in root class.

--- trunk/joram/src/org/objectweb/joram/shared/client/AbstractJmsRequest.java
+++ trunk/joram/src/org/objectweb/joram/shared/client/AbstractJmsRequest.java
@@ -86,12 +86,6 @@ public abstract class AbstractJmsRequest extends AbstractJmsMessage {
     this.target = target;
   }
 
-  public final String toString() {
-    StringBuffer strbuf = new StringBuffer();
-    toString(strbuf);
-    return strbuf.toString();
-  }
-
   public void toString(StringBuffer strbuf) {
     strbuf.append('(').append(super.toString());
     strbuf.append(",requestId=").append(requestId);
