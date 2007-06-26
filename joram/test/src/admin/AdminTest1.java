52b8d54616a99535d71d5ace5601a1e927c44384
badolle <badolle@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2007-06-26 12:22:56 +0000

    update test

--- joram/test/src/admin/AdminTest1.java
+++ joram/test/src/admin/AdminTest1.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s):(ScalAgent D.T.)
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s): Badolle Fabien (ScalAgent D.T.)
  */
 package admin;
