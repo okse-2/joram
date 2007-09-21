113f25a315d6efb67a5f9d1e93ff0cb706287e32
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2007-09-21 08:02:41 +0000

    Adds getMaxLogFileSize et setMaxLogFileSize operation.

--- trunk/joram/src/fr/dyade/aaa/util/NTransactionMBean.java
+++ trunk/joram/src/fr/dyade/aaa/util/NTransactionMBean.java
@@ -51,18 +51,25 @@ public interface NTransactionMBean extends TransactionMBean {
   public int getLogMemorySize();
 
   /**
-   * Returns the size of disk log in Mb, by default 16Mb.
+   * Returns the maximum size of disk log in Mb, by default 16Mb.
+   *
+   * @return The maximum size of disk log in Mb.
+   */
+  int getMaxLogFileSize();
+
+  /**
+   * Sets the maximum size of disk log in Mb.
+   *
+   * @param size The maximum size of disk log in Mb.
+   */
+  void setMaxLogFileSize(int size);
+
+  /**
+   * Returns the size of disk log in Mb.
    *
    * @return The size of disk log in Mb.
    */
   int getLogFileSize();
-
-  /**
-   * Sets the size of disk log in Mb.
-   *
-   * @param size The size of disk log in Mb.
-   */
-  void setLogFileSize(int size);
 
   /**
    * Returns the pool size for <code>operation</code> objects, by default 1000.
@@ -120,6 +127,13 @@ public interface NTransactionMBean extends TransactionMBean {
    * @param timeout The maximum time between two garbages (seconds).
    */
   void setGarbageDelay(int timeout);
+
+  /**
+   * Returns the status of the garbage thread.
+   *
+   * @return The status of the garbage thread.
+   */
+  boolean isGarbageRunning();
 
   /**
    *  Sets asynchronous garbage.
