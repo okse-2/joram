81ececd22cf4340cac0e31c66cd14940315deac6
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2007-11-27 16:06:13 +0000

    Remove methods now accessible directly through the ClientSubscriptionMBean

--- joram/src/org/objectweb/joram/mom/proxies/ProxyImplMBean.java
+++ joram/src/org/objectweb/joram/mom/proxies/ProxyImplMBean.java
@@ -22,7 +22,6 @@
  */
 package org.objectweb.joram.mom.proxies;
 
-import javax.management.openmbean.CompositeDataSupport;
 
 public interface ProxyImplMBean {
   /**
@@ -47,76 +46,6 @@ public interface ProxyImplMBean {
    * @return The list of subscriptions for this user.
    */
   String[] getSubscriptionNames();
-
-  /**
-   * Returns the number of pending messages for an identified subscription.
-   * The subscription must be identified by its unique 'symbolic' name.
-   *
-   * @param subName  The subscription unique name.
-   * @return The number of pending message for the subscription.
-   */
-  int getSubscriptionMessageCount(String subName);
-
-  /**
-   * Returns the unique identifier of the topic related to this subscription.
-   *
-   * @param subName  The subscription unique name.
-   * @return the unique identifier of the topic related to this subscription.
-   */
-  String getSubscriptionTopicId(String subName);
-
-  /**
-   * Returns the list of message's identifiers for a subscription.
-   * The subscription must be identified by its unique 'symbolic' name.
-   *
-   * @param subName  The subscription unique name.
-   * @return the list of message's identifiers for the subscription.
-   */
-  String[] getSubscriptionMessageIds(String subName);
-
-  /**
-   * Returns the description of a particular pending message in a subscription.
-   * The subscription is identified  by its unique name, the message is pointed
-   * out through its unique identifier.
-   * The description includes the type and priority of the message.
-   *
-   * @param subName  The subscription unique name.
-   * @param msgId    The unique message's identifier.
-   * @return the description of the message.
-   */
-  CompositeDataSupport getSubscriptionMessage(String subName,
-                                              String msgId) throws Exception;
-
-  /**
-   * Deletes a particular pending message in a subscription.
-   * The subscription is identified  by its unique name, the message is pointed
-   * out through its unique identifier.
-   *
-   * @param subName  The subscription unique name.
-   * @param msgId    The unique message's identifier.
-   */
-  void deleteSubscriptionMessage(String subName, String msgId);
-
-  /**
-   * Returns the maximum number of message for identified subscription.
-   * The subscription is identified  by its unique name, if the limit is unset
-   * the method returns -1.
-   *
-   * @param subName  The subscription unique name.
-   * @return the maximum number of message for subscription if set;
-   *	     -1 otherwise.
-   */
-  int getNbMaxMsg(String subName);
-
-  /**
-   * Sets the maximum number of message for identified subscription.
-   * The subscription is identified  by its unique name.
-   *
-   * @param subName  The subscription unique name.
-   * @param nbMaxMsg the maximum number of message for subscription (-1 set
-   *		     no limit).
-   */
-  void setNbMaxMsg(String subName, int nbMaxMsg);
 
   /**
    * Returns a string representation of this user's proxy.
