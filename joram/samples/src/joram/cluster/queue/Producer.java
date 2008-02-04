d5a094f31f6d3934709e369778ac34a1e6d06687
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-02-04 10:50:03 +0000

    New scenarii for clustered destination samples.

--- joram/samples/src/joram/cluster/queue/Producer.java
+++ joram/samples/src/joram/cluster/queue/Producer.java
@@ -1,6 +1,7 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2004 - France Telecom R&D
+ * Copyright (C) 2004 - 2008 ScalAgent Distributed Technologies
+ * Copyright (C) 2004 France Telecom R&D
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): Nicolas Tachker (ScalAgent)
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s):
  */
 package cluster.queue;
@@ -32,37 +33,51 @@ public class Producer {
   static Context ictx = null; 
 
   public static void main(String[] args) throws Exception {
+    ConnectionFactory cf = null;
+    Queue dest = null;
 
-    int i = new Integer(args[0]).intValue();
-    System.setProperty("location", ""+i);
+    if (args.length != 2)
+     throw new Exception("Bad number of argument");
+
+    ictx = new InitialContext();
+    try {
+      if (args[0].equals("-")) {
+        // Choose a connection factory and the associated topic depending of
+        // the location property.
+        cf = (ConnectionFactory) ictx.lookup("clusterCF");
+        dest = (Queue) ictx.lookup("clusterQueue");
+      } else {
+        cf = (ConnectionFactory) ictx.lookup("cf" + args[0]);
+        dest = (Queue) ictx.lookup("queue" + args[0]);
+        System.setProperty("location", "server" + args[0]);
+      }
+    } finally {
+      ictx.close();
+    }
 
     int nbMsg = new Integer(args[1]).intValue();
-
     int sleep = Integer.getInteger("sleep", 500).intValue();
     int nbMsgSleep = Integer.getInteger("nbMsgSleep", 10).intValue();
 
-    System.out.println();
-    System.out.println("Produces " + nbMsg + " messages on the cluster queue...");
+    System.out.println("Produces " + nbMsg + " messages on the clusterQueue.");
     System.out.println("sleep = " + sleep + ", nbMsgSleep=" + nbMsgSleep);
 
-    ictx = new InitialContext();
-    Destination clusterQueue = (Destination) ictx.lookup("clusterQueue");
-    System.out.println("clusterQueue = " + clusterQueue);
-    QueueConnectionFactory cf = (QueueConnectionFactory) ictx.lookup("qcf"+i);
-    ictx.close();
+    Connection cnx = cf.createConnection("anonymous", "anonymous");
+    Session session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    MessageProducer prod = session.createProducer(dest);
 
-    QueueConnection cnx = cf.createQueueConnection("user"+i,"user"+i);
-    QueueSession sess = cnx.createQueueSession(false,Session.AUTO_ACKNOWLEDGE);
-    MessageProducer producer = sess.createProducer(null);
+    String location = System.getProperty("location");
+    if (location != null)
+      System.out.println("Sends messages on queue on " + location);
 
-    TextMessage msg = sess.createTextMessage();
+    TextMessage msg = session.createTextMessage();
     long time = System.currentTimeMillis();
     System.out.println("FirstTime = " + time);
 
     int j;
     for (j = 0; j < nbMsg; j++) {
-      msg.setText("location " + i +" : Test number " + j);
-      producer.send(clusterQueue, msg);
+      msg.setText("location " + location +" : Test number " + j);
+      prod.send(msg);
       if (sleep > 0 && (j % nbMsgSleep) == 0) {
         Thread.sleep(sleep);
       }
