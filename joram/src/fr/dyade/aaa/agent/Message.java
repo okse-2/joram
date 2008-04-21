093fb6150293a217ba81e7a912189031bb99a159
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-04-21 11:47:22 +0000

    Minor changes.

--- joram/src/fr/dyade/aaa/agent/Message.java
+++ joram/src/fr/dyade/aaa/agent/Message.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies 
+ * Copyright (C) 2001 - 2008 ScalAgent Distributed Technologies 
  * Copyright (C) 1996 - 2000 BULL
  * Copyright (C) 1996 - 2000 INRIA
  *
