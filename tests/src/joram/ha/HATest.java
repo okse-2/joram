43027ac2ae3128d3b00b7d900f5d01e45dde3e40
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-05-19 20:00:47 +0000

    Clean implementation.

--- tests/src/joram/ha/HATest.java
+++ tests/src/joram/ha/HATest.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2005 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2005 - 2008 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,8 +17,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): (ScalAgent D.T.)
- * Contributor(s): Badolle Fabien (ScalAgent D.T.)
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
  */
 package joram.ha;
 
@@ -32,13 +32,19 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.objectweb.joram.client.jms.admin.User;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
+import org.objectweb.joram.client.jms.ha.tcp.HATcpConnectionFactory;
+import org.objectweb.joram.client.jms.Queue;
+
 import joram.framework.TestCase;
 
 import org.objectweb.joram.client.jms.admin.AdminModule;
 
 public class HATest extends TestCase {
+  public static final int MESSAGE_NUMBER = 50;
 
-  public static final int MESSAGE_NUMBER = 20;
+  public static final int  pause = 100;
 
   public HATest() {
     super();
@@ -48,118 +54,66 @@ public class HATest extends TestCase {
     Process p0 = null;
     Process p1 = null;
     Process p2 = null;
+
     try {
-      File r0 = new File("r0");
-      r0.mkdir();
-
-      File r1 = new File("r1");
-      r1.mkdir();
-
-      File r2 = new File("r2");
-      r2.mkdir();
-
-      try {
-        Thread.sleep(2000);
-      } catch (InterruptedException exc) {
-      }
-
       System.out.println("Start the replica 0");
-      p0 = startAgentServer((short) 0, r0, new String[] {
-          "-DnbClusterExpected=2",
-          "-DTransaction=fr.dyade.aaa.util.NullTransaction",
-          "-D" + fr.dyade.aaa.util.Debug.DEBUG_DIR_PROPERTY + "=.." },
-          new String[] { "0" });
+      p0 = startHAServer((short) 0, null, "0");
 
-      try {
-        Thread.sleep(2000);
-      } catch (InterruptedException exc) {
-      }
+      Thread.sleep(2000);
 
       System.out.println("Start the replica 1");
-      p1 = startAgentServer((short) 0, r1, new String[] {
-          "-DnbClusterExpected=2",
-          "-DTransaction=fr.dyade.aaa.util.NullTransaction",
-          /* "-Dcom.sun.management.jmxremote", */
-          /* "-DMXServer=com.scalagent.jmx.JMXServer", */
-          "-D" + fr.dyade.aaa.util.Debug.DEBUG_DIR_PROPERTY + "=.." },
-          new String[] { "1" });
+      p1 = startHAServer((short) 0, null, "1");
 
       System.out.println("Start the replica 2");
-      p2 = startAgentServer((short) 0, r2, new String[] {
-          "-DnbClusterExpected=2",
-          "-DTransaction=fr.dyade.aaa.util.NullTransaction",
-          "-D" + fr.dyade.aaa.util.Debug.DEBUG_DIR_PROPERTY + "=.." },
-          new String[] { "2" });
+      p2 = startHAServer((short) 0, null, "2");
 
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException exc) {
-      }
+      Thread.sleep(1000);
 
       AdminModule.connect("localhost", 2560, "root", "root", 60);
 
-      org.objectweb.joram.client.jms.admin.User user = org.objectweb.joram.client.jms.admin.User
-          .create("anonymous", "anonymous", 0);
+      User user = User.create("anonymous", "anonymous", 0);
 
-      ConnectionFactory cf = org.objectweb.joram.client.jms.ha.tcp.HATcpConnectionFactory
-          .create("hajoram://localhost:2560,localhost:2561,localhost:2562");
-      ((org.objectweb.joram.client.jms.ConnectionFactory) cf).getParameters().cnxPendingTimer = 500;
-      ((org.objectweb.joram.client.jms.ConnectionFactory) cf).getParameters().connectingTimer = 30;
+      ConnectionFactory cf = HATcpConnectionFactory.create("hajoram://localhost:2560,localhost:2561,localhost:2562");
+      ((HATcpConnectionFactory) cf).getParameters().cnxPendingTimer = 500;
+      ((HATcpConnectionFactory) cf).getParameters().connectingTimer = 30;
 
-      org.objectweb.joram.client.jms.Queue queue = org.objectweb.joram.client.jms.Queue
-          .create(0);
+      Queue queue = Queue.create(0, "queue");
       queue.setFreeReading();
       queue.setFreeWriting();
 
       AdminModule.disconnect();
 
       Connection cnx = cf.createConnection("anonymous", "anonymous");
-      Session session = cnx.createSession(false,
-          javax.jms.Session.AUTO_ACKNOWLEDGE);
+      Session session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
       MessageProducer producer = session.createProducer(queue);
       MessageConsumer consumer = session.createConsumer(queue);
       cnx.start();
 
-      new Killer(p0, 0, 500 * (MESSAGE_NUMBER / 2)).start();
+      new Killer(p0, 0, pause * (MESSAGE_NUMBER / 2)).start();
 
       for (int i = 0; i < MESSAGE_NUMBER; i++) {
         TextMessage msg = session.createTextMessage();
         msg.setText("Test number1 " + i);
         producer.send(msg);
 
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException exc) {
-        }
+        Thread.sleep(pause);
       }
 
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException exc) {
-      }
+      Thread.sleep(1000);
 
-      new Killer(p1, 1, 500 * (MESSAGE_NUMBER / 2)).start();
+      new Killer(p1, 1, pause * (MESSAGE_NUMBER / 2)).start();
 
       System.out.println("Restart the replica 0");
-      p0 = startAgentServer((short) 0, r0, new String[] {
-          "-DnbClusterExpected=2",
-          "-DTransaction=fr.dyade.aaa.util.NullTransaction",
-          "-D" + fr.dyade.aaa.util.Debug.DEBUG_DIR_PROPERTY + "=.." },
-          new String[] { "0" });
+      p0 = startHAServer((short) 0, null, "0");
 
       for (int i = 0; i < MESSAGE_NUMBER; i++) {
         TextMessage msg = (TextMessage) consumer.receive();
-        assertTrue(msg.getText().startsWith("Test number1"));
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException exc) {
-        }
+        assertTrue(msg.getText().equals("Test number1 " +i));
+
+        Thread.sleep(pause);
       }
 
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException exc) {
-      }
+      Thread.sleep(1000);
 
       for (int i = 0; i < MESSAGE_NUMBER; i++) {
         TextMessage msg = session.createTextMessage();
@@ -167,92 +121,46 @@ public class HATest extends TestCase {
         producer.send(msg);
       }
 
-      new Killer(p2, 2, 500 * (MESSAGE_NUMBER / 2)).start();
+      new Killer(p2, 2, pause * (MESSAGE_NUMBER / 2)).start();
 
       System.out.println("Start the replica 1");
-      p1 = startAgentServer((short) 0, r1, new String[] {
-          "-DnbClusterExpected=2",
-          "-DTransaction=fr.dyade.aaa.util.NullTransaction",
-          /* "-Dcom.sun.management.jmxremote", */
-          /* "-DMXServer=com.scalagent.jmx.JMXServer", */
-          "-D" + fr.dyade.aaa.util.Debug.DEBUG_DIR_PROPERTY + "=.." },
-          new String[] { "1" });
+      p1 = startHAServer((short) 0, null, "1");
 
       for (int i = 0; i < MESSAGE_NUMBER; i++) {
         TextMessage msg = (TextMessage) consumer.receive();
-        assertTrue(msg.getText().startsWith("Test number2"));
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException exc) {
-        }
+        assertTrue(msg.getText().equals("Test number2 " +i));
+
+        Thread.sleep(pause);
       }
     } catch (Exception exc) {
       exc.printStackTrace();
       error(exc);
     } finally {
-      p0.destroy();
-      p1.destroy();
-      p2.destroy();
+      if (p0 != null)
+        p0.destroy();
+      if (p1 != null)
+        p1.destroy();
+      if (p2 != null)
+        p2.destroy();
       endTest();
     }
   }
 
-  public static Process startAgentServer(short sid, File dir, String[] jvmarg,
-      String[] servarg) throws Exception {
-    return startAgentServer(sid, dir, jvmarg, servarg,
-        "fr.dyade.aaa.agent.AgentServer");
-  }
+  public static Process startHAServer(short sid,
+                                      File dir,
+                                      String rid) throws Exception {
+    String[] jvmargs = new String[] {
+      "-DnbClusterExpected=2",
+      "-DTransaction=fr.dyade.aaa.util.NullTransaction"};
 
-  public static Process startAgentServer(short sid, File dir, String[] jvmarg,
-      String[] servarg, String serverClassName) throws Exception {
-    String javapath = new File(
-        new File(System.getProperty("java.home"), "bin"), "java").getPath();
-    String classpath = System.getProperty("java.class.path");
+    String[] args = new String[] { rid };
 
-    Vector argv = new Vector();
-    argv.addElement(javapath);
-    argv.addElement("-classpath");
-    argv.addElement(classpath);
-    if (jvmarg != null) {
-      for (int i = 0; i < jvmarg.length; i++)
-        argv.addElement(jvmarg[i]);
-    }
-    argv.addElement(serverClassName);
-    argv.addElement(Short.toString(sid));
-    if (dir == null)
-      argv.addElement("s" + sid);
-    else
-      argv.addElement(dir.toString());
-    if (servarg != null) {
-      for (int i = 0; i < servarg.length; i++)
-        argv.addElement(servarg[i]);
-    }
+    Process p =  getAdmin().execAgentServer(sid, dir,
+                                            jvmargs,
+                                            "fr.dyade.aaa.agent.AgentServer",
+                                            args);
+    getAdmin().closeServerStream(p);
 
-    String[] command = new String[argv.size()];
-    argv.copyInto(command);
-
-    Process p;
-    if (dir == null) {
-      p = Runtime.getRuntime().exec(command);
-    } else {
-      p = Runtime.getRuntime().exec(command);
-      //p = Runtime.getRuntime().exec(command, null, dir);
-    }
-
-    // Close all streams of subprocess in order to avoid deadlock due
-    // to limited buffer size.
-    try {
-      p.getInputStream().close();
-    } catch (Exception exc) {
-    }
-    try {
-      p.getOutputStream().close();
-    } catch (Exception exc) {
-    }
-    try {
-      p.getErrorStream().close();
-    } catch (Exception exc) {
-    }
     return p;
   }
 
