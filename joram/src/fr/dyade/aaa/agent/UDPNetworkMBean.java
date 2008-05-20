f7e49fcb7334f2d6ad7da53be5ec0244b037cfb8
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-05-20 15:07:06 +0000

    Add throws declaration.

--- joram/src/fr/dyade/aaa/agent/UDPNetworkMBean.java
+++ joram/src/fr/dyade/aaa/agent/UDPNetworkMBean.java
@@ -1,13 +1,15 @@
 package fr.dyade.aaa.agent;
 
+import java.net.SocketException;
+
 public interface UDPNetworkMBean extends NetworkMBean {
 
-  public int getSocketReceiveBufferSize();
+  public int getSocketReceiveBufferSize() throws SocketException;
 
-  public int getSocketSendBufferSize();
+  public int getSocketSendBufferSize() throws SocketException;
 
-  public void setSocketReceiveBufferSize(int size);
+  public void setSocketReceiveBufferSize(int size) throws SocketException;
 
-  public void setSocketSendBufferSize(int size);
+  public void setSocketSendBufferSize(int size) throws SocketException;
 
 }
