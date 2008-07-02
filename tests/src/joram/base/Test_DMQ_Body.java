b9c993f89979cdf58e901b95f66479e3094633bc
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-07-02 08:29:37 +0000

    Correct test which wasn't testing anything (empty DMQ browser).

--- tests/src/joram/base/Test_DMQ_Body.java
+++ tests/src/joram/base/Test_DMQ_Body.java
@@ -1,5 +1,4 @@
 
-
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
  * Copyright (C)  2007 ScalAgent Distributed Technologies
@@ -39,160 +38,140 @@ import javax.naming.InitialContext;
 
 import joram.framework.TestCase;
 
+import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.DeadMQueue;
+import org.objectweb.joram.client.jms.admin.User;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
 /**
- * Test that a message body is not delete on a Dqueue when restart server 
- *        
- * 
+ * Test that a message body is not deleted on a DMQ after server restart
  */
 public class Test_DMQ_Body extends TestCase {
 
-   
-    public static void main(String[] args) {
-	new Test_DMQ_Body().run();
+  public static void main(String[] args) {
+    new Test_DMQ_Body().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("server start");
+      startAgentServer((short) 0);
+
+      admin();
+      System.out.println("admin config ok");
+
+      Context ictx = new InitialContext();
+      Queue queue = (Queue) ictx.lookup("queue");
+      DeadMQueue dq = (DeadMQueue) ictx.lookup("dq");
+
+      ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+
+      Connection cnx = cf.createConnection();
+
+      // create a connection for the deadqueue
+      Connection cnx_dead = cf.createConnection();
+      Session sessionp = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Session sessionc = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      // create a session for the deadqueue
+      Session sessioncdead = cnx_dead.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      cnx.start();
+      cnx_dead.start();
+      // create a producer and a consumer
+      MessageProducer producer = sessionp.createProducer(queue);
+      MessageConsumer consumer = sessionc.createConsumer(queue);
+      // create a consumer attach to the deadqueue
+
+      // create a message send to the queue by the pruducer 
+      TextMessage msg = sessionp.createTextMessage("test not delete");
+      producer.setTimeToLive(1000L);
+      producer.send(msg);
+
+      // sleep to be safe that the message expired
+      Thread.sleep(1500L);
+      // The receive reaction will send the message in the DMQ
+      assertEquals(null, consumer.receive(100));
+
+      QueueBrowser browserDmq = sessioncdead.createBrowser(dq);
+      Enumeration messages = browserDmq.getEnumeration();
+      assertTrue(messages.hasMoreElements());
+
+      while (messages.hasMoreElements()) {
+        TextMessage msg1 = (TextMessage) messages.nextElement();
+        System.out.println("message expired !");
+        //test messages
+        assertEquals(true, msg1.getBooleanProperty("JMS_JORAM_EXPIRED"));
+        assertEquals(msg.getJMSMessageID(), msg1.getJMSMessageID());
+        assertEquals(msg.getJMSType(), msg1.getJMSType());
+        assertEquals("test not delete", msg1.getText());
+      }
+
+      cnx.close();
+      stopAgentServer((short) 0);
+      Thread.sleep(2000);
+      startAgentServer((short) 0);
+      ictx = new InitialContext();
+      dq = (DeadMQueue) ictx.lookup("dq");
+      cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+
+      cnx_dead = cf.createConnection();
+      sessioncdead = cnx_dead.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      cnx_dead.start();
+
+      browserDmq = sessioncdead.createBrowser(dq);
+      messages = browserDmq.getEnumeration();
+
+      assertTrue(messages.hasMoreElements());
+
+      while (messages.hasMoreElements()) {
+        TextMessage msg1 = (TextMessage) messages.nextElement();
+        assertEquals(true, msg1.getBooleanProperty("JMS_JORAM_EXPIRED"));
+        assertEquals(msg.getJMSMessageID(), msg1.getJMSMessageID());
+        assertEquals(msg.getJMSType(), msg1.getJMSType());
+        assertEquals("test not delete", msg1.getText());
+      }
+
+      cnx_dead.close();
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      stopAgentServer((short) 0);
+      endTest();
     }
-          
-    public void run() {
-	try {
-	    System.out.println("server start");
-	    startAgentServer((short)0);
-	   
-	    
-	    admin();
-	    System.out.println("admin config ok");
-	    
-	    Context  ictx = new InitialContext();
-	    Queue queue = (Queue) ictx.lookup("queue");
-	    DeadMQueue dq =(DeadMQueue) ictx.lookup("dq");
+  }
 
-   	    ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
+  /**
+   * Admin : Create queue and a user anonymous use jndi
+   */
+  public void admin() throws Exception {
+    // conexion 
+    AdminModule.connect("localhost", 2560, "root", "root", 60);
+    // create a Queue   
+    org.objectweb.joram.client.jms.Queue queue = org.objectweb.joram.client.jms.Queue.create(0);
 
-	    Connection cnx = cf.createConnection();
+    // create a deadqueue for receive expired messages
+    DeadMQueue dq = (DeadMQueue) DeadMQueue.create(0);
+    dq.setFreeReading();
+    dq.setFreeWriting();
+    AdminModule.setDefaultDMQ(0, dq);
 
-	    // create a connection for the deadqueue
-	    Connection cnx_dead = cf.createConnection();
-	    Session sessionp = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    Session sessionc = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    // create a session for the deadqueue
-	    Session sessioncdead = cnx_dead.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    cnx.start();
-	   cnx_dead.start();
-	    // create a producer and a consumer
-	    MessageProducer producer = sessionp.createProducer(queue);
-	    MessageConsumer consumer = sessionc.createConsumer(queue);
-	    // create a consumer attach to the deadqueue
-	   
-	    // create a message send to the queue by the pruducer 
-	    TextMessage msg = sessionp.createTextMessage("test not delete");
-	    producer.setTimeToLive(1000L);
-	    producer.send(msg);
-	    
-	    // sleep to be safe that the message expired
-	    Thread.sleep(1000L);
-	    
+    // create a user
+    User.create("anonymous", "anonymous");
+    // set permissions
+    queue.setFreeReading();
+    queue.setFreeWriting();
 
- 
-	    QueueBrowser browserDmq = sessioncdead.createBrowser(dq);
-	    Enumeration messages = browserDmq.getEnumeration();
-	    
-	    while (messages.hasMoreElements()) {
-		TextMessage msg1 = (TextMessage) messages.nextElement();
-		if(msg1 != null){
-		    // System.out.println("message expired !");
-		    //test messages
-		 assertEquals(true, msg1.getBooleanProperty("JMS_JORAM_EXPIRED"));
-		 assertEquals(msg.getJMSMessageID(),msg1.getJMSMessageID());
-		 assertEquals(msg.getJMSType(),msg1.getJMSType());
-		 assertEquals("test not delete",msg1.getText());
-		 
-		}
-	    }
-    
-	    cnx.close();
-	    stopAgentServer((short)0);
-	    Thread.sleep(2000);
-	    startAgentServer((short)0);
-	    ictx = new InitialContext();
-	    dq =(DeadMQueue) ictx.lookup("dq");
-	    cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
-	    
-	    cnx_dead = cf.createConnection();
-	    sessioncdead = cnx_dead.createSession(false,
-							  Session.AUTO_ACKNOWLEDGE);
-	    cnx_dead.start();
+    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 2560);
 
-	    browserDmq = sessioncdead.createBrowser(dq);
-	    messages = browserDmq.getEnumeration();
-	    
-	    while (messages.hasMoreElements()) {
-		TextMessage msg1 = (TextMessage) messages.nextElement();
-		if(msg1 != null){
-		    // System.out.println("message expired !");
-		    //test messages
-		    assertEquals(true, msg1.getBooleanProperty("JMS_JORAM_EXPIRED"));
-		    assertEquals(msg.getJMSMessageID(),msg1.getJMSMessageID());
-		    assertEquals(msg.getJMSType(),msg1.getJMSType());
-		    assertEquals("test not delete",msg1.getText());
-		    
-		}
-	    }
-	    
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.bind("cf", cf);
+    jndiCtx.bind("queue", queue);
+    jndiCtx.bind("dq", dq);
+    jndiCtx.close();
 
-	     
-	  
-	    cnx_dead.close();
-	} catch (Throwable exc) {
-	    exc.printStackTrace();
-	    error(exc);
-	}
-	finally {
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-	    endTest(); 
-	}
-    }
-
-    /**
-     * Admin : Create queue and a user anonymous
-     *   use jndi
-     */
-    public void admin() throws Exception {
-	// conexion 
-	org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
-								 "root", "root", 60);
-	// create a Queue   
-	org.objectweb.joram.client.jms.Queue queue =
-	    (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create(0); 
-
-	// create a deadqueue for receive expired messages
-	DeadMQueue dq = (DeadMQueue) DeadMQueue.create(0);
-	dq.setFreeReading();
-	dq.setFreeWriting();
-       	org.objectweb.joram.client.jms.admin.AdminModule.setDefaultDMQ(0,dq);
-
-        // create a user
-	org.objectweb.joram.client.jms.admin.User user =
-	    org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
-	// set permissions
-	queue.setFreeReading();
-	queue.setFreeWriting();
-
-      	javax.jms.ConnectionFactory cf =
-	    org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
-
-	javax.naming.Context jndiCtx = new javax.naming.InitialContext();
-	jndiCtx.bind("cf", cf);
-	jndiCtx.bind("queue", queue);
-	jndiCtx.bind("dq", dq);
-	jndiCtx.close();
-	   
-	org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
-    }
+    AdminModule.disconnect();
+  }
 }
-
