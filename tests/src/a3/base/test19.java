da23c8558c7d8822fff6d853d8ee4dca02e02c5d
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-08-27 08:53:16 +0000

    Minor changes.

--- tests/src/a3/base/test19.java
+++ tests/src/a3/base/test19.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C)  2001 ScalAgent Distributed Technologies
+ * Copyright (C) 2008 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,10 +17,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s):ScalAgent D.T.
+ * Initial developer(s):ScalAgent Distributed Technologies
  * Contributor(s): 
  */
-
 package a3.base;
 
 import joram.framework.BaseTestCase;
