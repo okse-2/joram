7bef9ce343450510f6b5fef3f1d83a3c95522800
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-08-27 10:58:59 +0000

    Code cleaning.

--- tests/src/joram/noreg/Test20.java
+++ tests/src/joram/noreg/Test20.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2003 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2003 - 2008 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): Freyssinet Andre (ScalAgent D.T.)
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s): Badolle Fabien (ScalAgent D.T.)
  */
 package joram.noreg;
@@ -32,8 +32,6 @@ import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 
-import joram.framework.BaseTestCase;
-
 import org.objectweb.joram.client.jms.Queue;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.DeadMQueue;
@@ -44,27 +42,27 @@ import org.objectweb.joram.shared.MessageErrorConstants;
 import fr.dyade.aaa.agent.AgentServer;
 
 class ExcList20 implements ExceptionListener {
-    String name = null;
-    int nbexc;
+  String name = null;
+  int nbexc;
 
-    ExcList20(String name) {
-	this.name = name;
-	nbexc = 0;
-    }
+  ExcList20(String name) {
+    this.name = name;
+    nbexc = 0;
+  }
 
-    public void onException(JMSException exc) {
-	nbexc += 1;
-	System.err.println(name + ": " + exc.getMessage());
-	//exc.printStackTrace();
-    }
+  public void onException(JMSException exc) {
+    nbexc += 1;
+    System.err.println(name + ": " + exc.getMessage());
+    //exc.printStackTrace();
+  }
 }
 
 class MsgList20B implements MessageListener {
   public void onMessage(Message msg) {
     try {
-      BaseTestCase.assertEquals(1, msg.getIntProperty("JMS_JORAM_ERRORCOUNT"));
-      BaseTestCase.assertEquals(MessageErrorConstants.EXPIRED, msg.getIntProperty("JMS_JORAM_ERRORCODE_1"));
-    } catch (Exception exc) {
+      Test20.assertEquals(1, msg.getIntProperty("JMS_JORAM_ERRORCOUNT"));
+      Test20.assertEquals(MessageErrorConstants.EXPIRED, msg.getIntProperty("JMS_JORAM_ERRORCODE_1"));
+    } catch(Exception exc) {
       exc.printStackTrace();
     }
   }
@@ -75,87 +73,86 @@ class MsgList20B implements MessageListener {
  *
  */
 public class Test20 extends BaseTest{
-    static Connection cnx_a;
-    static Connection cnx_b;
+  static Connection cnx_a;
+  static Connection cnx_b;
 
-    static Session sess_a, sess_b;
+  static Session sess_a, sess_b;
 
-    static ExcList20 exclst_a, exclst_b;
-    static MsgList20B msglst_b;
+  static ExcList20 exclst_a, exclst_b;
+  static MsgList20B msglst_b;
 
-    public static void main (String args[]) throws Exception {
-	new Test20().run();
+  public static void main (String args[]) throws Exception {
+    new Test20().run();
+  }
+  public void run(){
+    try{
+      AgentServer.init((short) 0, "s0", null);
+      AgentServer.start();
+
+      Thread.sleep(1000L);
+      short sid = Integer.getInteger("sid", 0).shortValue();
+
+      AdminModule.connect("localhost", 16010, "root", "root", 60);
+
+      User user = User.create("anonymous", "anonymous");
+      Queue queue = Queue.create(0);
+      DeadMQueue dmq = (DeadMQueue) DeadMQueue.create(0);
+      dmq.setFreeReading();
+      AdminModule.setDefaultDMQ(0, dmq);
+
+      queue.setFreeReading();
+      queue.setFreeWriting();
+
+      ConnectionFactory cf = TcpConnectionFactory.create("localhost", 16010);
+      AdminModule.disconnect();
+
+      cnx_a = cf.createConnection();
+      cnx_a.setExceptionListener(new ExcList20("ClientA"));
+      sess_a = cnx_a.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer cons_a = sess_a.createConsumer(queue);
+      MessageProducer prod_a = sess_a.createProducer(queue);
+      prod_a.setTimeToLive(1000L);
+      cnx_a.start();
+
+      Connection cnx_b = cf.createConnection();
+      cnx_b.setExceptionListener(new ExcList20("ClientB"));
+      sess_b = cnx_b.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer cons_b = sess_b.createConsumer(dmq);
+      cons_b.setMessageListener(new MsgList20B());
+      cnx_b.start();
+
+      Message msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 0);
+      prod_a.send(msg);
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 1);
+      prod_a.send(msg);
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 2);
+      prod_a.send(msg);
+
+      Thread.sleep(2000L);
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 3);
+      prod_a.send(msg);
+
+      msg = cons_a.receive();
+      int index = msg.getIntProperty("Index");
+      //System.out.println("receives msg#" + index);
+
+      Thread.sleep(1000L);
+
+      cnx_a.close();
+      cnx_b.close();
+    } catch(Throwable exc){
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      AgentServer.stop();
+      endTest();
     }
-    public void run(){
-	try{
-	    AgentServer.init((short) 0, "s0", null);
-	    AgentServer.start();
-
-	    Thread.sleep(1000L);
-	    short sid = Integer.getInteger("sid", 0).shortValue();
-
-	    AdminModule.connect("localhost", 16010, "root", "root", 60);
-
-	    User user = User.create("anonymous", "anonymous");
-	    Queue queue = Queue.create(0);
-	    DeadMQueue dmq = (DeadMQueue) DeadMQueue.create(0);
-	    dmq.setFreeReading();
-	    AdminModule.setDefaultDMQ(0, dmq);
-
-	    queue.setFreeReading();
-	    queue.setFreeWriting();
-
-	    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 16010);
-	    AdminModule.disconnect();
-
-	    cnx_a = cf.createConnection();
-	    cnx_a.setExceptionListener(new ExcList20("ClientA"));
-	    sess_a = cnx_a.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	    MessageConsumer cons_a = sess_a.createConsumer(queue);
-	    MessageProducer prod_a = sess_a.createProducer(queue);
-	    prod_a.setTimeToLive(1000L);
-	    cnx_a.start();
-
-	    Connection cnx_b = cf.createConnection();
-	    cnx_b.setExceptionListener(new ExcList20("ClientB"));
-	    sess_b = cnx_b.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	    MessageConsumer cons_b = sess_b.createConsumer(dmq);
-	    cons_b.setMessageListener(new MsgList20B());
-	    cnx_b.start();
-
-	    Message msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 0);
-	    prod_a.send(msg);
-
-	    msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 1);
-	    prod_a.send(msg);
-
-	    msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 2);
-	    prod_a.send(msg);
-
-	    Thread.sleep(2000L);
-
-	    msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 3);
-	    prod_a.send(msg);
-
-	    msg = cons_a.receive();
-	    int index = msg.getIntProperty("Index");
-	    //System.out.println("receives msg#" + index);
-
-	    Thread.sleep(1000L);
-
-	    cnx_a.close();
-	    cnx_b.close();
-
-	}catch(Throwable exc){
-	    exc.printStackTrace();
-	    error(exc);
-	}finally {
-	    AgentServer.stop();
-	    endTest();
-	}
-    }
+  }
 }
--- tests/src/joram/noreg/Test21.java
+++ tests/src/joram/noreg/Test21.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2003 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2003 - 2008 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): Freyssinet Andre (ScalAgent D.T.)
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s): Badolle Fabien (ScalAgent D.T.)
  */
 package joram.noreg;
@@ -65,23 +65,25 @@ class MsgList21B implements MessageListener {
     nbReceived++;
     try {
       int index = msg.getIntProperty("Index");
-      BaseTestCase.assertEquals(msg.getIntProperty("JMS_JORAM_ERRORCOUNT"), 1);
+      BaseTestCase.assertEquals(1, msg.getIntProperty("JMS_JORAM_ERRORCOUNT"));
       if (index == 0) {
-        BaseTestCase.assertEquals(msg.getShortProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.UNDELIVERABLE);
-        System.out.println("msg#" + index + ", Undeliverable message: "
-            + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
+        BaseTestCase.assertEquals(msg.getIntProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.UNDELIVERABLE);
+
+        System.out.println("msg#" + index + ", " +
+                           msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1") + ", " +
+                           msg.getIntProperty("JMSXDeliveryCount"));
       } else if (index == 1) {
-        BaseTestCase.assertEquals(msg.getShortProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.NOT_WRITEABLE);
-        System.out.println("msg#" + index + ", Non writable destination: "
-            + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
+         BaseTestCase.assertEquals(msg.getIntProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.NOT_WRITEABLE);
+
+        System.out.println("msg#" + index + ", " + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
       } else if (index == 2) {
-        BaseTestCase.assertEquals(msg.getShortProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.DELETED_DEST);
-        System.out.println("msg#" + index + ", Destination does not exist: "
-            + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
+        BaseTestCase.assertEquals(msg.getIntProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.DELETED_DEST);
+
+        System.out.println("msg#" + index + ", " + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
       } else if (index == 3) {
-        BaseTestCase.assertEquals(msg.getShortProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.ADMIN_DELETED);
-        System.out.println("msg#" + index + ", An admin has deleted the message: "
-            + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
+        BaseTestCase.assertEquals(msg.getIntProperty("JMS_JORAM_ERRORCODE_1"), MessageErrorConstants.ADMIN_DELETED);
+
+        System.out.println("msg#" + index + ", " + msg.getStringProperty("JMS_JORAM_ERRORCAUSE_1"));
       } else {
         BaseTestCase.assertTrue(false);
       }
@@ -98,117 +100,117 @@ class MsgList21B implements MessageListener {
  *    -send to a deleted queue
  */
 public class Test21 extends BaseTest{
-    static Connection cnx_a;
-    static Connection cnx_b;
+  static Connection cnx_a;
+  static Connection cnx_b;
 
-    static Session sess_a, sess_b;
+  static Session sess_a, sess_b;
 
-    static ExcList21 exclst_a, exclst_b;
-    static MsgList21B msglst_b;
+  static ExcList21 exclst_a, exclst_b;
+  static MsgList21B msglst_b;
 
-    public static void main (String args[]) throws Exception {
-	new Test21().run();
+  public static void main (String args[]) throws Exception {
+    new Test21().run();
+  }
+  public void run(){
+    try{
+      AgentServer.init((short) 0, "s0", null);
+      AgentServer.start();
+
+      Thread.sleep(1000L);
+
+      AdminModule.connect("localhost", 16010, "root", "root", 60);
+
+      User user = User.create("anonymous", "anonymous");
+      Queue queue1 = Queue.create(0);
+      Queue queue2 = Queue.create(0);
+      Queue queue3 = Queue.create(0);
+
+      DeadMQueue dmq = (DeadMQueue) DeadMQueue.create(0);
+      dmq.setFreeReading();
+      AdminModule.setDefaultDMQ(0, dmq);
+
+      queue1.setFreeReading();
+      queue1.setFreeWriting();
+      queue1.setThreshold(2);
+
+      queue3.delete();
+
+      ConnectionFactory cf = TcpConnectionFactory.create("localhost", 16010);
+      AdminModule.disconnect();
+
+      cnx_a = cf.createConnection();
+      cnx_a.setExceptionListener(new ExcList21("ClientA"));
+      sess_a = cnx_a.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+      MessageConsumer cons_a = sess_a.createConsumer(queue1);
+      MessageProducer prod_a = sess_a.createProducer(null);
+      cnx_a.start();
+
+      Connection cnx_b = cf.createConnection();
+      cnx_b.setExceptionListener(new ExcList21("ClientB"));
+      sess_b = cnx_b.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+      MessageConsumer cons_b = sess_b.createConsumer(dmq);
+      msglst_b = new MsgList21B();
+      cons_b.setMessageListener(msglst_b);
+      cnx_b.start();
+
+      Message msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 0);
+      prod_a.send(queue1, msg);
+      sess_a.commit();
+
+      msg = cons_a.receive();
+      int index = msg.getIntProperty("Index");
+      sess_a.rollback();
+      System.out.println("rollback msg#" + index);
+
+      msg = cons_a.receive();
+      index = msg.getIntProperty("Index");
+      sess_a.rollback();
+      System.out.println("rollback msg#" + index);
+
+      Thread.sleep(500L);
+
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 1);
+      prod_a.send(queue2, msg);
+      try{
+        sess_a.commit();
+      }catch(JMSException j){}
+
+      Thread.sleep(500L);
+
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 2);
+      prod_a.send(queue3, msg);
+      try{
+        sess_a.commit();
+      }catch(JMSException jj){}
+
+      Thread.sleep(500);
+
+      msg = sess_a.createMessage();
+      msg.setIntProperty("Index", 3);
+      prod_a.send(queue1, msg);
+      try{
+        sess_a.commit();
+      } catch(JMSException jj) {}
+      AdminModule.connect("localhost", 16010, "root", "root", 60);
+      queue1.clear();
+      AdminModule.disconnect();
+
+      cnx_a.close();
+      cnx_b.close();
+
+      Thread.sleep(1000L);
+      assertEquals(4, msglst_b.nbReceived);
+    } catch(Throwable exc){
+      exc.printStackTrace();
+      error(exc);
+    } finally{
+      AgentServer.stop();
+      endTest();
     }
-    public void run(){
-	try{
-	    AgentServer.init((short) 0, "s0", null);
-	    AgentServer.start();
-
-	    Thread.sleep(1000L);
-
-	    AdminModule.connect("localhost", 16010, "root", "root", 60);
-
-	    User.create("anonymous", "anonymous");
-	    Queue queue1 = Queue.create(0);
-	    Queue queue2 = Queue.create(0);
-	    Queue queue3 = Queue.create(0);
-
-	    DeadMQueue dmq = (DeadMQueue) DeadMQueue.create(0);
-	    dmq.setFreeReading();
-	    AdminModule.setDefaultDMQ(0, dmq);
-
-	    queue1.setFreeReading();
-	    queue1.setFreeWriting();
-	    queue1.setThreshold(2);
-
-	    queue3.delete();
-
-	    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 16010);
-	    AdminModule.disconnect();
-
-	    cnx_a = cf.createConnection();
-	    cnx_a.setExceptionListener(new ExcList21("ClientA"));
-	    sess_a = cnx_a.createSession(true, Session.CLIENT_ACKNOWLEDGE);
-	    MessageConsumer cons_a = sess_a.createConsumer(queue1);
-	    MessageProducer prod_a = sess_a.createProducer(null);
-	    cnx_a.start();
-
-	    Connection cnx_b = cf.createConnection();
-	    cnx_b.setExceptionListener(new ExcList21("ClientB"));
-	    sess_b = cnx_b.createSession(false, Session.CLIENT_ACKNOWLEDGE);
-	    MessageConsumer cons_b = sess_b.createConsumer(dmq);
-	    msglst_b = new MsgList21B();
-	    cons_b.setMessageListener(msglst_b);
-	    cnx_b.start();
-
-	    Message msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 0);
-	    prod_a.send(queue1, msg);
-	    sess_a.commit();
-
-	    msg = cons_a.receive();
-	    int index = msg.getIntProperty("Index");
-	    sess_a.rollback();
-	    System.out.println("rollback msg#" + index);
-
-	    msg = cons_a.receive();
-	    index = msg.getIntProperty("Index");
-	    sess_a.rollback();
-	    System.out.println("rollback msg#" + index);
-
-	    Thread.sleep(500L);
-
-  
-	    msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 1);
-	    prod_a.send(queue2, msg);
-	    try{
-	    sess_a.commit();
-	    }catch(JMSException j){}
-
-	    Thread.sleep(500L);
-	   
-   
-	    msg = sess_a.createMessage();
-	    msg.setIntProperty("Index", 2);
-	    prod_a.send(queue3, msg);
-	    try{
-	    sess_a.commit();
-	    }catch(JMSException jj){}
-
-	    Thread.sleep(500);
-	    
-  	    msg = sess_a.createMessage();
-  	    msg.setIntProperty("Index", 3);
-  	    prod_a.send(queue1, msg);
-  	    try{
-  	      sess_a.commit();
-  	    } catch(JMSException jj) {}
-        AdminModule.connect("localhost", 16010, "root", "root", 60);
-        queue1.clear();
-        AdminModule.disconnect();
-        
-  	    cnx_a.close();
-  	    cnx_b.close();
-  	    
-	    Thread.sleep(1000L);
-	    assertEquals(4, msglst_b.nbReceived);
-	}catch(Throwable exc){
-	    exc.printStackTrace();
-	    error(exc);
-	}finally{
-	    AgentServer.stop();
-	    endTest();
-	}
-    }
+  }
 }
