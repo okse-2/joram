dbcf4dfd40c03c75c0662cda8d31e89bf42b4e21
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-08-27 10:26:30 +0000

    Code cleaning.

--- trunk/tests/src/joram/base/Test_Q_MObj.java
+++ trunk/tests/src/joram/base/Test_Q_MObj.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C)  2007 ScalAgent Distributed Technologies
+ * Copyright (C)  2007 - 2008 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,9 +18,11 @@
  * USA.
  *
  * Initial developer(s):Badolle Fabien (ScalAgent D.T.)
- * Contributor(s): 
+ * Contributor(s): ScalAgent Distributed Technologies
  */
 package joram.base;
+
+import java.io.Serializable;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -35,107 +37,105 @@ import javax.naming.InitialContext;
 
 import joram.framework.TestCase;
 
-
 /**
  * Test :
  *     The Object message received by the consumer is the same that 
  *     the object message sent by the producer 
- *     Use a Queue
+ *     Use String as object and a Queue as destination.
  */
 public class Test_Q_MObj extends TestCase {
+  public static void main(String[] args) {
+    new Test_Q_MObj().run();
+  }
 
-   
-    public static void main(String[] args) {
-	new Test_Q_MObj().run();
+  public void run() {
+    try {
+      System.out.println("server start");
+      startAgentServer((short)0);
+
+      admin();
+      System.out.println("admin config ok");
+
+      Context  ictx = new InitialContext();
+      Queue queue = (Queue) ictx.lookup("queue");
+      ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+
+      Connection cnx = cf.createConnection();
+      Session session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      // create a producer
+      MessageProducer producer = session.createProducer(queue);
+      // start the session
+      cnx.start();
+
+      // create an object and send the message containing the object to the queue 
+      Serializable obj_send = new String("abcde");
+      ObjectMessage msg = session.createObjectMessage();
+      msg.setObject(obj_send);
+      producer.send(msg);
+      
+      producer.close();
+      session.close();
+      cnx.close();
+
+      cnx = cf.createConnection();
+      session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      // create a consumer
+      MessageConsumer consumer = session.createConsumer(queue);
+      // start the session
+      cnx.start();
+
+      // the consumer receive the first message from the queue
+      ObjectMessage msg2 = (ObjectMessage) consumer.receive();
+      // and extract the object
+      String obj_receive = (String) msg2.getObject();
+
+      //test messages
+      assertEquals(msg.getJMSMessageID(), msg2.getJMSMessageID());
+      assertEquals(msg.getJMSType(), msg2.getJMSType());
+      assertEquals(msg.getJMSDestination(), msg2.getJMSDestination());
+      assertEquals(obj_send, obj_receive);
+      
+      cnx.close();
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      stopAgentServer((short)0);
+      endTest(); 
     }
-          
-    public void run() {
-	try {
-	    System.out.println("server start");
-	    startAgentServer((short)0);
-	   
-	   
-	    admin();
-	    System.out.println("admin config ok");
-	    
-	    Context  ictx = new InitialContext();
-	    Queue queue = (Queue) ictx.lookup("queue");
-   	    ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
+  }
 
-	    Connection cnx = cf.createConnection();
-	    
-	    Session sessionp = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    Session sessionc = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    cnx.start();
+  /**
+   * Admin : Create queue and a user anonymous
+   *   use jndi
+   */
+  public void admin() throws Exception {
+    // conexion 
+    org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
+                                                             "root", "root", 60);
+    // create a Queue   
+    org.objectweb.joram.client.jms.Queue queue =
+      (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create("queue"); 
 
-	    // create a producer and a consumer
-	    MessageProducer producer = sessionp.createProducer(queue);
-	    MessageConsumer consumer = sessionc.createConsumer(queue);
-	    
+    // create a user
+    org.objectweb.joram.client.jms.admin.User user =
+      org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
+    // set permissions
+    queue.setFreeReading();
+    queue.setFreeWriting();
 
-	    // create an object and a message containing the object send to the queue by the pruducer 
-	    Obj obj_send=new Obj();
-	    ObjectMessage msg = sessionp.createObjectMessage();
-	    msg.setObject(obj_send);
-	    // msg.setObject(null);
-	     producer.send(msg);
-	   	    
-	    // the consumer receive the message from the queue
-	    Message msg1= consumer.receive();
+    javax.jms.ConnectionFactory cf =
+      org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
 
-	    // and extract the object
-	    ObjectMessage msg2 =(ObjectMessage) msg1;
-	    Obj obj_receive=(Obj)msg2.getObject();
-	    
-	    //test messages
-	    assertEquals(msg.getJMSMessageID(),msg2.getJMSMessageID());
-	    assertEquals(msg.getJMSType(),msg2.getJMSType());
-	    assertEquals(msg.getJMSDestination(),msg2.getJMSDestination());
-	    assertEquals(obj_send.getA(),obj_receive.getA());
-	    // assertTrue(obj_receive == null);
-	    cnx.close();
-	} catch (Throwable exc) {
-	    exc.printStackTrace();
-	    error(exc);
-	} finally {
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-	    endTest(); 
-	}
-    }
-    
-    /**
-     * Admin : Create queue and a user anonymous
-     *   use jndi
-     */
-    public void admin() throws Exception {
-	// conexion 
-	org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
-								 "root", "root", 60);
-	// create a Queue   
-	org.objectweb.joram.client.jms.Queue queue =
-	    (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create("queue"); 
-	
-        // create a user
-	org.objectweb.joram.client.jms.admin.User user =
-	    org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
-	// set permissions
-	queue.setFreeReading();
-	queue.setFreeWriting();
-	
-      	javax.jms.ConnectionFactory cf =
-	    org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
-	
-	javax.naming.Context jndiCtx = new javax.naming.InitialContext();
-	jndiCtx.bind("cf", cf);
-	jndiCtx.bind("queue", queue);
-	jndiCtx.close();
-	
-	org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
-    }
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.bind("cf", cf);
+    jndiCtx.bind("queue", queue);
+    jndiCtx.close();
+
+    org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
+  }
 }
 
 
