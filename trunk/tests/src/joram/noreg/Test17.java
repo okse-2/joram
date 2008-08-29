6ba94b09aaddfe730ad4b6b1be0f62391782b4f7
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-08-29 08:17:03 +0000

    Really test and verify the results.

--- trunk/tests/src/joram/noreg/Test17.java
+++ trunk/tests/src/joram/noreg/Test17.java
@@ -31,6 +31,8 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
+import javax.jms.Queue;
+import javax.jms.Topic;
 
 import org.objectweb.joram.client.jms.Destination;
 import org.objectweb.joram.client.jms.admin.AdminModule;
@@ -38,191 +40,197 @@ import org.objectweb.joram.client.jms.admin.User;
 
 import fr.dyade.aaa.agent.AgentServer;
 
-class ExcList17 implements ExceptionListener {
-    String name = null;
-    int nbexc;
-
-    ExcList17(String name) {
-	this.name = name;
-	nbexc = 0;
-    }
-
-    public void onException(JMSException exc) {
-	nbexc += 1;
-	//     System.err.println(name + ": " + exc.getMessage());
-    }
-}
-
-class MsgList17 implements MessageListener {
-    int nb1, nb2, idx;
-
-    MsgList17() {
-	nb1 = 0;
-	nb2 = 0;
-	idx = 0;
-    }
-
-    public synchronized void onMessage(Message msg) {
-	try {
-	    nb2 += 1;
-	    int index = msg.getIntProperty("index");
-	    if (index != idx) {
-		//         System.out.println("recv#" + idx + '/' + index);
-		nb1 += 1;
-	    }
-	    idx = index +1;
-	} catch (Throwable exc) {
-	    exc.printStackTrace();
-	}
-    }
-}
-
 /**
  *
  */
-public class Test17 extends BaseTest {
-    static Connection cnx1;
-    static ConnectionFactory cf;
-    static Destination dest;
+public class Test17 extends BaseTest implements ExceptionListener, MessageListener {
+  ConnectionFactory cf;
+  Destination dest;
+  Connection cnx1;
+  Session sess1;
+  MessageConsumer cons;
 
-    static ExcList17 exclst;
-    static MsgList17 msglst;
+  int nbExc = 0;
 
-    public static void main (String args[]) throws Exception {
-	new Test17().run();
+  public void onException(JMSException exc) {
+//    System.out.println("onException");
+    nbExc += 1;
+    assertEquals("javax.jms.IllegalStateException", exc.getClass().getName());
+  }
+  
+  int nbMsg = 0;
+  int nbErr = 0;
+  int idx = 0;
+  
+  public void onMessage(Message msg) {
+    try {
+      nbMsg += 1;
+      int index = msg.getIntProperty("index");
+      if (index != idx) {
+//        System.out.println("recv#" + idx + '/' + index);
+        nbErr += 1;
+      }
+      idx = index +1;
+    } catch (Throwable exc) {
+      exc.printStackTrace();
     }
-    public void run(){
-	try{
-	    writeIntoFile("======================== start test ======================");
-	    startServer();
+  }
 
-	    String baseclass = "joram.noreg.ColocatedBaseTest";
-	    baseclass = System.getProperty("BaseClass", baseclass);
-
-	    String destclass =  "org.objectweb.joram.client.jms.Queue";
-	    destclass =  System.getProperty("Destination", destclass);
-
-	    Thread.sleep(500L);
-	    AdminConnect(baseclass);
-
-	    User user = User.create("anonymous", "anonymous", 0);
-	    dest = createDestination(destclass);
-	    dest.setFreeReading();
-	    dest.setFreeWriting();
-
-	    cf =  createConnectionFactory(baseclass);
-	    AdminModule.disconnect();
-
-	    Connection cnx2 = cf.createConnection();
-	    Session sess2 = cnx2.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	    MessageProducer producer = sess2.createProducer(dest);
-	    cnx2.start();
-
-	    exclst = new ExcList17("Receiver");
-	    cnx1 = cf.createConnection();
-	    cnx1.setExceptionListener(exclst);
-	    Session sess1 = cnx1.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	    MessageConsumer cons = sess1.createConsumer(dest);
-	    msglst = new MsgList17();
-	    cons.setMessageListener(msglst);
-	    cnx1.start();
-
-	    Sender17 sender = new Sender17(cnx2, sess2, producer);
-	    new Thread(sender).start();
-
-	    for (int i=0; i<50; i++) {
-		Thread.sleep(500L);
-
-		// System.out.println("closing");
-		cnx1.close();
-		Thread.sleep(100L);
-
-		//System.out.println("connecting#");
-		cnx1 = cf.createConnection();
-		cnx1.setExceptionListener(exclst);
-		sess1 = cnx1.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		cons = sess1.createConsumer(dest);
-		cons.setMessageListener(msglst);
-		cnx1.start();
-		// System.out.println("connected#");
-	    }
-
-	    sender.waitEnd();
-	    // System.out.println("Test OK: " + exclst.nbexc + ", " + msglst.nb1 + ", " + msglst.nb2);
-	   
-	    writeIntoFile("| nb_Exception :"+exclst.nbexc+
-			  "\n| nb_NoOrder_receive :"+msglst.nb1+
-			  "\n| nb_receive :"+msglst.nb2);
-
-	    cnx1.close();
-
-	}catch(Throwable exc){
-	    exc.printStackTrace();
-	    error(exc);
-	}finally{
-	    AgentServer.stop();
-	    endTest();
-	}
+  public static void main (String args[]) throws Exception {
+    new Test17().run();
+  }
+  
+  public synchronized void connect() {
+    try {
+//      System.out.println("connect");
+      cnx1 = cf.createConnection();
+      cnx1.setExceptionListener(this);
+      sess1 = cnx1.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      if (dest instanceof org.objectweb.joram.client.jms.Queue)
+        cons = sess1.createConsumer(dest);
+      else
+        cons = sess1.createDurableSubscriber((Topic) dest, "sub");
+      cons.setMessageListener(this);
+      cnx1.start();
+    } catch (JMSException exc2) {
+      exc2.printStackTrace();
+      error(exc2);
+      AgentServer.stop();
+      endTest();
     }
+  }
+  
+  public synchronized void close() {    
+    try {
+//      System.out.println("close");
+      if (cnx1 != null)
+        cnx1.close();
+      cnx1 = null;
+      cons = null;
+    } catch (JMSException exc2) {
+      exc2.printStackTrace();
+      error(exc2);
+      AgentServer.stop();
+      endTest();
+    }
+  }
+
+  public void run(){
+    try{
+      timeout = 300000L;
+      
+      startServer();
+
+      String baseclass = "joram.noreg.ColocatedBaseTest";
+      baseclass = System.getProperty("BaseClass", baseclass);
+
+      String destclass =  "org.objectweb.joram.client.jms.Queue";
+      destclass =  System.getProperty("Destination", destclass);
+
+      Thread.sleep(500L);
+      AdminConnect(baseclass);
+
+      User user = User.create("anonymous", "anonymous", 0);
+      dest = createDestination(destclass);
+      dest.setFreeReading();
+      dest.setFreeWriting();
+
+      cf =  createConnectionFactory(baseclass);
+      AdminModule.disconnect();
+
+      connect();
+      
+      Connection cnx2 = cf.createConnection();
+      Session sess2 = cnx2.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageProducer producer = sess2.createProducer(dest);
+      cnx2.start();
+
+      Sender17 sender = new Sender17(cnx2, sess2, producer);
+      new Thread(sender).start();
+
+      
+      int nbClose = 0;
+
+      while (nbMsg < 5000) {
+        
+        while (nbMsg < ((nbClose +1) *250))
+          Thread.sleep(500L);
+
+        nbClose += 1;
+        close();
+        Thread.sleep(500L);
+
+        connect();
+      }
+      
+      Thread.sleep(1000);
+      
+      assertEquals("nbExc=" + nbExc, nbClose, nbExc);
+      assertEquals("nbErr=" + nbErr, 0, nbErr);
+      assertEquals("nbMsg=" + nbMsg, 5000, nbMsg);
+      
+//      System.out.println("Test OK: " + nbExc + ", " + nbErr + ", " + nbMsg);
+
+      close();
+    } catch (Throwable exc){
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      AgentServer.stop();
+      endTest();
+    }
+  }
 }
 
 class Sender17 implements Runnable {
-    Connection cnx;
-    Session session;
-    MessageProducer producer;
-    boolean ended;
-    Object lock;
+  Connection cnx;
+  Session session;
+  MessageProducer producer;
+  boolean ended;
+  Object lock;
 
-    public Sender17(Connection cnx,
-		    Session session,
-		    MessageProducer producer) throws Exception {
-	this.cnx = cnx;
-	this.session = session;
-	this.producer = producer;
+  public Sender17(Connection cnx,
+                  Session session,
+                  MessageProducer producer) throws Exception {
+    this.cnx = cnx;
+    this.session = session;
+    this.producer = producer;
 
-	ended = false;
-	lock = new Object();
+    ended = false;
+    lock = new Object();
+  }
+
+  public void waitEnd() {
+    synchronized (lock) {
+      while (! ended) {
+        try {
+          lock.wait();
+        } catch (InterruptedException exc) {
+        }
+      }
     }
+  }
 
-    public void waitEnd() {
-	synchronized (lock) {
-	    while (! ended) {
-		try {
-		    lock.wait();
-		} catch (InterruptedException exc) {
-		}
-	    }
-	}
+  public void run() {
+    try {
+      for (int i=0; i<5000; i++) {
+        Message msg = session.createMessage();
+        msg.setIntProperty("index", i);
+        producer.send(msg);
+      }
+      System.out.println("end");
+    } catch (Exception exc) {
+      exc.printStackTrace();
+    } finally {
+      try {
+        cnx.close();
+      } catch (Exception exc) {
+        exc.printStackTrace();
+      }
+      synchronized (lock) {
+        ended = true;
+        lock.notify();
+      }
     }
-
-    public void run() {
-	try {
-	    for (int i=0; i<50000; i++) {
-		Message msg = session.createMessage();
-		msg.setIntProperty("index", i);
-		producer.send(msg);
-		//         if ((i%100) == 99) {
-		//           System.out.println("closing");
-		//           if (Test17.cnx1 != null) {
-		//             Test17.cnx1.close();
-		//             System.out.println("closed");
-		// //             Test17.cnx1 = null;
-		//           }
-		//         }
-	    }
-	    System.out.println("end");
-	} catch (Exception exc) {
-	    exc.printStackTrace();
-	} finally {
-	    try {
-		cnx.close();
-	    } catch (Exception exc) {
-		exc.printStackTrace();
-	    }
-	    synchronized (lock) {
-		ended = true;
-		lock.notify();
-	    }
-	}
-    }
+  }
 }
