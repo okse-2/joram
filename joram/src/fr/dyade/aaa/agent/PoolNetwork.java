b8dfdf04d481401e673b3409beb790c3f32dd06e
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-10-14 15:30:58 +0000

    Adds CSSI copyright.

--- joram/src/fr/dyade/aaa/agent/PoolNetwork.java
+++ joram/src/fr/dyade/aaa/agent/PoolNetwork.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2004 - 2008 ScalAgent Distributed Technologies
+ * Copyright (C) 2008 CSSI
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
