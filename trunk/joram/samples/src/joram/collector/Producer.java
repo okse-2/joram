29c377a78581db5137add8bf6b24abca7f6da6ab
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2008-10-20 15:01:06 +0000

    first collector sample.

---
+++ trunk/joram/samples/src/joram/collector/Producer.java
@@ -0,0 +1,62 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2008 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s):
+ */
+package collector;
+
+import javax.jms.*;
+import javax.naming.*;
+
+/**
+ * update properties on the collector queue and on the collector topic.
+ */
+public class Producer {
+  static Context ictx = null;
+
+  public static void main(String[] args) throws Exception {
+    System.out.println();
+    System.out.println("update properties on the collector queue and on the collector topic...");
+
+    ictx = new InitialContext();
+    Queue queue = (Queue) ictx.lookup("queue");
+//    Topic topic = (Topic) ictx.lookup("topic");
+    ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
+    ictx.close();
+
+    Connection cnx = cf.createConnection();
+    Session sess = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    MessageProducer producer = sess.createProducer(null);
+
+    Message msg = sess.createMessage();
+    msg.setStringProperty("collector.expirationMessage", "0");
+    msg.setStringProperty("collector.persistentMessage", "true");
+    msg.setStringProperty("collector.period", "60000");
+    msg.setStringProperty("collector.url", "file:///test.txt");
+    msg.setStringProperty("collector.type", "5");
+    msg.setStringProperty("com.scalagent.joram.mom.dest.collector.ClassName", "com.scalagent.joram.mom.dest.collector.URLCollector");
+    producer.send(queue, msg);
+//    producer.send(topic, msg);
+
+    System.out.println("messages sent.");
+
+    cnx.close();
+  }
+}
