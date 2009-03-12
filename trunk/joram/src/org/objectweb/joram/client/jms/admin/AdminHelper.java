cff77884a3e90cf39f75cb93ea53b40276e039fa
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-03-12 17:22:14 +0000

    Minor changes.

--- trunk/joram/src/org/objectweb/joram/client/jms/admin/AdminHelper.java
+++ trunk/joram/src/org/objectweb/joram/client/jms/admin/AdminHelper.java
@@ -1,8 +1,8 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2004 - Bull SA
- * Copyright (C) 2004 - ScalAgent Distributed Technologies
- * Copyright (C) 2004 - France Telecom R&D
+ * Copyright (C) 2004 - 2009 ScalAgent Distributed Technologies
+ * Copyright (C) 2004 Bull SA
+ * Copyright (C) 2004 France Telecom R&D
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s): Nicolas Tachker (ScalAgent)
+ * Initial developer(s): ScalAgent Distributed Technologies
  * Contributor(s): Frederic Maistre (Bull SA)
  */
 package org.objectweb.joram.client.jms.admin;
@@ -36,9 +36,10 @@ import java.net.ConnectException;
  * The <code>AdminHelper</code> class is a utility class providing methods
  * for building special configurations such as topics cluster or hierarchy,
  * queues cluster, etc.
+ * 
+ * @deprecated
  */
-public class AdminHelper
-{
+public class AdminHelper {
   /**
    * Links two given topics in a cluster relationship.
    * <p>
@@ -53,11 +54,9 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void setClusterLink(Topic clusterTopic, Topic joiningTopic)
-         throws ConnectException, AdminException
-  {
-    AdminModule.doRequest(new SetCluster(clusterTopic.getName(),
-                                         joiningTopic.getName()));
+  public static void setClusterLink(Topic clusterTopic,
+                                    Topic joiningTopic) throws ConnectException, AdminException {
+    AdminModule.doRequest(new SetCluster(clusterTopic.getName(), joiningTopic.getName()));
   }
 
   /**
@@ -72,9 +71,7 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void unsetClusterLink(Topic topic)
-         throws ConnectException, AdminException
-  {
+  public static void unsetClusterLink(Topic topic) throws ConnectException, AdminException {
     AdminModule.doRequest(new UnsetCluster(topic.getName()));
   }
 
@@ -91,9 +88,8 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void setHierarchicalLink(Topic father, Topic son)
-         throws ConnectException, AdminException
-  {
+  public static void setHierarchicalLink(Topic father,
+                                         Topic son) throws ConnectException, AdminException {
     AdminModule.doRequest(new SetFather(father.getName(), son.getName()));
   }
 
@@ -109,12 +105,9 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void unsetHierarchicalLink(Topic topic)
-         throws ConnectException, AdminException 
-  {
+  public static void unsetHierarchicalLink(Topic topic) throws ConnectException, AdminException {
     AdminModule.doRequest(new UnsetFather(topic.getName()));
   }
-
 
   /**
    * Adds a queue to a cluster.
@@ -130,17 +123,28 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void setQueueCluster(Queue clusterQueue, Queue joiningQueue)
-    throws ConnectException, AdminException {
-    AdminModule.doRequest(
-      new AddQueueCluster(clusterQueue.getName(), joiningQueue.getName()));
+  public static void setQueueCluster(Queue clusterQueue,
+                                     Queue joiningQueue) throws ConnectException, AdminException {
+    AdminModule.doRequest(new AddQueueCluster(clusterQueue.getName(), joiningQueue.getName()));
   }
 
+  /**
+   * Adds a queue to a cluster.
+   * <p>
+   * The request fails if one or both of the queues are deleted, or
+   * can't belong to a cluster.
+   *
+   * @param clusterQueue  Queue part of the cluster, or chosen as the 
+   *          initiator of the cluster.
+   * @param joiningQueue  Queue joining the cluster.
+   *
+   * @exception ConnectException  If the admin connection is closed or broken.
+   * @exception AdminException  If the request fails.
+   * @deprecated
+   */
   public static void setQueueCluster(Destination clusterQueue,
-                                     Queue joiningQueue)
-    throws ConnectException, AdminException {
-    AdminModule.doRequest(
-      new AddQueueCluster(clusterQueue.getName(), joiningQueue.getName()));
+                                     Queue joiningQueue) throws ConnectException, AdminException {
+    AdminModule.doRequest(new AddQueueCluster(clusterQueue.getName(), joiningQueue.getName()));
   }
 
   /**
@@ -156,10 +160,8 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static void leaveQueueCluster(Queue clusterQueue, Queue leaveQueue)
-    throws ConnectException, AdminException {
-    AdminModule.doRequest(
-      new RemoveQueueCluster(clusterQueue.getName(), leaveQueue.getName()));
+  public static void leaveQueueCluster(Queue clusterQueue, Queue leaveQueue) throws ConnectException, AdminException {
+    AdminModule.doRequest(new RemoveQueueCluster(clusterQueue.getName(), leaveQueue.getName()));
   }
 
   /**
@@ -171,9 +173,7 @@ public class AdminHelper
    * @exception AdminException  If the request fails.
    * @deprecated
    */
-  public static AdminReply listQueueCluster(Queue clusterQueue)
-    throws ConnectException, AdminException {
-    return AdminModule.doRequest(
-      new ListClusterQueue(clusterQueue.getName()));
+  public static AdminReply listQueueCluster(Queue clusterQueue) throws ConnectException, AdminException {
+    return AdminModule.doRequest( new ListClusterQueue(clusterQueue.getName()));
   }
 } 
