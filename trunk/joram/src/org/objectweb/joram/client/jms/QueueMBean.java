afa9450c445154bb8969d82dd8069c0b422ce328
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-04-16 09:05:04 +0000

    Adds method to fix default DMQ.

--- trunk/joram/src/org/objectweb/joram/client/jms/QueueMBean.java
+++ trunk/joram/src/org/objectweb/joram/client/jms/QueueMBean.java
@@ -28,53 +28,62 @@ import javax.jms.JMSException;
 import java.net.ConnectException;
 
 import org.objectweb.joram.client.jms.admin.AdminException;
+import org.objectweb.joram.client.jms.admin.DeadMQueue;
 
 public interface QueueMBean extends DestinationMBean {
-  public void setNbMaxMsg(int nbMaxMsg)
-    throws ConnectException, AdminException;
+  public void setNbMaxMsg(int nbMaxMsg) throws ConnectException, AdminException;
 
-  public int getNbMaxMsg()
-    throws ConnectException, AdminException;
+  public int getNbMaxMsg() throws ConnectException, AdminException;
 
   /**
    * Admin method setting or unsetting the threshold for this queue.
    */
-  public void setThreshold(int threshold)
-    throws ConnectException, AdminException;
+  public void setThreshold(int threshold) throws ConnectException, AdminException;
 
   /** 
    * Monitoring method returning the threshold of this queue, -1 if not set.
    */
-  public int getThreshold()
-    throws ConnectException, AdminException;
+  public int getThreshold() throws ConnectException, AdminException;
    
   /**
    * Monitoring method returning the number of pending messages on this queue.
    */
-  public int getPendingMessages()
-    throws ConnectException, AdminException;
+  public int getPendingMessages() throws ConnectException, AdminException;
 
   /**
    * Monitoring method returning the number of pending requests on this queue.
    */
-  public int getPendingRequests()
-    throws ConnectException, AdminException;
+  public int getPendingRequests() throws ConnectException, AdminException;
 
-  public String[] getMessageIds()
-    throws ConnectException, AdminException;
+  public String[] getMessageIds() throws ConnectException, AdminException;
 
-  public String getMessageDigest(String msgId)
-    throws AdminException, ConnectException, JMSException;
+  public String getMessageDigest(String msgId) throws AdminException, ConnectException, JMSException;
  
-  public Properties getMessageHeader(String msgId)
-    throws ConnectException, AdminException, JMSException;
+  public Properties getMessageHeader(String msgId) throws ConnectException, AdminException, JMSException;
   
-  public Properties getMessageProperties(String msgId)
-    throws ConnectException, AdminException, JMSException;
+  public Properties getMessageProperties(String msgId) throws ConnectException, AdminException, JMSException;
 
-  public void deleteMessage(String msgId)
-    throws ConnectException, AdminException;
+  public void deleteMessage(String msgId) throws ConnectException, AdminException;
 
-  public void clear()
-    throws ConnectException, AdminException;
+  public void clear() throws ConnectException, AdminException;
+  
+  /**
+   * Sets the current queue as the default DMQ for the local server.
+   *
+   * @exception ConnectException  If the connection fails.
+   * @exception AdminException  Never thrown.
+   */
+  public void setDefaultDMQ() throws ConnectException, AdminException;
+  
+  /**
+   * Sets the current queue as the default DMQ for the given server.
+   * <p>
+   * The request fails if the target server does not belong to the platform.
+   *
+   * @param serverId  The identifier of the server.
+   *
+   * @exception ConnectException  If the connection fails.
+   * @exception AdminException  If the request fails.
+   */
+  public void setDefaultDMQ(int serverId) throws ConnectException, AdminException;
 }
