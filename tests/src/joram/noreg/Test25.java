b6a94b7a622058046be5ba15ad1d6ff05be6f738
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-04-21 12:38:56 +0000

    Fix: Test wasn't testing anything.

--- tests/src/joram/noreg/Test25.java
+++ tests/src/joram/noreg/Test25.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2003 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2003 - 2009 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -32,15 +32,19 @@ import javax.jms.MessageListener;
 import javax.jms.Session;
 
 import org.objectweb.joram.client.jms.Destination;
+import org.objectweb.joram.client.jms.FactoryParameters;
 import org.objectweb.joram.client.jms.Queue;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.User;
 import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
 import fr.dyade.aaa.agent.AgentServer;
+import framework.BaseTestCase;
 
 class ExcList25 implements ExceptionListener {
-  String name = null;
+
+  private String name = null;
+  public boolean excOccurred = false;
 
   ExcList25(String name) {
     this.name = name;
@@ -48,8 +52,8 @@ class ExcList25 implements ExceptionListener {
 
   public void onException(JMSException exc) {
     System.err.println(name + ": " + exc.getMessage());
-    Test25.assertTrue(exc instanceof javax.jms.JMSSecurityException);
-    exc.printStackTrace();
+    BaseTestCase.assertTrue(exc instanceof javax.jms.IllegalStateException);
+    excOccurred = true;
   }
 }
 
@@ -57,7 +61,7 @@ class MsgList25 implements MessageListener {
   public void onMessage(Message msg) {
     try {
       System.out.println("onMessage");
-    } catch(Exception ex) {
+    } catch (Exception ex) {
       ex.printStackTrace();
     }
   }
@@ -67,49 +71,51 @@ class MsgList25 implements MessageListener {
  *
  */
 public class Test25 extends BaseTest {
-    static ConnectionFactory cf;
-    static Destination dest;
-    static Connection cnx1;
+  static ConnectionFactory cf;
+  static Destination dest;
+  static Connection cnx1;
 
-    public static void main (String args[]) throws Exception {
-	new Test25().run();
+  public static void main(String args[]) throws Exception {
+    new Test25().run();
+  }
+
+  public void run() {
+    try {
+      startServer();
+      Thread.sleep(500L);
+
+      cf = TcpConnectionFactory.create("localhost", 16010);
+      AdminModule.connect(cf);
+
+      User.create("anonymous", "anonymous", 0);
+      dest = Queue.create();
+      dest.setFreeReading();
+      dest.setFreeWriting();
+
+      FactoryParameters fp = ((org.objectweb.joram.client.jms.ConnectionFactory) cf).getParameters();
+      fp.cnxPendingTimer = 500;
+      fp.connectingTimer = 10;
+      AdminModule.disconnect();
+
+      Connection cnx = cf.createConnection();
+      ExcList25 excList = new ExcList25("Test25.ExcLst");
+      cnx.setExceptionListener(excList);
+      Session session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer consumer = session.createConsumer(dest);
+      consumer.setMessageListener(new MsgList25());
+      cnx.start();
+
+      Thread.sleep(1000L);
+      AgentServer.stop();
+      Thread.sleep(20000L);
+      
+      assertTrue(excList.excOccurred);
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+
+      endTest();
     }
-    public void run(){
-	try{
-	    startServer();
-	    Thread.sleep(500L);
-
-	   
-	    AdminModule.connect("localhost", 16010, "root", "root", 60);
-
-	    User user = User.create("anonymous", "anonymous", 0);
-	    dest = Queue.create();
-	    dest.setFreeReading();
-	    dest.setFreeWriting();
-
-	    cf = TcpConnectionFactory.create("localhost", 16010);
-	    org.objectweb.joram.client.jms.FactoryParameters fp = ((org.objectweb.joram.client.jms.ConnectionFactory) cf).getParameters();
-	    fp.cnxPendingTimer = 500;
-	    fp.connectingTimer = 90;
-	    AdminModule.disconnect();
-
-	    Connection cnx = cf.createConnection();
-	    cnx.setExceptionListener(new ExcList25("Test25.ExcLst"));
-	    Session session = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	    MessageConsumer consumer = session.createConsumer(dest);
-	    consumer.setMessageListener(new MsgList25());
-	    cnx.start();
-	   
-
-	    Thread.sleep(1000L);
-	    AgentServer.stop();
-	    Thread.sleep(10000L);
-	}catch(Throwable exc){
-	    exc.printStackTrace();
-	    error(exc);
-	}finally{
-	  
-	    endTest();
-	}
-    }
+  }
 }
