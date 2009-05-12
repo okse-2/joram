5f6b13608e3a574b4830d130140c8d9f6bf4237a
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-05-12 15:30:06 +0000

    Adds remote reconfiguration. Test stop and restart. Adds SimpleNetwork / PoolNetwork and HttpNetwork tests.

--- tests/src/joram/reconf/ReconfTest2.java
+++ tests/src/joram/reconf/ReconfTest2.java
@@ -24,19 +24,20 @@ package joram.reconf;
 
 import java.io.File;
 
-
 import org.objectweb.joram.client.jms.admin.AdminException;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.NameAlreadyUsedException;
 import org.objectweb.joram.client.jms.admin.StartFailureException;
 import org.objectweb.joram.client.jms.admin.User;
 
-import framework.TestCase;
-
 /**
- * Testing: server reconfiguration
+ * Tests basic server reconfiguration: Check various errors during reconfiguration
+ *  - Bad port number.
+ *  - Domain already exist.
+ *  - Try to remove non empty domain.
+ *  - Try to remove a routing server.
  */
-public class ReconfTest2 extends TestCase {
+public class ReconfTest2 extends ReconfTestBase {
 
   public static void main(String[] args) {
     new ReconfTest2().run();
@@ -44,104 +45,75 @@ public class ReconfTest2 extends TestCase {
 
   public void run() {
     try {
-      startAgentServer((short) 0, (File) null,
-          new String[] { "-DTransaction=fr.dyade.aaa.util.NullTransaction" });
+      startAgentServer((short) 0, (File) null, new String[0]);
 
       AdminModule.connect("localhost", 2560, "root", "root", 60);
-
       User.create("anonymous", "anonymous", 0);
+      
+      checkQueue((short) 0);
 
-      //System.out.println("Add domain D0");
-
-      Exception e = null;
       try {
         AdminModule.addDomain("D0", 0, 99999999);
       } catch (Exception exc) {
-        //System.out.println("Expected error: " + exc);
-        e = exc;
+        assertTrue("Exception expected: port out of range", exc instanceof StartFailureException);
       }
-      assertTrue("Exception expected: port out of range", e instanceof StartFailureException);
 
-      //System.out.println("Retry adding domain D0");
       AdminModule.addDomain("D0", 0, 17770);
 
-      Exception e2 = null;
-      // System.out.println("Retry adding domain D0");
       try {
         AdminModule.addDomain("D0", 0, 17770);
       } catch (Exception exc) {
         //System.out.println("Expected error: " + exc);
-        e2 = exc;
+        assertTrue("Exception expected: domain name already used", exc instanceof NameAlreadyUsedException);
       }
-      assertTrue("Exception expected: domain name already used", e2 instanceof NameAlreadyUsedException);
 
-      //System.out.println("Add server s1");
       AdminModule.addServer(1, "localhost", "D0", 17771, "s1");
 
-      Exception e3 = null;
-      //System.out.println("try to remove D0");
       try {
         AdminModule.removeDomain("D0");
       } catch (Exception exc) {
         //System.out.println("Expected error: " + exc);
-        e3 = exc;
+        assertTrue("Exception expected: domain contains more than 1 server", exc instanceof AdminException);
       }
-      assertTrue("Exception expected: domain contains more than 1 server", e3 instanceof AdminException);
 
-      ReconfTest.startServer((short) 1, "s1");
+      deployAgentServer((short) 1, "./s1");
+      startAgentServer((short) 1, new File("./s1"), new String[0]);
+      checkQueue((short) 1);
 
-      ReconfTest.checkQueue((short) 1);
-
-      //System.out.println("Add domain D1");
       AdminModule.addDomain("D1", 1, 18770);
 
-      //System.out.println("Add server s2");
       AdminModule.addServer(2, "localhost", "D1", 18771, "s2");
+      deployAgentServer((short) 2, "s2");
+      startAgentServer((short) 2, new File("./s2"), new String[0]);
+      checkQueue((short) 2);
 
-      ReconfTest.startServer((short) 2, "s2");
-
-      ReconfTest.checkQueue((short) 2);
-
-      //System.out.println("Remove server s1");
-      Exception e4 = null;
       try {
         AdminModule.removeServer(1);
       } catch (Exception exc) {
-        //System.out.println("Expected error: " + exc);
-        e4 = exc;
+        assertTrue("Exception expected: server belongs to more than 1 domain", exc instanceof AdminException);
       }
-      assertTrue("Exception expected: server belongs to more than 1 domain", e4 instanceof AdminException);
 
-      // First stop the server because it must be reachable
-      // in order to be stopped.
-      //System.out.println("Stop server s2");
+      // First stop the server (it must be reachable in order to be stopped).
       AdminModule.stopServer(2);
-      //System.out.println("Server s2 stopped");
-
-      // Then clean the configuration: 
-      // the server is not reachable
-      // anymore.
-      //System.out.println("Remove server s2");
+      // Cleans the configuration: the server is not reachable anymore.
       AdminModule.removeServer(2);
-
-      //System.out.println("Remove domain D1");
+      // Removes the corresponding domain
       AdminModule.removeDomain("D1");
 
-      //System.out.println("Stop server s1");
+      checkQueue((short) 1);
+      
+      // First stop the server (it must be reachable in order to be stopped).
       AdminModule.stopServer(1);
-      //System.out.println("Server s1 stopped");
-
-      //System.out.println("Remove server s1");
+      // Cleans the configuration: the server is not reachable anymore.
       AdminModule.removeServer(1);
-
-      //System.out.println("Remove domain D0");
+      // Removes the corresponding domain
       AdminModule.removeDomain("D0");
       
+      checkQueue((short) 0);
     } catch (Throwable exc) {
       exc.printStackTrace();
       error(exc);
     } finally {
-      System.out.println("Stop server s0");
       stopAgentServer((short) 0);
       endTest();
     }
--- tests/src/joram/reconf/ReconfTest3.java
+++ tests/src/joram/reconf/ReconfTest3.java
@@ -23,28 +23,19 @@
 package joram.reconf;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
-
-import org.objectweb.joram.client.jms.Queue;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.User;
-import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
-
-import framework.TestCase;
 
 /**
- * Testing: server reconfiguration
+ * Tests basic server reconfiguration: 2 initial servers in a SimpleNetwork domain
+ *  - Adds a third server (S2) in the domain D0, then removes it.
+ *  - Adds a third server (S3) in the domain D0, then removes it.
+ *  - Removes S2 and the useless dommain (D0).
+ *  
+ * This test works with classic networks: SimpleNetwork, PoolNetwork, etc.
  */
-public class ReconfTest3 extends TestCase {
+public class ReconfTest3 extends ReconfTestBase {
 
   public static void main(String[] args) {
     new ReconfTest3().run();
@@ -52,77 +43,51 @@ public class ReconfTest3 extends TestCase {
 
   public void run() {
     try {
-      startAgentServer((short) 0, (File) null,
-          new String[] { "-DTransaction=fr.dyade.aaa.util.NullTransaction" });
+      startAgentServer((short) 0, (File) null, new String[0]);
+      startAgentServer((short) 1, (File) null, new String[0]);
 
-      startAgentServer((short) 1, (File) null,
-          new String[] { "-DTransaction=fr.dyade.aaa.util.NullTransaction" });
-
-      System.out.println("waiting");
       Thread.sleep(1000L);
 
       AdminModule.connect("localhost", 2560, "root", "root", 60);
-
       User.create("anonymous", "anonymous", 0);
 
+      checkQueue((short) 0);
       checkQueue((short) 1);
 
-      System.out.println("Add server s2");
       AdminModule.addServer(2, "localhost", "D0", 17772, "s2");
-
-      startServer((short) 2, "s2");
-
+      deployAgentServer((short) 2, "./s2");
+      startAgentServer((short) 2, new File("./s2"), new String[0]);
+      
       checkQueue((short) 2);
       checkQueue((short) 1);
 
-      // First stop the server because it must be reachable
-      // in order to be stopped.
-      //       System.out.println("Stop server s2");
+      // First stop the server because it must be reachable in order to be stopped.
       AdminModule.stopServer(2);
-      //       System.out.println("Server s2 stopped");
-
-      // Then clean the configuration: 
-      // the server is not reachable
-      // anymore.
-      System.out.println("Remove server s2");
+      // Then clean the configuration: the server is not reachable anymore.
       AdminModule.removeServer(2);
-      //       System.out.println("Server s2 removed");
 
       checkQueue((short) 1);
 
-      System.out.println("Add server s3");
-      AdminModule.addServer(3, "localhost", "D0", 17773, "s3");
-
-      startServer((short) 3, "s3");
+      AdminModule.addServer(3, "localhost", "D0", 17773, "./s3");
+      deployAgentServer((short) 3, "s3");
+      startAgentServer((short) 3, new File("./s3"), new String[0]);
 
       checkQueue((short) 3);
       checkQueue((short) 1);
 
-      // First stop the server because it must be reachable
-      // in order to be stopped.
-      //       System.out.println("Stop server s3");
+      // First stop the server because it must be reachable in order to be stopped.
       AdminModule.stopServer(3);
-      //       System.out.println("Server s3 stopped");
-
-      // Then clean the configuration: 
-      // the server is not reachable
-      // anymore.
-      System.out.println("Remove server s3");
+      // Then clean the configuration: the server is not reachable anymore.
       AdminModule.removeServer(3);
-      //       System.out.println("Server s3 removed");
 
       checkQueue((short) 1);
 
-      //       System.out.println("Stop server s1");
       AdminModule.stopServer(1);
-      //       System.out.println("Server s1 stopped");
-
-      System.out.println("Remove server s1");
       AdminModule.removeServer(1);
-      //       System.out.println("Server s1 removed");
 
-      System.out.println("Remove domain D0");
       AdminModule.removeDomain("D0");
+
+      checkQueue((short) 0);
     } catch (Throwable exc) {
       exc.printStackTrace();
       error(exc);
@@ -131,49 +96,5 @@ public class ReconfTest3 extends TestCase {
       stopAgentServer((short) 0);
       endTest();
     }
-  }
-
-  public static void startServer(short sid, String serverName) throws Exception {
-    String configXml = AdminModule.getConfiguration();
-
-    File sdir = new File("./" + serverName);
-    sdir.mkdir();
-    File sconfig = new File(sdir, "a3servers.xml");
-    FileOutputStream fos = new FileOutputStream(sconfig);
-    PrintWriter pw = new PrintWriter(fos);
-    pw.println(configXml);
-    pw.flush();
-    pw.close();
-    fos.close();
-
-    //     System.out.println("Start server " + serverName);
-    startAgentServer(sid, sdir, new String[] { "-DTransaction=fr.dyade.aaa.util.NullTransaction" });
-  }
-
-  public static void checkQueue(short sid) throws Exception {
-    //     System.out.println("Create queue on site " + sid);
-    Queue queue = Queue.create(sid);
-    queue.setFreeReading();
-    queue.setFreeWriting();
-
-    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 2560);
-
-    Connection connection = cf.createConnection("anonymous", "anonymous");
-    connection.start();
-
-    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-    MessageProducer producer = session.createProducer(queue);
-    TextMessage msg = session.createTextMessage("testcheck");
-
-    //     System.out.println("send msg");
-    producer.send(msg);
-
-    MessageConsumer consumer = session.createConsumer(queue);
-
-    //     System.out.println("receive msg");
-    msg = (TextMessage) consumer.receive();
-    assertEquals("testcheck", msg.getText());
-    connection.close();
   }
 }
