56feb0d082ec6f546bff54166c9a1c2a9d1f5fec
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-05-12 15:30:06 +0000

    Adds remote reconfiguration. Test stop and restart. Adds SimpleNetwork / PoolNetwork and HttpNetwork tests.

--- trunk/tests/src/joram/reconf/ReconfTestBase.java
+++ trunk/tests/src/joram/reconf/ReconfTestBase.java
@@ -17,11 +17,11 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
- * Initial developer(s):  ScalAgent D.T.
- * Contributor(s):
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
  */
-package joram.reconf;
 
+package joram.reconf;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,100 +34,67 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-
 import org.objectweb.joram.client.jms.Queue;
 import org.objectweb.joram.client.jms.admin.AdminModule;
-import org.objectweb.joram.client.jms.admin.User;
 import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
 import framework.TestCase;
 
 /**
- * Testing: basic server reconfiguration. Add one domain and one server then
- * remove them.
+ * Base class to check the reconfiguration methods.
  */
 public class ReconfTestBase extends TestCase {
-
-  public static void main(String[] args) {
-    new ReconfTestBase().run();
-  }
-
-  public void run() {
-    try {
-      startAgentServer(
-        (short)0, (File)null, 
-        new String[] { "-DTransaction=fr.dyade.aaa.util.NullTransaction -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,address=2222,suspend=y" });
-
-      AdminModule.connect("localhost", 2560, "root", "root", 60);
-
-      AdminModule.addDomain("D0", "fr.dyade.aaa.agent.PoolNetwork", 0, 17770);
-      AdminModule.addServer(1, "localhost", "D0", 17771, "s1");
-
-      User.create("anonymous", "anonymous", 0);
-      
-      startServer((short)1, "s1");
-
-      checkQueue((short)1);
-
-      AdminModule.stopServer(1);
-      AdminModule.removeServer(1);
-      AdminModule.removeDomain("D0");
-
-    } catch (Throwable exc) {
-      exc.printStackTrace();
-      error(exc);
-    } finally {
-      System.out.println("Stop server s0");
-      stopAgentServer((short) 0);
-      endTest();
-    }
-  }
-
-  public static void startServer(short sid, String serverName) 
-    throws Exception {
+  /**
+   * Creates a directory to deploy a new server and generates the corresponding
+   * configuration file.
+   * 
+   * @param sid   the unique id of the server to deploy.
+   * @param dir   the working directory of the new server.
+   * @throws Exception
+   */
+  public static void deployAgentServer(short sid, String dir) throws Exception {
     String configXml = AdminModule.getConfiguration();
 
-    File sdir = new File("./" + serverName);
+    File sdir = new File(dir);
     sdir.mkdir();
+    
     File sconfig = new File(sdir, "a3servers.xml");
+    
     FileOutputStream fos = new FileOutputStream(sconfig);
     PrintWriter pw = new PrintWriter(fos);
     pw.println(configXml);
     pw.flush();
     pw.close();
     fos.close();
-    
-    System.out.println("Start server " + serverName);
-    startAgentServer(
-      sid, sdir, 
-      new String[]{"-DTransaction=fr.dyade.aaa.util.NullTransaction"});
   }
 
-  public static void checkQueue(short sid) 
-    throws Exception {
-    System.out.println("Create queue on site " + sid);
+  /**
+   * Check that the specified server is running. This method creates a queue on the
+   * server then send and receive a message through this queue.
+   * 
+   * @param sid   the unique id of the server to check.
+   * @throws Exception
+   */
+  public static void checkQueue(short sid) throws Exception {
     Queue queue = Queue.create(sid);
     queue.setFreeReading();
     queue.setFreeWriting();
-    
-    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 2560);
 
+    ConnectionFactory cf = TcpConnectionFactory.create("localhost", 2560);
     Connection connection = cf.createConnection("anonymous", "anonymous");
+    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    MessageProducer producer = session.createProducer(queue);
+    MessageConsumer consumer = session.createConsumer(queue);
     connection.start();
 
-    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    TextMessage msg1 = session.createTextMessage("testcheck-" + System.currentTimeMillis());
+    producer.send(msg1);
+
+    TextMessage msg2 = (TextMessage) consumer.receive();
+    assertEquals("check JMSMessageID", msg1.getJMSMessageID(), msg2.getJMSMessageID());
+    assertEquals("check text", msg1.getText(), msg2.getText());
     
-    MessageProducer producer = session.createProducer(queue);
-    TextMessage msg = session.createTextMessage("testcheck");
-    
-    System.out.println("send msg");
-    producer.send(msg);
-    
-    MessageConsumer consumer = session.createConsumer(queue);
-    
-    System.out.println("receive msg");
-    msg = (TextMessage)consumer.receive();
-    assertEquals("testcheck",msg.getText());
     connection.close();
   }
+
 }
