52f0b575345829158a7fd8403752bf4091dad7c2
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-07-22 13:20:17 +0000

    Adds the capacity to transmit extra information about the deleted agent when notifying the deletion.

--- joram/src/fr/dyade/aaa/agent/AgentDeleteRequest.java
+++ joram/src/fr/dyade/aaa/agent/AgentDeleteRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2001 - 2009 ScalAgent Distributed Technologies
  * Copyright (C) 1996 - 2000 BULL
  * Copyright (C) 1996 - 2000 INRIA
  *
@@ -22,17 +22,27 @@ package fr.dyade.aaa.agent;
 
 public class AgentDeleteRequest extends Notification {
 
-  /**
-   * 
-   */
+  /** Define serialVersionUID for interoperability. */
   private static final long serialVersionUID = 1L;
+
   /**
    * the agent identified by 'reply' will receive a DeleteAck
    * when destruction complete
    */
   public AgentId reply;
 
+  /**
+   * The extra information is added to the DeleteAck when notifying the reply
+   * agent.
+   */
+  public Object extraInformation;
+
   public AgentDeleteRequest(AgentId reply) {
     this.reply = reply;
+  }
+
+  public AgentDeleteRequest(AgentId reply, Object extraInformation) {
+    this.reply = reply;
+    this.extraInformation = extraInformation;
   }
 }
