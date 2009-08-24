852085372d3f691b0dc536b4ba4c725c9b67139f
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-08-24 14:47:09 +0000

    Final version.

--- tests/src/joram/reconf/ReconfTest9.java
+++ tests/src/joram/reconf/ReconfTest9.java
@@ -31,13 +31,11 @@ import org.objectweb.joram.client.jms.admin.User;
  * Tests basic server reconfiguration: 2 initial servers in a domain
  *  - Successively adds N server in the domain D0.
  *  
- *  - Adds a third server (S3) in the domain D0, then removes it.
- *  - Removes S2 and the useless dommain (D0).
- *  
  * This test works with classic networks: SimpleNetwork, PoolNetwork, etc.
  */
 public class ReconfTest9 extends ReconfTestBase {
-
+  static int nbServers = 20;
+  
   public static void main(String[] args) {
     new ReconfTest9().run();
   }
@@ -56,7 +54,7 @@ public class ReconfTest9 extends ReconfTestBase {
       checkQueue((short) 1);
 
       long start, end;
-      for (int i=2; i<20; i++) {
+      for (int i=2; i<nbServers; i++) {
         start = System.currentTimeMillis();
         AdminModule.addServer(i, "localhost", "D0", 17771 + i, "s" + i);
         deployAgentServer((short) i, "./s" + i);
@@ -65,79 +63,63 @@ public class ReconfTest9 extends ReconfTestBase {
         System.out.println("Adds server#" + i + "-> " + (end - start));
       }
       
-//      start = System.currentTimeMillis();
-//      for (int i=2; i<10; i++) {
-//        startAgentServer((short) i, new File("./s" + i), new String[0]);
-//      }
-//      end = System.currentTimeMillis();
-//      System.out.println("starts servers -> " + (end - start));
-//      
-//      for (int i=0; i<10; i++) {      
-//        start = System.currentTimeMillis();
-//        checkQueue((short) i);
-//        end = System.currentTimeMillis();
-//        
-//        System.out.println("checkQueue#" + i + "-> " + (end - start));
-//      }
-//
-//      for (int i=2; i<10; i++) {
-//        // First stop the server because it must be reachable in order to be stopped.
-//        AdminModule.stopServer(i);
-//        // Then clean the configuration: the server is not reachable anymore.
-//        AdminModule.removeServer(i);
-//      }
-//      
-//      checkQueue((short) 0);
-//      checkQueue((short) 1);
-//
-//      for (int i=2; i<10; i++) {
-//        start = System.currentTimeMillis();
-//        AdminModule.addServer(i, "localhost", "D0", 17771 + i, "s" + i);
-//        deployAgentServer((short) i, "./s" + i);
-//        end = System.currentTimeMillis();
-//        
-//        startAgentServer((short) i, new File("./s" + i), new String[0]);
-//        System.out.println("Adds server#" + i + "-> " + (end - start));
-//      }
-//      
-//      for (int i=0; i<10; i++) {      
-//        start = System.currentTimeMillis();
-//        checkQueue((short) i);
-//        end = System.currentTimeMillis();
-//        
-//        System.out.println("checkQueue#" + i + "-> " + (end - start));
-//      }
-//
-//      for (int i=2; i<10; i++) {
-//        // First stop the server because it must be reachable in order to be stopped.
-//        AdminModule.stopServer(i);
-//        // Then clean the configuration: the server is not reachable anymore.
-//        AdminModule.removeServer(i);
-//      }
-//      
-//      checkQueue((short) 1);
-//
-//      
-//      
-//      
-////      AdminModule.addServer(3, "localhost", "D0", 17773, "./s3");
-////      deployAgentServer((short) 3, "s3");
-////      startAgentServer((short) 3, new File("./s3"), new String[0]);
-////
-////      checkQueue((short) 3);
-////      checkQueue((short) 1);
-////
-////      // First stop the server because it must be reachable in order to be stopped.
-////      AdminModule.stopServer(3);
-////      // Then clean the configuration: the server is not reachable anymore.
-////      AdminModule.removeServer(3);
-////
-////      checkQueue((short) 1);
-//
+      start = System.currentTimeMillis();
+      for (int i=2; i<nbServers; i++) {
+        startAgentServer((short) i, new File("./s" + i), new String[0]);
+      }
+      end = System.currentTimeMillis();
+      System.out.println("starts servers -> " + (end - start));
+      
+      for (int i=0; i<nbServers; i++) {      
+        start = System.currentTimeMillis();
+        checkQueue((short) i);
+        end = System.currentTimeMillis();
+        
+        System.out.println("checkQueue#" + i + "-> " + (end - start));
+      }
+
+      for (int i=2; i<nbServers; i++) {
+        // First stop the server because it must be reachable in order to be stopped.
+        AdminModule.stopServer(i);
+        // Then clean the configuration: the server is not reachable anymore.
+        AdminModule.removeServer(i);
+      }
+      
+      checkQueue((short) 0);
+      checkQueue((short) 1);
+
+      for (int i=2; i<nbServers; i++) {
+        start = System.currentTimeMillis();
+        AdminModule.addServer(i, "localhost", "D0", 17771 + i, "s" + i);
+        deployAgentServer((short) i, "./s" + i);
+        end = System.currentTimeMillis();
+        
+        startAgentServer((short) i, new File("./s" + i), new String[0]);
+        System.out.println("Adds server#" + i + "-> " + (end - start));
+      }
+      
+      for (int i=0; i<nbServers; i++) {      
+        start = System.currentTimeMillis();
+        checkQueue((short) i);
+        end = System.currentTimeMillis();
+        
+        System.out.println("checkQueue#" + i + "-> " + (end - start));
+      }
+
+      for (int i=2; i<nbServers; i++) {
+        // First stop the server because it must be reachable in order to be stopped.
+        AdminModule.stopServer(i);
+        // Then clean the configuration: the server is not reachable anymore.
+        AdminModule.removeServer(i);
+      }
+      
+      checkQueue((short) 1);
+
+
       AdminModule.stopServer(1);
       AdminModule.removeServer(1);
 
-//      AdminModule.removeDomain("D0");
+      AdminModule.removeDomain("D0");
 
       checkQueue((short) 0);
     } catch (Throwable exc) {
