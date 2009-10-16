4fdb2c7ae1cfa30273aa918ae6e700cb6e10372d
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-10-16 12:37:10 +0000

    Rename package objectweb in ow2.

--- mom-amqp/org.ow2.joram.mom.amqp/src/main/java/org/objectweb/joram/mom/amqp/DeliveryListener.java
+++
@@ -1,7 +0,0 @@
-package org.objectweb.joram.mom.amqp;
-
-public interface DeliveryListener {
-
-  public abstract void deliver(Deliver deliver, Queue queue);
-
-}
---
+++ mom-amqp/org.ow2.joram.mom.amqp/src/main/java/org/ow2/joram/mom/amqp/exceptions/ConnectionException.java
@@ -0,0 +1,11 @@
+package org.ow2.joram.mom.amqp.exceptions;
+
+public abstract class ConnectionException extends AMQPException {
+
+  private static final long serialVersionUID = 1L;
+
+  public ConnectionException(String message) {
+    super(message);
+  }
+
+}
