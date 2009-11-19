1ef6b16bce606921945e7e5baee294647f1c62f5
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-11-19 14:24:42 +0000

    Add some time to let the server start. Correct indentation.

--- trunk/tests/src/joram/recovery/Recover_2.java
+++ trunk/tests/src/joram/recovery/Recover_2.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2006 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2006 - 2009 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -34,146 +34,137 @@ import javax.naming.InitialContext;
 
 import framework.TestCase;
 
-
-
 /**
- * Test : send 10 message to the server; receive 5 messages with ack and 5 with no ack,
- * kill the server; restart the server. 
- * The server restores 5 messages, and then the receiver can receive it.
- * Use a Queue
- *
+ * Test : send 10 message to the server; receive 5 messages with ack and 5 with
+ * no ack, kill the server; restart the server. The server restores 5 messages,
+ * and then the receiver can receive it. Use a Queue
  */
 public class Recover_2 extends TestCase {
 
-    private Connection cnx;
-    private Session sessionp ;
-    private Session sessionc ; 
-    private ConnectionFactory cf;
-    private MessageProducer producer;
-    private MessageConsumer consumer;
-    private Queue queue;
-   
+  private Connection cnx;
+  private Session sessionp;
+  private Session sessionc;
+  private ConnectionFactory cf;
+  private MessageProducer producer;
+  private MessageConsumer consumer;
+  private Queue queue;
 
-    public void connect(){
-	try{
-	    cnx = cf.createConnection();
-	    sessionp = cnx.createSession(false,
-					 Session.AUTO_ACKNOWLEDGE);
-	    sessionc = cnx.createSession(false,
-					 Session.CLIENT_ACKNOWLEDGE);
-	    cnx.start();
-	    consumer = sessionc.createConsumer(queue);
-	    producer = sessionp.createProducer(queue);
-	}catch(Exception exc){
-	    exc.printStackTrace();
-	    error(exc);
-	}
+  public void connect() {
+    try {
+      cnx = cf.createConnection();
+      sessionp = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      sessionc = cnx.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+      cnx.start();
+      consumer = sessionc.createConsumer(queue);
+      producer = sessionp.createProducer(queue);
+    } catch (Exception exc) {
+      exc.printStackTrace();
+      error(exc);
     }
-    
-    public static void main(String[] args) {
-	new Recover_2().run();
+  }
+
+  public static void main(String[] args) {
+    new Recover_2().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("server start");
+      startAgentServer((short) 0);
+
+      Thread.sleep(2000);
+
+      admin();
+      System.out.println("admin config ok");
+
+      Context ictx = new InitialContext();
+      queue = (Queue) ictx.lookup("queue");
+      cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+      connect();
+
+      TextMessage msg = null;
+
+      for (int j = 0; j < 10; j++) {
+        msg = sessionp.createTextMessage();
+        msg.setText("message#" + j);
+        producer.send(msg);
+      }
+      System.out.println("message sent");
+
+      TextMessage msg1 = null;
+      // receive 5 messages with ack
+      for (int j = 0; j < 5; j++) {
+        msg1 = (TextMessage) consumer.receive();
+        //	System.out.println(msg1.getText());
+        assertTrue(msg1.getText().startsWith("message#"));
+      }
+      msg1.acknowledge();
+
+      // and 5 messages with no ack
+      for (int j = 0; j < 5; j++) {
+        msg1 = (TextMessage) consumer.receive();
+        //	System.out.println(msg1.getText());
+        assertTrue(msg1.getText().startsWith("message#"));
+      }
+      System.out.println("all message receive");
+
+      // kill and restart server
+      System.out.println("Server stop ");
+      stopAgentServer((short) 0);
+      System.out.println("server start");
+      startAgentServer((short) 0);
+
+      Thread.sleep(2000);
+
+      connect();
+      // the consumer receive the message from the queue
+
+      for (int j = 0; j < 5; j++) {
+        msg1 = (TextMessage) consumer.receive();
+        //	System.out.println(msg1.getText());
+        assertTrue(msg1.getText().startsWith("message#"));
+      }
+      msg1.acknowledge();
+      msg1 = (TextMessage) consumer.receive(3000);
+      // no more message
+      assertEquals(null, msg1);
+      cnx.close();
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      stopAgentServer((short) 0);
+      endTest();
     }
-          
-    public void run() {
-	try {
-	    System.out.println("server start");
-	    startAgentServer((short)0);
-	   
-	    
-	    admin();
-	    System.out.println("admin config ok");
-	    
-	    Context  ictx = new InitialContext();
-	    queue = (Queue) ictx.lookup("queue");
-   	    cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
-	    connect();
-	   	    
-	    TextMessage msg = null; 
+  }
 
-	    
-	    for(int j=0;j<10;j++){
-		msg = sessionp.createTextMessage();
-		msg.setText("message#"+j);
-		producer.send(msg);
-	    }
-	    System.out.println("message sent");
+  /**
+   * Admin : Create queue and a user anonymous; use jndi
+   */
+  public void admin() throws Exception {
+    // conexion 
+    org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560, "root", "root", 60);
+    // create a Queue   
+    org.objectweb.joram.client.jms.Queue queue = (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue
+        .create("queue");
 
-	    TextMessage msg1= null;
-	    // receive 5 messages witn ack
-	    for(int j=0;j<5;j++){
-		msg1 = (TextMessage) consumer.receive();
-		//	System.out.println(msg1.getText());
-		assertTrue(msg1.getText().startsWith("message#"));
-	    }
-	    msg1.acknowledge();
+    // create a user
+    org.objectweb.joram.client.jms.admin.User user = org.objectweb.joram.client.jms.admin.User.create(
+        "anonymous", "anonymous");
+    // set permissions
+    queue.setFreeReading();
+    queue.setFreeWriting();
 
-	    // and 5 messages with no ack
-	    for(int j=0;j<5;j++){
-		msg1 = (TextMessage) consumer.receive();
-		//	System.out.println(msg1.getText());
-		assertTrue(msg1.getText().startsWith("message#"));
-	    }
-	    System.out.println("all message receive");
+    javax.jms.ConnectionFactory cf = org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create(
+        "localhost", 2560);
 
-	    // kill and restart server
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-       	    System.out.println("server start");
-	    startAgentServer((short)0);
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.bind("cf", cf);
+    jndiCtx.bind("queue", queue);
+    jndiCtx.close();
 
-	    connect();
-	    // the consumer receive the message from the queue
-	   
-	    for(int j=0;j<5;j++){
-		msg1 = (TextMessage) consumer.receive();
-		//	System.out.println(msg1.getText());
-		assertTrue(msg1.getText().startsWith("message#"));
-	    }
-	    msg1.acknowledge();
-	    msg1 = (TextMessage) consumer.receive(3000);
-	    // no more message
-	    assertEquals(null,msg1);
-	    cnx.close();
-	} catch (Throwable exc) {
-	    exc.printStackTrace();
-	    error(exc);
-	}
-	finally {
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-	    endTest(); 
-	}
-    }
-
-    /**
-     * Admin : Create queue and a user anonymous
-     *   use jndi
-     */
-    public void admin() throws Exception {
-	// conexion 
-	org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
-								 "root", "root", 60);
-	// create a Queue   
-	org.objectweb.joram.client.jms.Queue queue =
-	    (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create("queue"); 
-
-        // create a user
-	org.objectweb.joram.client.jms.admin.User user =
-	    org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
-	// set permissions
-	queue.setFreeReading();
-	queue.setFreeWriting();
-
-      	javax.jms.ConnectionFactory cf =
-	    org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
-
-	javax.naming.Context jndiCtx = new javax.naming.InitialContext();
-	jndiCtx.bind("cf", cf);
-	jndiCtx.bind("queue", queue);
-	jndiCtx.close();
-	   
-	org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
-    }
+    org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
+  }
 }
-
