272a79da9a60540c35c9c31f5c5e1a5caa4196b4
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2009-12-04 15:51:38 +0000

    Override getFactoryHint method to avoid bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=281014

---
+++ designer/org.ow2.joram.design.diagram/src/org/ow2/joram/design/model/joram/diagram/edit/policies/JORAMJMSCanonicalEditPolicy.java
@@ -0,0 +1,90 @@
+package org.ow2.joram.design.model.joram.diagram.edit.policies;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CanonicalEditPolicy;
+import org.eclipse.gmf.runtime.notation.View;
+import org.ow2.joram.design.model.joram.JoramPackage;
+import org.ow2.joram.design.model.joram.diagram.edit.parts.QueueEditPart;
+import org.ow2.joram.design.model.joram.diagram.edit.parts.TopicEditPart;
+import org.ow2.joram.design.model.joram.diagram.edit.parts.UserEditPart;
+import org.ow2.joram.design.model.joram.diagram.part.JoramDiagramUpdater;
+import org.ow2.joram.design.model.joram.diagram.part.JoramNodeDescriptor;
+import org.ow2.joram.design.model.joram.diagram.part.JoramVisualIDRegistry;
+
+/**
+ * @generated
+ */
+public class JORAMJMSCanonicalEditPolicy extends CanonicalEditPolicy {
+
+  /**
+   * @generated
+   */
+  Set myFeaturesToSynchronize;
+
+  /**
+   * @generated NOT
+   */
+  @Override
+  protected String getFactoryHint(IAdaptable elementAdapter) {
+    CanonicalElementAdapter element = (CanonicalElementAdapter) elementAdapter;
+    int VID = JoramVisualIDRegistry.getNodeVisualID((View) getHost().getModel(), (EObject) element
+        .getRealObject());
+    return JoramVisualIDRegistry.getType(VID);
+  }
+
+  /**
+   * @generated
+   */
+  protected List getSemanticChildrenList() {
+    View viewObject = (View) getHost().getModel();
+    List result = new LinkedList();
+    for (Iterator it = JoramDiagramUpdater.getJORAMJMS_7005SemanticChildren(viewObject).iterator(); it
+        .hasNext();) {
+      result.add(((JoramNodeDescriptor) it.next()).getModelElement());
+    }
+    return result;
+  }
+
+  /**
+   * @generated
+   */
+  protected boolean isOrphaned(Collection semanticChildren, final View view) {
+    int visualID = JoramVisualIDRegistry.getVisualID(view);
+    switch (visualID) {
+    case TopicEditPart.VISUAL_ID:
+    case QueueEditPart.VISUAL_ID:
+    case UserEditPart.VISUAL_ID:
+      if (!semanticChildren.contains(view.getElement())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @generated
+   */
+  protected String getDefaultFactoryHint() {
+    return null;
+  }
+
+  /**
+   * @generated
+   */
+  protected Set getFeaturesToSynchronize() {
+    if (myFeaturesToSynchronize == null) {
+      myFeaturesToSynchronize = new HashSet();
+      myFeaturesToSynchronize.add(JoramPackage.eINSTANCE.getJORAM_JmsObjects());
+    }
+    return myFeaturesToSynchronize;
+  }
+
+}
