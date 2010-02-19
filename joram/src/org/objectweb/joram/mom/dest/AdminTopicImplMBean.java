03ad7330fe2b37fcec1114710c316a2695986122
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-02-19 16:51:37 +0000

    Add MBean methods to create users and destinations.

--- joram/src/org/objectweb/joram/mom/dest/AdminTopicImplMBean.java
+++ joram/src/org/objectweb/joram/mom/dest/AdminTopicImplMBean.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2006 ScalAgent Distributed Technologies
+ * Copyright (C) 2006 - 2010 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,6 +22,108 @@
  */
 package org.objectweb.joram.mom.dest;
 
+import org.objectweb.joram.shared.security.SimpleIdentity;
+
 public interface AdminTopicImplMBean extends TopicImplMBean {
+
+  /**
+   * Creates a new topic on the selected server.
+   * 
+   * @param name
+   *          the topic name
+   * @param topicClassName
+   *          the topic class to instantiate
+   * @param serverId
+   *          the server where the topic will be deployed
+   */
+  public void createTopic(String name, String topicClassName, int serverId);
+
+  /**
+   * Creates a new topic on the selected server. The topic will be an instance
+   * of {@link Topic}.
+   * 
+   * @param name
+   *          the topic name
+   * @param serverId
+   *          the server where the topic will be deployed
+   */
+  public void createTopic(String name, int serverId);
+
+  /**
+   * Creates a new topic on the local server.
+   * 
+   * @param name
+   *          the topic name
+   */
+  public void createTopic(String name);
+
+  /**
+   * Creates a new queue on the selected server.
+   * 
+   * @param name
+   *          the topic name
+   * @param queueClassName
+   *          the queue class to instantiate
+   * @param serverId
+   *          the server where the queue will be deployed
+   */
+  public void createQueue(String name, String queueClassName, int serverId);
+
+  /**
+   * Creates a new queue on the selected server. The queue will be an instance
+   * of {@link Queue}.
+   * 
+   * @param name
+   *          the topic name
+   * @param serverId
+   *          the server where the queue will be deployed
+   */
+  public void createQueue(String name, int serverId);
+
+  /**
+   * Creates a new queue on the local server.
+   * 
+   * @param name
+   *          the queue name
+   */
+  public void createQueue(String name);
+
+  /**
+   * Creates a new user on the selected server.
+   * 
+   * @param user
+   *          the user name
+   * @param passwd
+   *          the user password
+   * @param serverId
+   *          the server where the user will be created
+   * @param identityClassName
+   *          the identity class name to instantiate
+   */
+  public void createUser(String user, String passwd, int serverId, String identityClassName) throws Exception;
+
+  /**
+   * Creates a new user on the selected server. {@link SimpleIdentity} class is
+   * used.
+   * 
+   * @param user
+   *          the user name
+   * @param passwd
+   *          the user password
+   * @param serverId
+   *          the server where the user will be created
+   */
+  public void createUser(String user, String passwd, int serverId) throws Exception;
+
+  /**
+   * Creates a new user on the local server. {@link SimpleIdentity} class is
+   * used.
+   * 
+   * @param user
+   *          the user name
+   * @param passwd
+   *          the user password
+   */
+  public void createUser(String user, String passwd) throws Exception;
 
 }
