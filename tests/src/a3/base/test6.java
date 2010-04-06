9118641f2e8ff4201209a01635cc6203769f3871
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-04-06 14:50:44 +0000

    Change assertion as exception message has been changed to a more detailed one.

--- tests/src/a3/base/test6.java
+++ tests/src/a3/base/test6.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C)  2001 ScalAgent Distributed Technologies
+ * Copyright (C)  2001 - 2010 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,7 +24,7 @@
 
 package a3.base;
 
-import fr.dyade.aaa.agent.*;
+import fr.dyade.aaa.agent.AgentServer;
 import framework.TestCase;
 
 public class test6 extends TestCase {
@@ -46,7 +46,7 @@ public class test6 extends TestCase {
                                  excType + '(' + excMsg + ')'));
     } catch (Throwable exc) {
       assertEquals(excType, exc.getClass().getName());
-      assertEquals(excMsg, exc.getMessage());
+      assertTrue(exc.getMessage().startsWith(excMsg));
     }
     endTest();
   }
