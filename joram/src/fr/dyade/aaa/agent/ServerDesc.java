668373ab7b10a087fe2d5f1db71d5c0d9e3eae31
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-05-05 14:50:20 +0000

    Regroup some common classes of 'fr.dyade.aaa.common' to new subpackages 'net', 'monitoring' and 'stream'.

--- joram/src/fr/dyade/aaa/agent/ServerDesc.java
+++ joram/src/fr/dyade/aaa/agent/ServerDesc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2001 - 2010 ScalAgent Distributed Technologies
  * Copyright (C) 2004 - France Telecom R&D
  * Copyright (C) 1996 - 2000 BULL
  * Copyright (C) 1996 - 2000 INRIA
@@ -29,8 +29,8 @@ import java.net.InetAddress;
 import java.util.Enumeration;
 import java.util.Vector;
 
-import fr.dyade.aaa.common.SocketAddress;
 import fr.dyade.aaa.common.Strings;
+import fr.dyade.aaa.common.net.SocketAddress;
 
 /**
  * Description of an agent server. It is used by <code>Channel</code> and
@@ -49,7 +49,7 @@ public final class ServerDesc implements Serializable {
   String name;
   /**
    * The IP address of the server.
-   * It contains hostname and port ({@link fr.dyade.aaa.common.SocketAddress
+   * It contains hostname and port ({@link fr.dyade.aaa.common.net.SocketAddress
    * <code>SocketAddress</code>}) of remote server. The communication port
    * is set only if the server is directly accessible from this node; in
    * this case it corresponds to the communication port of the server in the
