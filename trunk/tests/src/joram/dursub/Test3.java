870e816c7d8beda24fa395838e5327758cee0a4f
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-05-05 09:11:05 +0000

    Adds JavaDoc comments.

--- trunk/tests/src/joram/dursub/Test3.java
+++ trunk/tests/src/joram/dursub/Test3.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2003 - 2010 ScalAgent Distributed Technologies
+ * Copyright (C) 2004 - 2010 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,29 +24,30 @@ package joram.dursub;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.TopicSubscriber;
 
 import org.objectweb.joram.client.jms.Topic;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.User;
 import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
-import fr.dyade.aaa.agent.AgentServer;
+import framework.TestCase;
 
 /**
- * Test durable subscription:
- * - Verify that unsubscribe with a null name throws an exception.
- * - Verify that unsubscribe to an active subscription throws an exception.
- * - Closes the session and verify that unsubscribe is ok.
+ * Test the functioning of DurableSubscriber:
+ *  - Creates a durable subscription then close the connection.
+ *  - Sends a message.
+ *  - Restarts the durable subscriber and receive message.
  */
-public class Test2 extends framework.TestCase {
-  public static void main (String args[]) {
-    new Test2().run();
+public class Test3 extends TestCase  {
+  public static void main(String[] args) {
+    new Test3().run();
   }
-  
-  public void run(){
+
+  public void run() {
     try {
       startAgentServer((short)0);
       Thread.sleep(1000L);
@@ -62,39 +63,48 @@ public class Test2 extends framework.TestCase {
 
       AdminModule.disconnect();
 
+      // Creates a connection for messages producer
       Connection cnx1 = cf.createConnection();
-      cnx1.start();
-      
       Session sess1 = cnx1.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      JMSException exc = null;
-      try{
-        sess1.unsubscribe(null);
-      }catch(JMSException e){
-        exc = e;
-      }
-      assertTrue(exc != null);
-      sess1.close();
-      
-      sess1 = cnx1.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      MessageConsumer cons = sess1.createDurableSubscriber(topic, "dursub1");
-      exc = null;
-      try{
-        sess1.unsubscribe("dursub1");
-      }catch(JMSException e){
-        exc = e;
-      }
-      assertTrue(exc != null);
-      cons.close();
+      MessageProducer prod = sess1.createProducer(topic);
+      cnx1.start();
 
-      sess1.unsubscribe("dursub1");
+      // Creates a connection and initializes the durable subscriber
+      Connection cnx2 = cf.createConnection();
+      Session sess2 = cnx2.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      TopicSubscriber cons = sess2.createDurableSubscriber(topic, "dursub");  
+      cnx2.start();
+
+      // close connection of subscriber
+      cnx2.close();
       
+      // send a message
+      TextMessage msg = sess1.createTextMessage();
+      msg.setText("message_text");
+      prod.send(msg);
       cnx1.close();
+      
+      // reconnection of subscribe
+      cnx2 = cf.createConnection();
+      sess2 = cnx2.createSession(false,Session.AUTO_ACKNOWLEDGE);
+      cons = sess2.createDurableSubscriber(topic, "dursub");
+      cnx2.start();
+      // and receive the message
+      TextMessage msg2 = (TextMessage) cons.receiveNoWait();
+      cnx2.close();
+      
+      assertTrue(msg2 != null);
+      if (msg2 != null) {
+        assertEquals(msg.getJMSMessageID(), msg2.getJMSMessageID());
+        assertEquals(msg.getText(), msg2.getText());
+      }
     } catch (Throwable exc) {
       exc.printStackTrace();
       error(exc);
-    } finally{
+    } finally {
       stopAgentServer((short)0);
       endTest(); 
     }
   }
 }
+
