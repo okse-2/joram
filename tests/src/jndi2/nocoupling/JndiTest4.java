f2b081d88637203522876eaafaa4ac943bd27be5
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-05-12 08:23:57 +0000

    Add sleep time to let the server start. Code cleaning.

--- tests/src/jndi2/nocoupling/JndiTest4.java
+++ tests/src/jndi2/nocoupling/JndiTest4.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 2001 - 2010 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,6 @@
  * Initial developer(s): (ScalAgent D.T.)
  * Contributor(s): Badolle Fabien (ScalAgent D.T.)
  */
- 
 package jndi2.nocoupling;
 
 import java.util.Hashtable;
@@ -31,14 +30,16 @@ import javax.naming.NamingEnumeration;
 
 import framework.TestCase;
 
-
 /**
  * Tests: Simple test with 2 servers
- *    - bind and createsubcontext
- *    - rebind, unbind, destroysubcontext
- *
+ * <ul>
+ * <li>bind and create subcontext</li>
+ * <li>rebind, unbind, destroy subcontext
+ * <li>
+ * </ul>
  */
 public class JndiTest4 extends TestCase {
+
   public static String NAMING_FACTORY_PROP = "java.naming.factory.initial";
   public static String NAMING_FACTORY = "fr.dyade.aaa.jndi2.client.NamingContextFactory";
   public static String NAMING_HOST_PROP = "java.naming.factory.host";
@@ -51,99 +52,97 @@ public class JndiTest4 extends TestCase {
 
   public void run() {
     try {
-	startAgentServer(
-			 (short)0, new String[]{"-DTransaction=fr.dyade.aaa.util.NTransaction"});
-	startAgentServer(
-			 (short)1, new String[]{"-DTransaction=fr.dyade.aaa.util.NTransaction"});      
-	
-		startAgentServer(
-			 (short)2, new String[]{"-DTransaction=fr.dyade.aaa.util.NTransaction"}); 
-	
-	Hashtable env1 = new Hashtable();
-	env1.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
-	env1.put(NAMING_HOST_PROP, LOCALHOST);
-	env1.put(NAMING_PORT_PROP, "16401");
-	
-	Hashtable env0 = new Hashtable();
-	env0.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
-	env0.put(NAMING_HOST_PROP, LOCALHOST);
-	env0.put(NAMING_PORT_PROP, "16400");
+      startAgentServer((short) 0, new String[] { "-DTransaction=fr.dyade.aaa.util.NTransaction" });
+      startAgentServer((short) 1, new String[] { "-DTransaction=fr.dyade.aaa.util.NTransaction" });
+      startAgentServer((short) 2, new String[] { "-DTransaction=fr.dyade.aaa.util.NTransaction" });
 
-		Hashtable env2 = new Hashtable();
-	env2.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
-	env2.put(NAMING_HOST_PROP, LOCALHOST);
-	env2.put(NAMING_PORT_PROP, "16402");
-	
-	InitialContext ctx1 = new InitialContext(env1);
-	InitialContext ctx0 = new InitialContext(env0);
-		InitialContext ctx2 = new InitialContext(env2);
+      Thread.sleep(3000);
 
+      Hashtable env0 = new Hashtable();
+      env0.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
+      env0.put(NAMING_HOST_PROP, LOCALHOST);
+      env0.put(NAMING_PORT_PROP, "16400");
 
-	Exception excp=null;
-	try{
-	     ctx0.bind("/B","B");
-	     // ctx0.bind("/C","C");
-	     // ctx0.bind("/D","D");
+      Hashtable env1 = new Hashtable();
+      env1.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
+      env1.put(NAMING_HOST_PROP, LOCALHOST);
+      env1.put(NAMING_PORT_PROP, "16401");
 
-	     // ctx0.createSubcontext("/A");
-	     // ctx0.bind("/A/C","C");
-	    Thread.sleep(2000);
-	    ctx1.bind("/D","D");
-	    // ctx1.createSubcontext("/A/E");
-	    // ctx1.bind("/A/E/S","S");
-	     Thread.sleep(2000);
-	}catch(Exception exc){
-	    excp=exc;
-	}
+      Hashtable env2 = new Hashtable();
+      env2.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
+      env2.put(NAMING_HOST_PROP, LOCALHOST);
+      env2.put(NAMING_PORT_PROP, "16402");
 
+      InitialContext ctx0 = new InitialContext(env0);
+      InitialContext ctx1 = new InitialContext(env1);
+      InitialContext ctx2 = new InitialContext(env2);
 
-	NamingEnumeration namingEnumeration = ctx0.listBindings("");
-	while (namingEnumeration.hasMoreElements()) {
-	    // NameClassPair classPair = (NameClassPair) namingEnumeration.next(); 
-	    Binding classPair = (Binding) namingEnumeration.next();
-	    
-	    System.out.println("zz "+classPair.getName());
-	}
+      ctx0.bind("/B", "B");
+      // ctx0.bind("/C","C");
+      // ctx0.bind("/D","D");
 
-	//listBindings
-	namingEnumeration = ctx1.listBindings("");
-	while (namingEnumeration.hasMoreElements()) {
-	    // NameClassPair classPair = (NameClassPair) namingEnumeration.next();
- Binding classPair = (Binding) namingEnumeration.next();
-	    System.out.println("zz1 "+classPair.getName());
-	}
+      // ctx0.createSubcontext("/A");
+      // ctx0.bind("/A/C","C");
+      Thread.sleep(2000);
+      ctx1.bind("/D", "D");
+      // ctx1.createSubcontext("/A/E");
+      // ctx1.bind("/A/E/S","S");
+      Thread.sleep(2000);
 
-		namingEnumeration = ctx2.listBindings("");
-	while (namingEnumeration.hasMoreElements()) {
-	    //  NameClassPair classPair = (NameClassPair) namingEnumeration.next();
- Binding classPair = (Binding) namingEnumeration.next();
-	    System.out.println("zz2 "+classPair.getName());
-	}
-	
+      NamingEnumeration namingEnumeration = ctx0.listBindings("");
+      int count = 0;
+      while (namingEnumeration.hasMoreElements()) {
+        // NameClassPair classPair = (NameClassPair) namingEnumeration.next(); 
+        Binding classPair = (Binding) namingEnumeration.next();
+        System.out.println("zz " + classPair.getName());
+        count++;
+      }
+      assertEquals(2, count);
 
+      //listBindings
+      count = 0;
+      namingEnumeration = ctx1.listBindings("");
+      while (namingEnumeration.hasMoreElements()) {
+        // NameClassPair classPair = (NameClassPair) namingEnumeration.next();
+        Binding classPair = (Binding) namingEnumeration.next();
+        System.out.println("zz1 " + classPair.getName());
+        count++;
+      }
+      assertEquals(2, count);
 
+      namingEnumeration = ctx2.listBindings("");
+      count = 0;
+      while (namingEnumeration.hasMoreElements()) {
+        //  NameClassPair classPair = (NameClassPair) namingEnumeration.next();
+        Binding classPair = (Binding) namingEnumeration.next();
+        System.out.println("zz2 " + classPair.getName());
+        count++;
+      }
+      assertEquals(2, count);
 
-	/*	String look = (String) ctx1.lookup("/A/C");
-	assertEquals("C",look);
-	System.out.println(look);
-	look = (String) ctx0.lookup("/B");
-	assertEquals("B",look);
-	System.out.println(look);*/
-	System.out.println("End test4");
-	
-    } catch (Exception exc) {   
-	exc.printStackTrace();
-	error(exc);      
+      /*
+       * String look = (String) ctx1.lookup("/A/C");
+       * assertEquals("C",look);
+       * System.out.println(look);
+       * look = (String) ctx0.lookup("/B");
+       * assertEquals("B",look);
+       * System.out.println(look);
+       */
+      System.out.println("End test4");
+
+    } catch (Exception exc) {
+      exc.printStackTrace();
+      error(exc);
     } finally {
-	stopAgentServer((short)0);
-	stopAgentServer((short)1);
-	stopAgentServer((short)2);
+      stopAgentServer((short) 0);
+      stopAgentServer((short) 1);
+      stopAgentServer((short) 2);
 
-	endTest();
+      endTest();
     }
   }
-    
-    public static void main(String args[]) {
-	new JndiTest4().run();
-    }
+
+  public static void main(String args[]) {
+    new JndiTest4().run();
+  }
 }
