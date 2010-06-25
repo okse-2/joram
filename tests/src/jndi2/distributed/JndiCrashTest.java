ac96c1f568283b8e1ba5a269743c3f3442c8a3a5
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2010-06-25 11:45:45 +0000

    change jndi port.

--- tests/src/jndi2/distributed/JndiCrashTest.java
+++ tests/src/jndi2/distributed/JndiCrashTest.java
@@ -76,19 +76,19 @@ public class JndiCrashTest extends TestCase {
       Hashtable env0 = new Hashtable();
       env0.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
       env0.put(NAMING_HOST_PROP, LOCALHOST);
-      env0.put(NAMING_PORT_PROP, "16400");
+      env0.put(NAMING_PORT_PROP, "16600");
       InitialContext ctx0 = new InitialContext(env0);
       
       Hashtable env1 = new Hashtable();
       env1.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
       env1.put(NAMING_HOST_PROP, LOCALHOST);
-      env1.put(NAMING_PORT_PROP, "16401");
+      env1.put(NAMING_PORT_PROP, "16601");
       InitialContext ctx1 = new InitialContext(env1);
       
       Hashtable env2 = new Hashtable();
       env2.put(NAMING_FACTORY_PROP, NAMING_FACTORY);
       env2.put(NAMING_HOST_PROP, LOCALHOST);
-      env2.put(NAMING_PORT_PROP, "16402");
+      env2.put(NAMING_PORT_PROP, "16602");
       InitialContext ctx2 = new InitialContext(env2);
       
       ctx0.bind("/A", "A");
