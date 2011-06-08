8a3700a7f9a1462f08511a82960ec8634dea17fc
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-06-08 11:36:54 +0000

    Update performance sample.

--- trunk/joram/samples/src/joram/perfs/PerfsAdmin.java
+++ trunk/joram/samples/src/joram/perfs/PerfsAdmin.java
@@ -34,21 +34,20 @@ import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 public class PerfsAdmin {
 
   public static void main(String[] args) throws Exception {
-    
-    AdminModule.connect("root", "root", 60);
-
-    Queue queue = Queue.create(0);
-    Topic topic = Topic.create(0);
-
     javax.jms.ConnectionFactory cf =
       TcpConnectionFactory.create("localhost", 16010);
+    
+    AdminModule.connect(cf, "root", "root");
+
+    Queue queue = Queue.create(0);
+    queue.setFreeReading();
+    queue.setFreeWriting();
+    
+    Topic topic = Topic.create(0);
+    topic.setFreeReading();
+    topic.setFreeWriting();
 
     User.create("anonymous", "anonymous", 0);
-
-    queue.setFreeReading();
-    topic.setFreeReading();
-    queue.setFreeWriting();
-    topic.setFreeWriting();
 
     javax.naming.Context jndiCtx = new javax.naming.InitialContext();
     jndiCtx.bind("cf", cf);
