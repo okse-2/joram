b4d588b631ff156eff5c9f9518cba0916458de22
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-06-15 15:06:11 +0000

    Reworked message deliveries.

--- trunk/mom-amqp/org.ow2.joram.mom.amqp/src/main/java/org/ow2/joram/mom/amqp/structures/GetDeliveries.java
+++ trunk/mom-amqp/org.ow2.joram.mom.amqp/src/main/java/org/ow2/joram/mom/amqp/structures/GetDeliveries.java
@@ -1,32 +1,40 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2009 - 2011 ScalAgent Distributed Technologies
+ * Copyright (C) 2011 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA.
  *
  * Initial developer(s): ScalAgent Distributed Technologies
- * Contributor(s): 
+ * Contributor(s):
  */
-package org.ow2.joram.mom.amqp;
+package org.ow2.joram.mom.amqp.structures;
 
 /**
- * Interface used for message deliveries.
+ * A {@link GetDeliveries} is sent to the proxy each time the proxy is
+ * susceptible to have new deliveries to transmit to the client.
  */
-public interface DeliveryListener {
+public class GetDeliveries {
 
-  public boolean deliver(String consumerTag, int channelNumber, Queue queue, short serverId, long proxyId);
+  public String consumerTag;
+
+  public int channelId;
+
+  public GetDeliveries(String consumerTag, int channelId) {
+    this.consumerTag = consumerTag;
+    this.channelId = channelId;
+  }
 
 }
