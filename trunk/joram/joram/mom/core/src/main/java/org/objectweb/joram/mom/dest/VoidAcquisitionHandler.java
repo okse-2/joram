0a5d511f473a54976b7f1db40a652f9e84497e6a
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-07-12 14:00:52 +0000

    Remove @Override annotation (not compatible with Java 5 when overriding interfaces). Minor fixes. Correct indentation.

--- trunk/joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/VoidAcquisitionHandler.java
+++ trunk/joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/VoidAcquisitionHandler.java
@@ -10,42 +10,20 @@ import java.util.Properties;
  * An acquisition handler that does nothing and can be used, for instance to
  * configure an alias acquisition destination.
  */
-public class VoidAcquisitionHandler implements AcquisitionHandler
-{
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.objectweb.joram.mom.dest.AcquisitionHandler#retrieve(org.objectweb
-	 * .joram.mom.dest.ReliableTransmitter)
-	 */
-	@Override
-	public void retrieve(ReliableTransmitter transmitter) throws Exception
-	{
-		// Nothing to do
-	}
+public class VoidAcquisitionHandler implements AcquisitionHandler {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.objectweb.joram.mom.dest.AcquisitionHandler#setProperties(java.util
-	 * .Properties)
-	 */
-	@Override
-	public void setProperties(Properties properties)
-	{
-		// Nothing to do
-	}
+  /** @see AcquisitionHandler#retrieve(ReliableTransmitter) */
+  public void retrieve(ReliableTransmitter transmitter) throws Exception {
+    // Nothing to do
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.objectweb.joram.mom.dest.AcquisitionHandler#close()
-	 */
-	@Override
-	public void close()
-	{
-		// Nothing to do
-	}
+  /** @see AcquisitionHandler#setProperties(Properties) */
+  public void setProperties(Properties properties) {
+    // Nothing to do
+  }
+
+  /** @see AcquisitionHandler#close() */
+  public void close() {
+    // Nothing to do
+  }
 }
