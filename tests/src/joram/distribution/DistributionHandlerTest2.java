ddbac0b76e6d9278454be88a970c9337d2e57c59
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-07-12 13:23:31 +0000

    Add distribution tests.

---
+++ tests/src/joram/distribution/DistributionHandlerTest2.java
@@ -0,0 +1,65 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2011 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
+package joram.distribution;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.objectweb.joram.mom.dest.DistributionHandler;
+import org.objectweb.joram.shared.messages.Message;
+
+public class DistributionHandlerTest2 implements DistributionHandler {
+
+  private static List messages = new ArrayList();
+
+  public void close() {
+  }
+
+  public void distribute(Message msg) throws Exception {
+    // Block odd messages during some retries, to see
+    // differences with batch mode: even messages will
+    // quickly arrive before odd messages
+    int number = Integer.parseInt(msg.getText());
+    if (number % 2 == 1) {
+      if (msg.deliveryCount < 3) {
+        throw new Exception("Message could not be delivered.");
+      }
+    }
+    addMessage(msg);
+  }
+
+  public void init(Properties props) {
+  }
+
+  private static synchronized void addMessage(Message msg) {
+    messages.add(msg);
+  }
+
+  public static synchronized List getAllMessages() {
+    List result = messages;
+    messages = new ArrayList();
+    return result;
+  }
+
+}
