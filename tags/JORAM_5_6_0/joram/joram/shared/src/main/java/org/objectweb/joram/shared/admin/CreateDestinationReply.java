b55d2bf8f89c4b3292d9adbe68d5f69c3fd90a16
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-07-13 13:08:32 +0000

    Joram 5.6.0

---
+++ tags/JORAM_5_6_0/joram/joram/shared/src/main/java/org/objectweb/joram/shared/admin/CreateDestinationReply.java
@@ -0,0 +1,97 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies
+ * Copyright (C) 1996 - 2000 Dyade
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): Frederic Maistre (INRIA)
+ * Contributor(s): ScalAgent Distributed Technologies
+ */
+package org.objectweb.joram.shared.admin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import fr.dyade.aaa.common.stream.StreamUtil;
+
+/**
+ * A <code>CreateDestinationReply</code> instance replies to a
+ * destination creation request, produced by the AdminTopic.
+ */
+public class CreateDestinationReply extends AdminReply {
+  private static final long serialVersionUID = 1L;
+
+  /** Identifier of the created destination. */
+  private String id;
+
+  private String name;
+
+  // TODO (AF): No longer needed, the destination type is specified in the request,
+  // it must be the same in the reply otherwise an error must be returned !!
+  
+//  private byte type;
+
+  /**
+   * Constructs a <code>CreateDestinationReply</code> instance.
+   *
+   * @param id  The id of the created destination.
+   * @param info  Related information.
+   */
+  public CreateDestinationReply(String id, String name, String info) {
+    super(true, info);
+    this.id = id;
+    this.name = name;
+  }
+
+  public CreateDestinationReply() { }
+  
+  /** Returns the id of the created queue. */
+  public final String getId() {
+    return id;
+  }
+
+  public final String getName() {
+    return name;
+  }
+
+//  public final byte getType() {
+//    return type;
+//  }
+
+  public String toString() {
+    return '(' + super.toString() + ",id=" + id + ",name=" + name + ')';
+  }
+  
+  protected int getClassId() {
+    return CREATE_DESTINATION_REPLY;
+  }
+  
+  public void readFrom(InputStream is) throws IOException {
+    super.readFrom(is);
+    id = StreamUtil.readStringFrom(is);
+    name = StreamUtil.readStringFrom(is);
+//    type = StreamUtil.readByteFrom(is);
+  }
+
+  public void writeTo(OutputStream os) throws IOException {
+    super.writeTo(os);
+    StreamUtil.writeTo(id, os);
+    StreamUtil.writeTo(name, os);
+//    StreamUtil.writeTo(type, os);
+  }
+}
---
+++ tags/JORAM_5_6_0/joram/joram/shared/src/main/java/org/objectweb/joram/shared/admin/GetSubscriptionsRep.java
@@ -0,0 +1,90 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2003 - 2007 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s):
+ */
+package org.objectweb.joram.shared.admin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import fr.dyade.aaa.common.stream.StreamUtil;
+
+public class GetSubscriptionsRep extends AdminReply {
+  private static final long serialVersionUID = 1L;
+
+  private String[] subNames;
+
+  private String[] topicIds;
+
+  private int[] messageCounts;
+
+  private boolean[] durable;
+
+  public GetSubscriptionsRep(String[] subNames,
+                             String[] topicIds,
+                             int[] messageCounts,
+                             boolean[] durable) {
+    super(true, null);
+    this.subNames = subNames;
+    this.topicIds = topicIds;
+    this.messageCounts = messageCounts;
+    this.durable = durable;
+  }
+
+  public GetSubscriptionsRep() { }
+  
+  public final String[] getSubNames() {
+    return subNames;
+  }
+
+  public final String[] getTopicIds() {
+    return topicIds;
+  }
+
+  public final int[] getMessageCounts() {
+    return messageCounts;
+  }
+
+  public final boolean[] getDurable() {
+    return durable;
+  }
+  
+  protected int getClassId() {
+    return GET_SUBSCRIPTIONS_REP;
+  }
+    
+  public void readFrom(InputStream is) throws IOException {
+    super.readFrom(is);
+    subNames = StreamUtil.readArrayOfStringFrom(is);
+    topicIds = StreamUtil.readArrayOfStringFrom(is);
+    messageCounts = StreamUtil.readArrayOfIntFrom(is);
+    durable = StreamUtil.readArrayOfBooleanFrom(is);
+  }
+
+  public void writeTo(OutputStream os) throws IOException {
+    super.writeTo(os);
+    StreamUtil.writeArrayOfStringTo(subNames, os);
+    StreamUtil.writeArrayOfStringTo(topicIds, os);
+    StreamUtil.writeArrayOfIntTo(messageCounts, os);
+    StreamUtil.writeArrayOfBooleanTo(durable, os);
+  }
+}
