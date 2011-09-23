970e3f4d9538375f7db85c439c7e8c5c621f837c
surrel <surrel@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2011-09-23 08:42:17 +0000

    Use generics for the Map, to fix warnings about GWT serialization.

--- trunk/webconsole/admin/src/main/java/com/scalagent/appli/shared/MessageWTO.java
+++ trunk/webconsole/admin/src/main/java/com/scalagent/appli/shared/MessageWTO.java
@@ -39,7 +39,7 @@ public class MessageWTO extends BaseWTO {
   private String text;
   private int type;
 
-  private Map properties;
+  private Map<String, String> properties;
 
   public long getExpiration() {
     return expiration;
@@ -65,7 +65,7 @@ public class MessageWTO extends BaseWTO {
     return type;
   }
 
-  public Map getProperties() {
+  public Map<String, String> getProperties() {
     return properties;
   }
 
@@ -93,7 +93,7 @@ public class MessageWTO extends BaseWTO {
     this.type = type;
   }
 
-  public void setProperties(Map properties) {
+  public void setProperties(Map<String, String> properties) {
     this.properties = properties;
   }
 
@@ -101,7 +101,7 @@ public class MessageWTO extends BaseWTO {
   }
 
   public MessageWTO(String idS, long expiration, long timestamp, int deliveryCount, int priority,
-      String text, int type, Map properties) {
+      String text, int type, Map<String, String> properties) {
     super();
     this.id = idS;
     this.expiration = expiration;
