5e32d80da88d2fc512f3a32d9931e0dcba2db17d
elrhedda <elrhedda@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-06-05 09:28:16 +0000

    - Added new experiments' results to Elastic Joram.

--- elastic/java/src/alias/RegulatedSender.java
+++ elastic/java/src/alias/RegulatedSender.java
@@ -31,23 +31,39 @@ public class RegulatedSender {
 	static Context ictx = null;
 
 	public static int computeLoad(int round) {
-		/*int unit = 90; 
-		if (round < 100)
-			return unit;
 		if (round < 200)
-			return unit*2;
-		if (round < 300)
-			return unit*3;
-		if (round < 400)
-			return unit*2;
-		
-		return unit;*/
-		if (round < 600)
 			return round / 2;
-		if (round < 800)
+		if (round < 400)
+			return 100;
+		if (round < 500)
+			return 100 + (round - 400);
+		if (round < 700)
+			return 200;
+		if (round < 750)
+			return 200 + 2*(round - 700);
+		if (round < 950)
 			return 300;
+		if (round < 1000)
+			return 300 - 2*(round - 950);
+		if (round < 1200)
+			return 200;
+		if (round < 1300)
+			return 200 - (round - 1200);
+		if (round < 1500)
+			return 100;
+		if (round < 1600)
+			return 100 + (round - 1500);
+		if (round < 1800)
+			return 200;
+		if (round < 1900)
+			return 200 - (round - 1800);
+		if (round < 2100)
+			return 100;
+		if (round < 2300)
+			return 100 - (round - 2100) / 2;
 		
-		return 300 - (round - 800) / 2; 
+		return 0;
+			
 	}
 	
 	public static void main(String argv[]) throws Exception {
@@ -74,15 +90,15 @@ public class RegulatedSender {
 		int load;
 		cnx.start();
 		start = System.currentTimeMillis();
-		for(int i = 0; i < 1400; i++) {
+		for(int i = 0; i < 2300; i++) {
 			load = computeLoad(i);
 			for(int j = 0; j < load; j++) {
 				sender.send(message);
 				if ((j % 10) == 9)
 					session.commit();
 			}
-			System.out.println("[RegulatedSender]\t" + i);
-			wait = start + Constants.TIME_UNIT*i - System.currentTimeMillis();
+			System.out.println("[RegulatedSender]\t" + i + "\t" + load);
+			wait = start + Constants.TIME_UNIT*(i+1) - System.currentTimeMillis();
 			if (wait > 0)
 				Thread.sleep(wait);
 		}
