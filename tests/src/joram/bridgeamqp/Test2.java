0bd9b6435c36db32f0975d43c5bf1cc5e925b6d2
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-06-21 14:20:13 +0000

    Adds AMQP bridge tests.

---
+++ tests/src/joram/bridgeamqp/Test2.java
@@ -0,0 +1,246 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2012 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
+package joram.bridgeamqp;
+
+import java.util.Properties;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.objectweb.joram.client.jms.Queue;
+import org.objectweb.joram.client.jms.admin.AdminModule;
+import org.objectweb.joram.client.jms.admin.User;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
+import org.objectweb.joram.mom.dest.amqp.AmqpAcquisition;
+import org.objectweb.joram.mom.dest.amqp.AmqpDistribution;
+
+import framework.TestCase;
+
+/**
+ * Test: Use 2 bridge queues to send and receive messages through a foreign AMQP queue.
+ *       Test the bridge behavior with kill and start of the Joram bridge server.
+ *  - Sends 10 messages.
+ *  - Receives 10 messages.
+ *  - Kills then restarts the bridge server.
+ *  - Sends 10 messages.
+ *  - Receives 10 messages.
+ *  - Sends 10 messages.
+ *  - Kills then restarts the bridge server.
+ *  - Receives the waiting 10 messages.
+ */
+public class Test2 extends TestCase {
+
+  public static void main(String[] args) {
+    new Test2().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("servers start");
+      startAgentServer((short)0, new String[]{"-DTransaction.UseLockFile=false"});
+      Thread.sleep(1000);
+      
+      // Administration code
+      
+      {
+        boolean async = Boolean.getBoolean("async");
+        System.out.println("async=" + async);
+
+        javax.jms.ConnectionFactory joramCF = TcpConnectionFactory.create("localhost", 16010);
+
+        AdminModule.connect(joramCF, "root", "root");
+        javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+
+        User.create("anonymous", "anonymous", 0);
+
+        // Setting the bridge properties
+        Properties prop1 = new Properties();
+        prop1.setProperty("acquisition.className", AmqpAcquisition.class.getName());
+        prop1.setProperty("amqp.QueueName", "amqpQueue");
+        prop1.setProperty("amqp.Queue.DeclarePassive", "false");
+        prop1.setProperty("amqp.Queue.DeclareExclusive", "false");
+        prop1.setProperty("amqp.Queue.DeclareDurable", "true");
+        prop1.setProperty("amqp.Queue.DeclareAutoDelete", "false");
+        prop1.setProperty("amqp.ConnectionUpdatePeriod", "1000");
+        
+        Queue joramInQueue = Queue.create(0, "queue", Queue.ACQUISITION_QUEUE, prop1);
+        joramInQueue.setFreeReading();
+        joramInQueue.setFreeWriting();
+        System.out.println("joramInQueue = " + joramInQueue);
+
+        // Setting the bridge properties
+        Properties prop2 = new Properties();
+        prop2.setProperty("distribution.className", AmqpDistribution.class.getName());
+        prop2.setProperty("amqp.QueueName", "amqpQueue");
+        prop2.setProperty("amqp.Queue.DeclarePassive", "false");
+        prop2.setProperty("amqp.Queue.DeclareExclusive", "false");
+        prop2.setProperty("amqp.Queue.DeclareDurable", "true");
+        prop2.setProperty("amqp.Queue.DeclareAutoDelete", "false");
+        prop2.setProperty("amqp.ConnectionUpdatePeriod", "1000");
+        
+        prop2.setProperty("period", "1000");      
+        prop2.put("distribution.async", "" + async);
+
+        Queue joramOutQueue = Queue.create(0, "BridgeOutQueue", Queue.DISTRIBUTION_QUEUE, prop2);
+        joramOutQueue.setFreeWriting();
+        System.out.println("joramOutQueue = " + joramOutQueue);
+
+        jndiCtx.rebind("joramInQueue", joramInQueue);
+        jndiCtx.rebind("joramOutQueue", joramOutQueue);
+        jndiCtx.rebind("joramCF", joramCF);
+
+        jndiCtx.close();
+
+        AdminModule.disconnect();
+        System.out.println("Admin closed.");
+      }
+
+      System.out.println("admin config ok");
+      Thread.sleep(1000);
+
+      // Gets administered objects from JNDI
+      javax.naming.Context jndiCtx = new javax.naming.InitialContext();  
+      Destination joramInDest = (Destination) jndiCtx.lookup("joramInQueue");
+      Destination joramOutDest = (Destination) jndiCtx.lookup("joramOutQueue");
+      ConnectionFactory joramCF = (ConnectionFactory) jndiCtx.lookup("joramCF");
+      jndiCtx.close();
+      
+      // Creates needed Connection and Session objects
+      Connection joramCnx = joramCF.createConnection();
+      Session joramSess = joramCnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer joramCons = joramSess.createConsumer(joramInDest);
+      MessageProducer joramProd = joramSess.createProducer(joramOutDest);
+      joramCnx.start(); 
+
+      // Run the simple test: send 10 messages then receive it.
+      TextMessage msgOut = joramSess.createTextMessage();
+      for (int i = 1; i < 11; i++) {
+        msgOut.setText("Message number " + i);
+        System.out.println("send msg = " + msgOut.getText());
+        joramProd.send(msgOut);
+      }
+
+      int nbmsg = 0;
+      TextMessage msgIn;
+      for (int i = 1; i < 11; i++) { 
+        msgIn=(TextMessage) joramCons.receive(5000);
+        if (msgIn != null) {
+          nbmsg += 1;
+        } else {
+          assertTrue("Message not received", false);
+          break;
+        }
+        System.out.println("receive msg = " + msgIn.getText());
+        assertEquals("Message number " + i, msgIn.getText());
+      }
+      assertEquals(10, nbmsg);
+
+      // TODO (AF): This sleep is needed to avoid message replication. This issue
+      // needs to be study.
+      Thread.sleep(500);
+      System.out.println("Kills the bridge server.");
+      killAgentServer((short) 0);
+      Thread.sleep(1000);
+      startAgentServer((short) 0, new String[]{"-DTransaction.UseLockFile=false"});
+      System.out.println("Bridge server started.");
+      Thread.sleep(1000);
+      
+      // Creates needed Connection and Session objects
+      joramCnx = joramCF.createConnection();
+      joramSess = joramCnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      joramCons = joramSess.createConsumer(joramInDest);
+      joramProd = joramSess.createProducer(joramOutDest);
+      joramCnx.start(); 
+
+      // Run the simple test: send 10 messages then receive it.
+      msgOut = joramSess.createTextMessage();
+      for (int i = 11; i < 21; i++) {
+        msgOut.setText("Message number " + i);
+        System.out.println("send msg = " + msgOut.getText());
+        joramProd.send(msgOut);
+      }
+
+      for (int i = 11; i < 21; i++) { 
+        msgIn=(TextMessage) joramCons.receive(5000);
+        if (msgIn != null) {
+          nbmsg += 1;
+        } else {
+          assertTrue("Message not received", false);
+          break;
+        }
+        System.out.println("receive msg = " + msgIn.getText());
+        assertEquals("Message number " + i, msgIn.getText());
+      }
+      assertEquals(20, nbmsg);
+
+      // Send 10 messages then receive it after server restart.
+      msgOut = joramSess.createTextMessage();
+      for (int i = 21; i < 31; i++) {
+        msgOut.setText("Message number " + i);
+        System.out.println("send msg = " + msgOut.getText());
+        joramProd.send(msgOut);
+      }
+
+      System.out.println("Kills the bridge server.");
+      killAgentServer((short) 0);
+      Thread.sleep(1000);
+      startAgentServer((short) 0, new String[]{"-DTransaction.UseLockFile=false"});
+      System.out.println("Bridge server started.");
+      Thread.sleep(1000);
+      
+      // Creates needed Connection and Session objects
+      joramCnx = joramCF.createConnection();
+      joramSess = joramCnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      joramCons = joramSess.createConsumer(joramInDest);
+      joramProd = joramSess.createProducer(joramOutDest);
+      joramCnx.start(); 
+
+      // Receive the waiting messages.
+      for (int i = 21; i < 31; i++) { 
+        msgIn=(TextMessage) joramCons.receive(5000);
+        if (msgIn != null) {
+          nbmsg += 1;
+        } else {
+          assertTrue("Message not received", false);
+          break;
+        }
+        System.out.println("receive msg = " + msgIn.getText());
+        assertEquals("Message number " + i, msgIn.getText());
+      }
+      assertEquals(30, nbmsg);
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      killAgentServer((short)0);
+      endTest(); 
+    }
+  }
+}
+
---
+++ tests/src/joram/bridgeamqp/Test4.java
@@ -0,0 +1,188 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2012 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
+package joram.bridgeamqp;
+
+import java.util.Properties;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.objectweb.joram.client.jms.Queue;
+import org.objectweb.joram.client.jms.admin.AdminModule;
+import org.objectweb.joram.client.jms.admin.User;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
+import org.objectweb.joram.mom.dest.amqp.AmqpAcquisition;
+
+import framework.TestCase;
+
+/**
+ * Test: Producer on AMQP server, Consumer on Joram through AMQP acquisition queue.
+ *  - Sends 10.000 messages on AMQP queue.
+ *  - Receives 10.000 messages on Joram AMQP acquisition queue.
+ */
+public class Test4 extends TestCase {
+  public static boolean debug = false;
+  public static int nbloop = 10;
+  
+  public static void main(String[] args) {
+    new Test4().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("servers start");
+      startAgentServer((short)0, new String[]{"-DTransaction.UseLockFile=false"});
+      Thread.sleep(1000);
+      
+      debug = Boolean.getBoolean("DEBUG");
+      nbloop = Integer.getInteger("nbloop", nbloop);
+      
+      // Administration code
+      {
+        boolean async = Boolean.getBoolean("async");
+        System.out.println("async=" + async);
+
+        javax.jms.ConnectionFactory joramCF = TcpConnectionFactory.create("localhost", 16010);
+        ((TcpConnectionFactory) joramCF).getParameters().connectingTimer = 60;
+
+        AdminModule.connect(joramCF, "root", "root");
+        javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+
+        User.create("anonymous", "anonymous", 0);
+
+        // Setting the bridge properties
+        Properties prop1 = new Properties();
+        prop1.setProperty("acquisition.className", AmqpAcquisition.class.getName());
+        prop1.setProperty("amqp.QueueName", "amqpQueue");
+        prop1.setProperty("amqp.Queue.DeclarePassive", "false");
+        prop1.setProperty("amqp.Queue.DeclareExclusive", "false");
+        prop1.setProperty("amqp.Queue.DeclareDurable", "true");
+        prop1.setProperty("amqp.Queue.DeclareAutoDelete", "false");
+        prop1.setProperty("amqp.ConnectionUpdatePeriod", "1000");
+
+        Queue joramInQueue = Queue.create(0, "queue", Queue.ACQUISITION_QUEUE, prop1);
+        joramInQueue.setFreeReading();
+        joramInQueue.setFreeWriting();
+        System.out.println("joramInQueue = " + joramInQueue);
+
+        jndiCtx.rebind("joramInQueue", joramInQueue);
+        jndiCtx.rebind("joramCF", joramCF);
+
+        jndiCtx.close();
+
+        AdminModule.disconnect();
+      }
+      System.out.println("admin config ok");
+      Thread.sleep(1000);
+      
+      AMQPSender sender = new AMQPSender("amqpQueue", true, 1000, 0);
+      new Thread(sender).start();
+      
+      // Gets administered objects from JNDI
+      javax.naming.Context jndiCtx = new javax.naming.InitialContext();  
+      Destination joramInDest = (Destination) jndiCtx.lookup("joramInQueue");
+      ConnectionFactory joramCF = (ConnectionFactory) jndiCtx.lookup("joramCF");
+      jndiCtx.close();
+      
+      // Creates needed Connection and Session objects
+      Connection joramCnx = joramCF.createConnection();
+      Session joramSess = joramCnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer joramCons = joramSess.createConsumer(joramInDest);
+      joramCnx.start();
+      
+      TextMessage msgIn;
+      for (int i=0; i<1000; i++) { 
+        msgIn = (TextMessage) joramCons.receive(5000);
+        if (msgIn != null) {
+          if (debug) System.out.println("receive msg = " + msgIn.getText());
+          assertEquals("Message number " + i, msgIn.getText());
+        }
+      }      
+      joramCnx.close();
+  
+      for (int i=0; i<nbloop; i++) {
+        test(1000, 0);
+        if (failureCount()+errorCount() != 0) break;
+      }
+      
+//      test(1000, 0);
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      killAgentServer((short)0);
+      endTest(); 
+    }
+  }
+  
+
+  final static long timeout = 1L;
+  final static Object lock = new Object();
+  
+  void test(final int msgs, int first) throws Exception {
+    // Gets administered objects from JNDI
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();  
+    Destination joramInDest = (Destination) jndiCtx.lookup("joramInQueue");
+    ConnectionFactory joramCF = (ConnectionFactory) jndiCtx.lookup("joramCF");
+    jndiCtx.close();
+    
+    AMQPSender sender = new AMQPSender("amqpQueue", true, msgs, timeout);
+    new Thread(sender).start();
+    
+    Thread.sleep((3*timeout*msgs)/5);
+    crashAgentServer((short) 0);
+    System.out.println("Joram server stopped.");
+    Thread.sleep(250);
+    startAgentServer((short) 0, new String[]{"-DTransaction.UseLockFile=false"});
+    System.out.println("Joram server started.");
+    
+    // wait all the messages are acquired by the bridge queue.
+    Thread.sleep(1000);
+    
+    // Creates needed Connection and Session objects
+    Connection joramCnx = joramCF.createConnection();
+    Session joramSess = joramCnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    MessageConsumer joramCons = joramSess.createConsumer(joramInDest);
+    joramCnx.start(); 
+
+    int i = 0;
+    TextMessage msgIn;
+    do {
+      msgIn = (TextMessage) joramCons.receive(5000);
+      if (msgIn != null) {
+        if (debug) System.out.println("receive msg#" + (first+i) + " = " + msgIn.getText());
+        assertEquals("Message number " + (first+i), msgIn.getText());
+        i += 1;
+      }
+    } while (msgIn != null);
+    assertEquals(i, msgs);
+    
+    joramCnx.close();
+  }
+}
+
