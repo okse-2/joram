83e3865708be1d4ddc5923370ccce6632a295f75
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-08-24 13:41:09 +0000

    adding custom.test.vtest target to joram tests and providing a working runtest

--- trunk/tests/vtest/runtest.sh
+++ trunk/tests/vtest/runtest.sh
@@ -3,23 +3,30 @@
 # Vtest linux script for JORAM  #
 #################################
 
-#ensure existence of ENV variable VTEST_HOME
+#ensure existence of environment variable VTEST_HOME
 if [ -z "$VTEST_HOME" ]
 then
     echo "variable VTEST_HOME doesn't exist"
-    echo "please set env variable to the Vtest \"runtest\" script path"
+    echo "please set variable VTEST_HOME to the Vtest \"runtest\" script path"
     exit 1
 else
     echo "VTEST_HOME set to $VTEST_HOME"
 fi
 
+#following command suppose that svn check out has been made into path $VTEST_HOME/joram
 cd $VTEST_HOME/joram;
-mvn install;
+
+#installing joram tests using maven
+mvn install > mvn.log;
+
+# launching tests
 cd src;
-ant tests.jms.all;
+echo "on launching ant custom.tests.vtest"
+ant tests.jms.all > ant.log;
+
 mkdir results;
 echo "OK" > results/report.txt;
-zip -r results.zip results;
+jar cf results.zip results;
 
 echo " joram tests are finished";
 exit 0;
