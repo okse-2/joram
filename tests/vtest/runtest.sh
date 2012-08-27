5ef99b2392e2cabe194be4ae4ab357d8b4db61e0
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-08-27 07:19:12 +0000

    including ant check report and custom vtest target and providing a first version of complete runtest.sh

--- tests/vtest/runtest.sh
+++ tests/vtest/runtest.sh
@@ -13,20 +13,37 @@ else
     echo "VTEST_HOME set to $VTEST_HOME"
 fi
 
+date=`date +%x`
+date=`echo $date | sed -e s:/:.:g`
+LOGFILE= $VTEST_HOME/"vtest.log"
+ZIPFILE= $VTEST_HOME/"joram-vtest-$date.zip"
+
 #following command suppose that svn check out has been made into path $VTEST_HOME/joram
 cd $VTEST_HOME/joram;
 
 #installing joram tests using maven
-mvn install > mvn.log;
+mvn install > $LOGFILE;
 
 # launching tests
 cd src;
 echo "on launching ant custom.tests.vtest"
-ant tests.jms.all > ant.log;
+ant custom.tests.vtest > $LOGFILE;
 
 mkdir results;
-echo "OK" > results/report.txt;
-jar cf results.zip results;
+mv $LOGFILE results;
 
-echo " joram tests are finished";
-exit 0;
+# getting test exit code
+if [[ $? -gt 0 ]]; then
+    echo "TEST FAILED !";
+    #save contents when test failed
+    mv $VTEST_HOME/ERROR-* results;
+    jar cf $ZIPFILE results;
+    exit 1;
+else
+    echo "TEST OK";
+    cp $VTEST_HOME/joram/src/jndi2/report.txt results/jndi2-report.txt;
+    cp $VTEST_HOME/joram/src/joram/report.txt results/joram-report.txt;
+    cp $VTEST_HOME/joram/src/jms/report.txt results/jms-report.txt;
+    jar cf $ZIPFILE results;
+    exit 0;
+fi
