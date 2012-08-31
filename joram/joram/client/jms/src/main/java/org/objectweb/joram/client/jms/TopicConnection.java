452558105b571efbf12448055890c60ee5474bf9
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-08-31 14:43:38 +0000

    JavaDoc enhancements.

--- joram/joram/client/jms/src/main/java/org/objectweb/joram/client/jms/TopicConnection.java
+++ joram/joram/client/jms/src/main/java/org/objectweb/joram/client/jms/TopicConnection.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2001 - 2009 ScalAgent Distributed Technologies
+ * Copyright (C) 2001 - 2012 ScalAgent Distributed Technologies
  * Copyright (C) 1996 - 2000 Dyade
  *
  * This library is free software; you can redistribute it and/or
@@ -43,6 +43,17 @@ public class TopicConnection extends Connection implements javax.jms.TopicConnec
 
   /**
    * API method.
+   * Creates a connection consumer for this connection, this is an expert facility needed
+   * for applications servers.
+   * 
+   * @param topic       the topic to access.
+   * @param selector    only messages with properties matching the message selector expression
+   *                    are delivered. A value of null or an empty string indicates that there is
+   *                    no message selector for this message consumer.
+   * @param sessionPool the server session pool to associate with this connection consumer.
+   * @param maxMessages the maximum number of messages that can be assigned to a server session
+   *                    at one time.
+   * @return The connection consumer.
    * 
    * @exception IllegalStateException  If the connection is closed.
    * @exception InvalidSelectorException  If the selector syntax is wrong.
@@ -59,7 +70,15 @@ public class TopicConnection extends Connection implements javax.jms.TopicConnec
 
   /**
    * API method.
-   * 
+   * Creates a TopicSession object.
+   *    
+   * @param transacted      indicates whether the session is transacted.
+   * @param acknowledgeMode indicates whether the consumer or the client will acknowledge any
+   *                        messages it receives; ignored if the session is transacted. Legal
+   *                        values are Session.AUTO_ACKNOWLEDGE, Session.CLIENT_ACKNOWLEDGE, and
+   *                        Session.DUPS_OK_ACKNOWLEDGE.
+   * @return A newly created session.
+
    * @exception IllegalStateException  If the connection is closed.
    * @exception JMSException  In case of an invalid acknowledge mode.
    */
