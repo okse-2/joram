9e5cfe430d67a6810c4c44f20526313b8efed856
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-09-03 09:11:26 +0000

    trying very simple command

--- trunk/tests/vtest/runtest.sh
+++ trunk/tests/vtest/runtest.sh
@@ -18,20 +18,20 @@ date=`echo $date | sed -e s:/:.:g`
 LOGFILE=$VTEST_HOME/"vtest-$date.log"
 ZIPFILE=$VTEST_HOME/"result.zip"
 
+touch $ZIPFILE
+
 #extracting joram source so as to test updated trunk artifacts
-svn co svn://svn.forge.objectweb.org/svnroot/joram/trunk/joram $VTEST_HOME/joram-src
-cd $VTEST_HOME/joram-src;
-echo "installing joram trunk"
-mvn install;
+#svn co svn://svn.forge.objectweb.org/svnroot/joram/trunk/joram $VTEST_HOME/joram-src
+#cd $VTEST_HOME/joram-src;
+#echo "installing joram trunk"
+#mvn install;
 
 #following command suppose that svn check out has been made into path $VTEST_HOME/joram
-cd $VTEST_HOME/joram;
+#cd $VTEST_HOME/joram;
 
 #installing joram tests using maven
-echo "installing joram tests"
-mvn install
-
-touch /home/vsadt/ok.txt
+#echo "installing joram tests"
+#mvn install
 
 # launching tests
 #cd src;
