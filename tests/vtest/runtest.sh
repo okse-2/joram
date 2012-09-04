4fb223129a31f23af35e3b359aec7fed22989d15
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-09-04 15:27:23 +0000

    updating runtest

--- tests/vtest/runtest.sh
+++ tests/vtest/runtest.sh
@@ -19,29 +19,29 @@ LOGFILE=$VTEST_HOME/"vtest-$date.log"
 ZIPFILE=$VTEST_HOME/"result.zip"
 
 #extracting joram source so as to test updated trunk artifacts
-svn co svn://svn.forge.objectweb.org/svnroot/joram/trunk/joram $VTEST_HOME/joram-src >> $LOGFILE 2>1&;
-cd $VTEST_HOME/joram-src;
+svn co svn://svn.forge.objectweb.org/svnroot/joram/trunk/joram $VTEST_HOME/joram-src >> $LOGFILE 2>1&
+cd $VTEST_HOME/joram-src ;
 echo "installing joram trunk"
-mvn install >> $LOGFILE 2>1&;
+mvn install >> $LOGFILE 2>1&
 
 #following command suppose that svn check out has been made into path $VTEST_HOME/joram
-cd $VTEST_HOME/joram;
+cd $VTEST_HOME/joram ;
 
 #installing joram tests using maven
 echo "installing joram tests"
-mvn install >> $LOGFILE 2>1&;
+mvn install >> $LOGFILE 2>1&
 
 #launching tests
 cd src;
 echo "on launching ant custom.tests.vtest"
-ant custom.tests.vtest >> $LOGFILE 2>1&;
-ant vtest.check.reports >> $LOGFILE 2>1&;
+ant custom.tests.vtest >> $LOGFILE 2>1& 
+ant vtest.check.reports >> $LOGFILE 2>1& 
 TEST_RESULT=$?;
 
-mkdir results;
-cp $LOGFILE results;
+mkdir results ;
+cp $LOGFILE results ;
 
-getting test exit code
+#getting test exit code
 if [[ $TEST_RESULT -gt 0 ]]; then
     echo "TEST FAILED !";
     #save contents when test failed
