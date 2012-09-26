60e80bf5953e29d44b6ffe4eeb41e0420acf573f
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-09-26 15:25:08 +0000

    Update the framework classes to shared a base class between classic and OSGi implementations.

--- trunk/tests/src/framework/SCAdminClassic.java
+++ trunk/tests/src/framework/SCAdminClassic.java
@@ -23,57 +23,25 @@
 package framework;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.objectweb.util.monolog.api.BasicLevel;
-import org.objectweb.util.monolog.api.Logger;
 
 import fr.dyade.aaa.agent.AgentServer;
-import fr.dyade.aaa.agent.UnknownServerException;
 import fr.dyade.aaa.agent.conf.A3CML;
 import fr.dyade.aaa.agent.conf.A3CMLConfig;
 import fr.dyade.aaa.agent.conf.UnknownServiceException;
-import fr.dyade.aaa.common.Daemon;
-import fr.dyade.aaa.common.Debug;
 
-public class SCAdminClassic implements SCAdminItf {
-
-  private static final Logger logmon = Debug.getLogger(SCAdminClassic.class.getName());
-
-  /** Map containing all <code>Process</code> of running AgentServers */
-  private Map launchedServers = new HashMap();
-
-  public void killAgentServer(short sid) {
-    Server server = (Server) launchedServers.get(new Short(sid));
-    logmon.log(BasicLevel.DEBUG, "SCAdminClassic: kill AgentServer#" + sid);
-    if (server != null) {
-      server.process.destroy();
-      try {
-        server.process.waitFor();
-      } catch (InterruptedException exc) {
-        if (logmon.isLoggable(BasicLevel.ERROR)) {
-          logmon.log(BasicLevel.ERROR, "SCAdminClassic: AgentServer#" + sid
-              + " error waiting for process kill.", exc);
-        }
-      }
-    } else {
-      logmon.log(BasicLevel.WARN, "Server process to kill not found: " + sid);
-    }
-  }
-
-  public void startAgentServer(short sid) throws Exception {
-    startAgentServer(sid, null);
+public class SCAdminClassic extends SCBaseAdmin {
+  private static byte [] halt = "halt\n".getBytes();
+  
+  protected byte[] getHaltCommand() {
+    return halt;
   }
 
   public void startAgentServer(short sid, String[] jvmargs) throws Exception {
-    logmon.log(BasicLevel.DEBUG, "SCAdminClassic: run AgentServer#" + sid);
+    logmon.log(BasicLevel.DEBUG, "SCAdmin: run AgentServer#" + sid);
 
     Server server = (Server) launchedServers.get(new Short(sid));
 
@@ -81,19 +49,20 @@ public class SCAdminClassic implements SCAdminItf {
       try {
         int exitValue = server.process.exitValue();
         if (logmon.isLoggable(BasicLevel.DEBUG)) {
-          logmon.log(BasicLevel.DEBUG, "SCAdminClassic: AgentServer#" + sid + " -> " + exitValue);
+          logmon.log(BasicLevel.DEBUG, "SCAdmin: AgentServer#" + sid + " -> " + exitValue);
         }
       } catch (IllegalThreadStateException exc) {
         if (logmon.isLoggable(BasicLevel.WARN)) {
-          logmon.log(BasicLevel.WARN, "SCAdminClassic: AgentServer#" + sid + " already running.");
+          logmon.log(BasicLevel.WARN, "SCAdmin: AgentServer#" + sid + " already running.");
         }
         throw new IllegalStateException("AgentServer#" + sid + " already running.");
       }
     }
     
+    // Get the configuration
     A3CMLConfig a3config;
-    String configPath = null;
     try {
+      String configPath = null;
       if (jvmargs != null) {
         for (int i = 0; i < jvmargs.length; i++) {
           String jvmArg = jvmargs[i];
@@ -109,7 +78,7 @@ public class SCAdminClassic implements SCAdminItf {
         a3config = A3CML.getXMLConfig(configPath);
       }
     } catch (Exception exc) {
-      logmon.log(BasicLevel.ERROR, "SCAdminClassic: problem during configuration parsing", exc);
+      logmon.log(BasicLevel.ERROR, "SCAdmin: problem during configuration parsing", exc);
       throw new Exception("Problem during configuration parsing");
     }
 
@@ -135,12 +104,12 @@ public class SCAdminClassic implements SCAdminItf {
       port = Integer.parseInt(a3config.getServiceArgs(sid, "fr.dyade.aaa.agent.AdminProxy"));
     } catch (UnknownServiceException exc) {
       if (logmon.isLoggable(BasicLevel.WARN)) {
-        logmon.log(BasicLevel.WARN, "AdminProxy service not found, server will not be stoppable "
+        logmon.log(BasicLevel.WARN, "SCAdmin: AdminProxy service not found, server will not be stoppable "
             + "using SCAdmin. Only the killAgentServer() method can be used. ");
       }
     }
     if (logmon.isLoggable(BasicLevel.DEBUG)) {
-      logmon.log(BasicLevel.DEBUG, "SCAdminClassic: AgentServer#" + sid + " telnet port: " + port);
+      logmon.log(BasicLevel.DEBUG, "SCAdmin: AgentServer#" + sid + " telnet port: " + port);
     }
 
     // Main class
@@ -160,134 +129,4 @@ public class SCAdminClassic implements SCAdminItf {
 
     launchedServers.put(new Short(sid), new Server(port, p));
   }
-
-  public void stopAgentServer(short sid) throws Exception {
-    Socket socket = null;
-    Server server = (Server) launchedServers.get(new Short(sid));
-    TelnetReaderDaemon daemon = null;
-
-    if (server != null) {
-      if (logmon.isLoggable(BasicLevel.DEBUG)) {
-        logmon.log(BasicLevel.DEBUG, "SCAdminClassic: stop AgentServer#" + sid);
-      }
-      try {
-        socket = new Socket("localhost", server.telnetPort);
-        daemon = new TelnetReaderDaemon(socket.getInputStream(), logmon);
-        daemon.start();
-
-        socket.getOutputStream().write("halt\n".getBytes());
-        socket.getOutputStream().flush();
-
-
-      } catch (Throwable exc) {
-        if (logmon.isLoggable(BasicLevel.ERROR))
-          logmon.log(BasicLevel.ERROR, "SCAdminClassic: Can't stop server#" + sid + ", kill it.", exc);
-        killAgentServer(sid);
-        throw new Exception("SCAdminClassic: Can't stop server#" + sid, exc);
-      }
-
-      if (logmon.isLoggable(BasicLevel.DEBUG)) {
-        logmon.log(BasicLevel.DEBUG, "SCAdminClassic: join AgentServer#" + sid + " [" + server.process + ']');
-      }
-      server.process.waitFor();
-      daemon.stop();
-    } else {
-      throw new UnknownServerException("Server " + sid + " unknown: not started using SCAdmin.");
-    }
-  }
-
-  public void stopAgentServerExt(int telnetPort) throws Exception {
-    Socket socket = null;
-    TelnetReaderDaemon daemon = null;
-
-    if (logmon.isLoggable(BasicLevel.DEBUG)) {
-      logmon.log(BasicLevel.DEBUG, "SCAdminClassic: stop on port " + telnetPort);
-    }
-    try {
-      socket = new Socket("localhost", telnetPort);
-      daemon = new TelnetReaderDaemon(socket.getInputStream(), logmon);
-      daemon.start();
-
-      socket.getOutputStream().write("halt\n".getBytes());
-      socket.getOutputStream().flush();
-
-    } catch (Throwable exc) {
-      if (logmon.isLoggable(BasicLevel.DEBUG))
-        logmon.log(BasicLevel.DEBUG, "SCAdminClassic: Can't stop server on port " + telnetPort, exc);
-      if (daemon != null) {
-        daemon.stop();
-      }
-      throw new Exception("Can't stop server on port " + telnetPort + ": " + exc.getMessage());
-    }
-
-    if (logmon.isLoggable(BasicLevel.DEBUG)) {
-      logmon.log(BasicLevel.DEBUG, "SCAdminClassic: wait closing telnet connection on " + telnetPort);
-    }
-    while (daemon.isRunning()) {
-      Thread.sleep(500);
-    }
-    daemon.close();
-  }
-
-  private static class TelnetReaderDaemon extends Daemon {
-
-    private InputStreamReader reader;
-
-    protected TelnetReaderDaemon(InputStream stream, Logger logger) {
-      super("TelnetReaderDaemon", logger);
-      this.reader = new InputStreamReader(stream);
-    }
-
-    protected void close() {
-    }
-
-    protected void shutdown() {
-      try {
-        reader.close();
-      } catch (IOException exc) {
-      }
-    }
-
-    public void run() {
-      int character;
-      StringBuffer sb = new StringBuffer();
-      try {
-        while (running) {
-          canStop = true;
-          try {
-            character = reader.read();
-          } catch (IOException exc) {
-            logmon.log(BasicLevel.ERROR, "Error in telnet daemon.", exc);
-            return;
-          }
-          canStop = false;
-          if (character == -1) break;
-
-          if (character == '\n') {
-            if (logmon.isLoggable(BasicLevel.DEBUG)) {
-              logmon.log(BasicLevel.DEBUG, "TelnetReaderDaemon read: " + sb);
-            }
-            sb.setLength(0);
-          } else {
-            sb.append((char) character);
-          }
-        }
-      } finally {
-        finish();
-      }
-    }
-  }
-
-  private static class Server {
-    public int telnetPort;
-
-    public Process process;
-
-    public Server(int telnetPort, Process process) {
-      super();
-      this.telnetPort = telnetPort;
-      this.process = process;
-    }
-  }
-
 }
