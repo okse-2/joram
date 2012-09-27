f860fdd867a0bf22ad74489cde3b8410f962cbc8
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-09-27 12:55:57 +0000

    removing useless runtest scripts and adding new one for windows

--- trunk/tests/vtest/runtest.bat
+++
@@ -1,9 +0,0 @@
-REM Vtest script that launches JORAM test
-REM uncomment following line to disable output
-REM @echo off
-
-set SERVER_RUN_DIR=%VTEST_HOME%\joram\src
-
-echo == Launching JORAM test via ant tests.jms.all ==
-start /D %SERVER_RUN_DIR% ant tests.jms.all
-REM ant vtest.check.reports
