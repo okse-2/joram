b76c57837cde95e2fda2ee887f70b4be67555800
malvault <malvault@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2012-12-19 10:04:21 +0000

    updated runtest script for joram

--- tests/vtest/runtest.sh
+++ tests/vtest/runtest.sh
@@ -4,45 +4,71 @@
 # $1 = version, sous form url SVN #
 ###################################
 
+############
+## Variable setting
+############
+EXPECTED_ARGS=1
+
 #ensure existence of environment variable VTEST_HOME
 if [ -z "$VTEST_HOME" ]
 then
     echo "variable VTEST_HOME doesn't exist"
     echo "please set variable VTEST_HOME to the Vtest \"runtest\" script path"
-    exit 1
+    export VTEST_HOME="/home/vsadt"
 else
     echo "VTEST_HOME set to $VTEST_HOME"
 fi
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "runtest is expecting a svn url"
+    echo "Usage: `basename $0` svn_url"
+    exit 1;
+fi
+
+source $VTEST_HOME/.bashrc
+
+#value should be synchronized whith vtest configuration (test.properties file)
+JORAM_TEST_DIR="joram-test"
 
 date=`date +%x`
 date=`echo $date | sed -e s:/:.:g`
 LOGFILE=$VTEST_HOME/"vtest-$date.log"
 ZIPFILE=$VTEST_HOME/"result.zip"
 
-#extracting joram source so as to test updated trunk artifacts
+echo "printing JDK version"
+java -version >> $LOGFILE 2>&1 
+echo "--"
+
+############
+## Test installation
+############
 svn co svn://svn.forge.objectweb.org/svnroot/joram/$1/joram $VTEST_HOME/joram-src >> $LOGFILE 2>&1
 cd $VTEST_HOME/joram-src ;
-echo "installing joram trunk"
+echo "installing joram sources from url $1"
 mvn install >> $LOGFILE 2>&1
 
-#following command suppose that svn check out has been made into path $VTEST_HOME/joram
-cd $VTEST_HOME/joram-test ;
+cd $VTEST_HOME/$JORAM_TEST_DIR ;
 
 #installing joram tests using maven
 echo "installing joram tests"
 mvn install >> $LOGFILE 2>&1
 
-#launching tests
+############
+## Launching tests
+############
 cd src;
-echo "on launching ant custom.tests.vtest"
-ant custom.tests.vtest -Dship.dir=../../../joram-src/ship >> $LOGFILE 2>&1 
+echo "on launching ant tests.jms.all"
+ant custom.tests.vtestl -Dship.dir=../../../joram-src/ship >> $LOGFILE 2>&1
 ant vtest.check.reports >> $LOGFILE 2>&1 
 TEST_RESULT=$?;
 
+############
+## Collecting results
+############
 mkdir results ;
 cp $LOGFILE results ;
 
-#getting test exit code
 if [[ $TEST_RESULT -gt 0 ]]; then
     echo "TEST FAILED !";
     #save contents when test failed
