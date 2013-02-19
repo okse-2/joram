b0635336f7a2aeae6ed802ae5a90e6d3af612e30
feliot <feliot@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-02-19 17:19:07 +0000

    Batch writing and decoupled reading

--- joram/joram/client/jms/src/main/java/org/objectweb/joram/client/jms/tcp/ReliableTcpConnection.java
+++ joram/joram/client/jms/src/main/java/org/objectweb/joram/client/jms/tcp/ReliableTcpConnection.java
@@ -22,11 +22,12 @@
 package org.objectweb.joram.client.jms.tcp;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Vector;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.objectweb.joram.shared.client.AbstractJmsMessage;
 import org.objectweb.joram.shared.client.AbstractJmsReply;
@@ -49,31 +50,28 @@ public class ReliableTcpConnection {
   public static String WINDOW_SIZE_PROP_NAME = "fr.dyade.aaa.util.ReliableTcpConnection.windowSize";
 
   public static int DEFAULT_WINDOW_SIZE = 100;
+  
+  public static int DEFAULT_BUFFER_SIZE = 8192;
+  
+  public static final int DEFAULT_TCP_SENDBUFFER_SIZE = 32768;
+
+  public static final int DEFAULT_TCP_RECEIVEBUFFER_SIZE = 32768;
 
   private int windowSize;
 
-  // JORAM_PERF_BRANCH
-  //private volatile long inputCounter;
-
-  //private long outputCounter;
-
-  //private volatile int unackCounter;
-
-  //private Vector pendingMessages;
-
   private Socket sock;
 
-  private NetOutputStream nos;
+  private BufferedOutputStream bos;
 
   private BufferedInputStream bis;
 
-  //private Object inputLock;
-
-  //private Object outputLock;
+  private LinkedBlockingQueue<byte[]> receiveQueue;
 
   private int status;
   
   private java.util.Timer timer;
+  
+  private Reader reader;
 
   public ReliableTcpConnection(java.util.Timer timer) {    
     windowSize = Integer.getInteger(
@@ -83,13 +81,7 @@ public class ReliableTcpConnection {
       logger.log(BasicLevel.INFO, 
                  "ReliableTcpConnection.windowSize=" + windowSize);
     this.timer = timer;
-    // JORAM_PERF_BRANCH:
-    //inputCounter = -1;
-    //outputCounter = 0;
-    //unackCounter = 0;
-    //pendingMessages = new Vector();
-    //inputLock = new Object();
-    //outputLock = new Object();
+    this.receiveQueue = new LinkedBlockingQueue<byte[]>();
     setStatus(INIT);
   }
 
@@ -115,27 +107,20 @@ public class ReliableTcpConnection {
 
     try {
       this.sock = sock;
-      nos = new NetOutputStream(sock);
+      sock.setSendBufferSize(DEFAULT_TCP_SENDBUFFER_SIZE);
+      sock.setReceiveBufferSize(DEFAULT_TCP_RECEIVEBUFFER_SIZE);
+      sock.setTcpNoDelay(true);
       
-      // JORAM_PERF_BRANCH
-      /*
-        synchronized (pendingMessages) {
-          for (int i = 0; i < pendingMessages.size(); i++) {
-            TcpMessage pendingMsg = (TcpMessage) pendingMessages.elementAt(i);
-            // JORAM_PERF_BRANCH:
-            doSend(pendingMsg.object);
-          }
-        }
-      */
-
-      //synchronized (inputLock) {
-      bis = new BufferedInputStream(sock.getInputStream());
-      //}
+      bos = new BufferedOutputStream(sock.getOutputStream(), DEFAULT_BUFFER_SIZE);
+      bis = new BufferedInputStream(sock.getInputStream(), DEFAULT_BUFFER_SIZE);
 
       setStatus(CONNECT);
 
       FlushTask flushTask = new FlushTask();
       timer.schedule(flushTask, 50, 50);
+      
+      reader = new Reader();
+      reader.start();
     } catch (IOException exc) {
       if (logger.isLoggable(BasicLevel.DEBUG))
         logger.log(BasicLevel.DEBUG, "", exc);
@@ -152,13 +137,7 @@ public class ReliableTcpConnection {
     if (getStatus() != CONNECT) 
       throw new IOException("Connection closed");
     try {      
-        // JORAM_PERF_BRANCH:
-        doSend(request);
-        /* JORAM_PERF_BRANCH:
-        addPendingMessage(new TcpMessage(
-          outputCounter, request));
-        outputCounter++;
-          JORAM_PERF_BRANCH.*/
+      doSend(request);
     } catch (IOException exc) {
       if (logger.isLoggable(BasicLevel.DEBUG))
         logger.log(BasicLevel.DEBUG, "ReliableTcpConnection.send()", exc);
@@ -172,135 +151,30 @@ public class ReliableTcpConnection {
       logger.log(BasicLevel.DEBUG,
                  "ReliableTcpConnection.doSend(" + msg + ')');
     // JORAM_PERF_BRANCH:
-    nos.send(msg);
-    //unackCounter = 0;
-  }
-
-  static class NetOutputStream extends ByteArrayOutputStream {
-    private static final int MAX_BUFFER_SIZE = 8192;
-    
-    private OutputStream os = null;
-
-    NetOutputStream(Socket sock) throws IOException {
-      super(1024);
-      reset();
-      os = sock.getOutputStream();
-    }
-
-    public void reset() {
-      count = 0;
-    }
-
-    synchronized void send(AbstractJmsMessage msg) throws IOException {
-        // JORAM_PERF_BRANCH:
-        //StreamUtil.writeTo(id, this);
-        //StreamUtil.writeTo(ackId, this);
-      AbstractJmsMessage.write(msg, this);
-      if (size() > MAX_BUFFER_SIZE) {
-        flushSocket();
-      }
-/* JORAM_PERF_BRANCH:
-        buf[0] = (byte) ((count -4) >>>  24);
-        buf[1] = (byte) ((count -4) >>>  16);
-        buf[2] = (byte) ((count -4) >>>  8);
-        buf[3] = (byte) ((count -4) >>>  0);
-*/
-    }
-    
-    // JORAM_PERF_BRANCH:
-    void flushSocket() throws IOException {
-      if (size() > 0) {
-        synchronized (this) {
-          writeTo(os);
-          reset();
-        }
-        os.flush();
-      }
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    AbstractJmsMessage.write(msg, baos);
+    byte[] bytes = baos.toByteArray();
+    synchronized (bos) {
+      StreamUtil.writeTo(bytes.length, bos);
+      bos.write(bytes);
     }
   }
   
-  /* JORAM_PERF_BRANCH:
-  private void addPendingMessage(TcpMessage msg) {
-    if (logger.isLoggable(BasicLevel.DEBUG))
-      logger.log(BasicLevel.DEBUG,
-                 "ReliableTcpConnection.addPendingMessage(" + msg + ')');
-    synchronized (pendingMessages) {
-      pendingMessages.addElement(msg);
-    }
-  }
-  JORAM_PERF_BRANCH.*/
-  /* JORAM_PERF_BRANCH:
-  private void ackPendingMessages(long ackId) {
-    if (logger.isLoggable(BasicLevel.DEBUG))
-      logger.log(BasicLevel.DEBUG,
-                 "ReliableTcpConnection.ackPendingMessages(" + ackId + ')');
-    
-    synchronized (pendingMessages) {
-      while (pendingMessages.size() > 0) {
-        TcpMessage pendingMsg = (TcpMessage)pendingMessages.elementAt(0);
-        if (ackId < pendingMsg.id) {
-          // It's an old acknowledge
-          break;
-        }
-        
-        pendingMessages.removeElementAt(0);
-      }
-    }
-  }
-*/
   public AbstractJmsReply receive() throws Exception {
     if (logger.isLoggable(BasicLevel.DEBUG))
       logger.log(BasicLevel.DEBUG,
                  "ReliableTcpConnection.receive()");
     if (getStatus() != CONNECT) 
       throw new IOException("Connection closed");
-
-    while (true) {
-      try {
-        //long messageId;
-        //long ackId;
-        AbstractJmsReply obj;
-
-        // JORAM_PERF_BRANCH:
-        //synchronized (inputLock) {
-          
-          //int len = StreamUtil.readIntFrom(bis);
-          //messageId = StreamUtil.readLongFrom(bis);
-          //ackId = StreamUtil.readLongFrom(bis);
-        obj =  (AbstractJmsReply) AbstractJmsMessage.read(bis);
-          // JORAM_PERF_BRANCH:
-        return obj;
-          //JORAM_PERF_BRANCH.
-
-        /* JORAM_PERF_BRANCH:
-        if (logger.isLoggable(BasicLevel.DEBUG))
-          logger.log(BasicLevel.DEBUG, " -> id = " + messageId);
-        ackPendingMessages(ackId);
-        if (obj != null) {
-          if (unackCounter < windowSize) {
-            if (logger.isLoggable(BasicLevel.DEBUG))
-              logger.log(BasicLevel.DEBUG, " -> unackCounter++");
-            unackCounter++;
-          } else {
-            if (logger.isLoggable(BasicLevel.DEBUG))
-              logger.log(BasicLevel.DEBUG, " -> schedule");
-            AckTimerTask ackTimertask = new AckTimerTask();
-            timer.schedule(ackTimertask, 0);
-          }
-          if (messageId > inputCounter) {
-            inputCounter = messageId;
-            return obj;
-          } else if (logger.isLoggable(BasicLevel.DEBUG))
-            logger.log(BasicLevel.DEBUG,
-                       " -> already received message: " + messageId + " " + obj);
-        }
-        JORAM_PERF_BRANCH.*/       
-      } catch (IOException exc) {
-        if (logger.isLoggable(BasicLevel.DEBUG))
-          logger.log(BasicLevel.DEBUG, "", exc);
-        close();
-        throw exc;
-      }
+    byte[] bytes = receiveQueue.take();
+    try {
+      ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+      return (AbstractJmsReply) AbstractJmsMessage.read(bais);
+    } catch (IOException exc) {
+      if (logger.isLoggable(BasicLevel.DEBUG))
+        logger.log(BasicLevel.DEBUG, "", exc);
+      close();
+      throw exc;
     }
   }
 
@@ -319,6 +193,7 @@ public class ReliableTcpConnection {
     try { 
       sock.close();
     } catch (IOException exc) {}
+    reader.stop();
     setStatus(INIT);
   }
 
@@ -338,34 +213,59 @@ public class ReliableTcpConnection {
     }
   }
   
-  /* JORAM_PERF_BRANCH:
-  class AckTimerTask extends java.util.TimerTask {
-    public void run() {
-      if (logger.isLoggable(BasicLevel.DEBUG))
-        logger.log(BasicLevel.DEBUG, "AckTimerTask.run()");
-      try {
-        doSend(-1, inputCounter, null);
-        cancel();
-      } catch (IOException exc) {
-        if (logger.isLoggable(BasicLevel.DEBUG))
-          logger.log(BasicLevel.DEBUG, "", exc);
-      }
-    }
-  }
-  JORAM_PERF_BRANCH.*/
-  
   //JORAM_PERF_BRANCH:
   class FlushTask extends java.util.TimerTask {
     public void run() {
       if (logger.isLoggable(BasicLevel.DEBUG))
         logger.log(BasicLevel.DEBUG, "FlushTask.run()");
       try {
-        nos.flushSocket();
+        synchronized (bos) {
+          bos.flush();
+        }
       } catch (IOException exc) {
         if (logger.isLoggable(BasicLevel.WARN))
           logger.log(BasicLevel.WARN, "", exc);
       }
     }
+  }
+  
+  class Reader extends fr.dyade.aaa.common.Daemon {
+    Reader() {
+      super("ReliableTcpConnectionReader", logger);
+    }
+
+    public void run() {
+      try {
+        while (running) {
+          canStop = true;
+          int len = StreamUtil.readIntFrom(bis);
+          byte[] bytes = StreamUtil.readFully(len, bis);
+          receiveQueue.offer(bytes);
+        }
+      } catch (Exception exc) { 
+        if (logger.isLoggable(BasicLevel.DEBUG))
+          logger.log(BasicLevel.DEBUG, "", exc);
+        close();
+      } finally {
+        finish();
+      }
+    }
+    
+    /**
+     * Enables the daemon to stop itself.
+     */
+    public void stop() {
+      if (isCurrentThread()) {
+        finish();
+      } else {
+        super.stop();
+      }
+    }
+
+    protected void shutdown() {}
+
+    protected void close() {}
+    
   }
   
 }
