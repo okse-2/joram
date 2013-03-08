cae3837f178d486edbc884832f92987c4b4d8f89
feliot <feliot@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-03-08 17:05:41 +0000

    Replace Java serialization with a specific serializer

--- branches/JORAM_5_8_1_PERF/joram/a3/rt/src/main/java/fr/dyade/aaa/util/TransactionObject.java
+++ branches/JORAM_5_8_1_PERF/joram/a3/rt/src/main/java/fr/dyade/aaa/util/TransactionObject.java
@@ -8,6 +8,7 @@ public interface TransactionObject {
   
   public static final int CLASS_ID_AREA = 0x0000;
   public static final int NOTIFICATION_CLASS_ID = CLASS_ID_AREA + 0;
+  public static final int AGENTID_CLASS_ID = CLASS_ID_AREA + 1;
   
   public int getClassId();
   
