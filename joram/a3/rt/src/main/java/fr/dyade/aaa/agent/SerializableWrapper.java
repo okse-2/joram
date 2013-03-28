0af4ae51c094c596a41f8b289f5efa81982cf0a2
feliot <feliot@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-03-28 16:19:07 +0000

    Serializable wrapper

---
+++ joram/a3/rt/src/main/java/fr/dyade/aaa/agent/SerializableWrapper.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2001 - 2007 ScalAgent Distributed Technologies 
+ * Copyright (C) 1996 - 2000 BULL
+ * Copyright (C) 1996 - 2000 INRIA
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ */
+package fr.dyade.aaa.agent;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import fr.dyade.aaa.common.encoding.Decoder;
+import fr.dyade.aaa.common.encoding.Encodable;
+import fr.dyade.aaa.common.encoding.Encoder;
+
+public class SerializableWrapper implements Encodable {
+  
+  private Serializable value;
+
+  public SerializableWrapper(Serializable value) {
+    super();
+    this.value = value;
+  }
+
+  public int getClassId() {
+    return EncodableHelper.SERIALIZABLE_WRAPPER_CLASS_ID;
+  }
+
+  public int getEncodedSize() {
+    // Unknown
+    return 0;
+  }
+
+  public void encode(Encoder encoder) throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(baos);
+    oos.writeObject(value);
+    byte[] bytes = baos.toByteArray();
+    encoder.encodeByteArray(bytes);
+  }
+
+  public void decode(Decoder decoder) throws Exception {
+    byte[] bytes = decoder.decodeByteArray();
+    ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+    ObjectInputStream ois = new ObjectInputStream(bais);
+    value = (Serializable) ois.readObject();
+  }
+
+}
