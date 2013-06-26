7ddb058484f4e4af316f3fc9911b2acd16e15efd
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-06-26 08:00:48 +0000

    update onException

--- trunk/tests/src/joram/base/Test_ExcpList_Q.java
+++ trunk/tests/src/joram/base/Test_ExcpList_Q.java
@@ -36,106 +36,91 @@ import javax.naming.InitialContext;
 
 import framework.TestCase;
 
-
-
 /**
- * Test ExceptionListener. 
- * Use a queue 
- *
+ * Test ExceptionListener. Use a queue
+ * 
  */
 public class Test_ExcpList_Q extends TestCase implements ExceptionListener {
 
-   
-    public static void main(String[] args) {
-	new Test_ExcpList_Q().run();
+  public static void main(String[] args) {
+    new Test_ExcpList_Q().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("server start");
+      startAgentServer((short) 0);
+
+      admin();
+      System.out.println("admin config ok");
+
+      Context ictx = new InitialContext();
+      Queue queue = (Queue) ictx.lookup("queue");
+
+      ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+
+      Connection cnx = cf.createConnection();
+
+      Session sessionp = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Session sessionc = cnx.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      cnx.setExceptionListener(this);
+      cnx.start();
+
+      // create a producer and a consumer
+
+      MessageConsumer consumer = sessionc.createConsumer(queue);
+      MessageProducer producer = sessionp.createProducer(queue);
+      // create a message send to the queue by the pruducer
+      Message msg = sessionp.createMessage();
+
+      producer.send(msg);
+
+      // receive the message
+      Message msg1 = consumer.receive(3000);
+
+      // We do not close the connection, so we have an exception
+      // cnx.close();
+
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
+    } finally {
+      System.out.println("Server stop ");
+      stopAgentServer((short) 0);
+      endTest();
     }
-          
-    public void run() {
-	try {
-	    System.out.println("server start");
-	    startAgentServer((short)0);
-	   
-	    
-	    admin();
-	    System.out.println("admin config ok");
-	    
-	    Context  ictx = new InitialContext();
-	    Queue queue = (Queue) ictx.lookup("queue");
-	   
-	    ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
-   
-	    Connection cnx = cf.createConnection();
+  }
 
-	  
-	    Session sessionp = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    Session sessionc = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    cnx.setExceptionListener(this);
-	    cnx.start();
-	   
-	    // create a producer and a consumer
-	   
-	    MessageConsumer consumer = sessionc.createConsumer(queue);
-	    MessageProducer producer = sessionp.createProducer(queue);
-	    // create a message send to the queue by the pruducer 
-	    Message msg = sessionp.createMessage();
-	    
-	    producer.send(msg);
-	    
-	    // receive the message
-	     Message msg1= consumer.receive(3000);
-	    
-	    // We do not close the connection, so we have an exception
-	    //cnx.close();
-	    
-	}catch (Throwable exc) {
-	    exc.printStackTrace();
-	    error(exc);
-	}
-	finally {
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-	    endTest(); 
-	}
-    }
+  /**
+   * Admin : Create queue and a user anonymous use jndi
+   */
+  public void admin() throws Exception {
+    // conexion
+    org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560, "root", "root", 60);
+    // create a Queue
+    org.objectweb.joram.client.jms.Queue queue = 
+        (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create(0);
+    // create a user
+    org.objectweb.joram.client.jms.admin.User user = 
+        org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
+    // set permissions
+    queue.setFreeReading();
+    queue.setFreeWriting();
 
+    javax.jms.ConnectionFactory cf = 
+        org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
 
-    /**
-     * Admin : Create queue and a user anonymous
-     *   use jndi
-     */
-    public void admin() throws Exception {
-	// conexion 
-	org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
-								 "root", "root", 60);
-	// create a Queue   
-	org.objectweb.joram.client.jms.Queue queue =
-	    (org.objectweb.joram.client.jms.Queue) org.objectweb.joram.client.jms.Queue.create(0); 
-        // create a user
-	org.objectweb.joram.client.jms.admin.User user =
-	    org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
-	// set permissions
-	queue.setFreeReading();
-	queue.setFreeWriting();
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.bind("cf", cf);
+    jndiCtx.bind("queue", queue);
 
-      	javax.jms.ConnectionFactory cf =
-	    org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
+    jndiCtx.close();
 
-	javax.naming.Context jndiCtx = new javax.naming.InitialContext();
-	jndiCtx.bind("cf", cf);
-	jndiCtx.bind("queue", queue);
+    org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
+  }
 
-	jndiCtx.close();
-	   
-	org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
-    }
-
-
-
-    public void onException(JMSException jms){
-	assertEquals("javax.jms.IllegalStateException",jms.toString());
-	
-    }
+  public void onException(JMSException jms) {
+    assertTrue("unexpected exception: " + jms, jms instanceof javax.jms.IllegalStateException);
+  }
 }
--- trunk/tests/src/joram/base/Test_ExcpList_T.java
+++ trunk/tests/src/joram/base/Test_ExcpList_T.java
@@ -43,94 +43,93 @@ import framework.TestCase;
  */
 public class Test_ExcpList_T extends TestCase implements ExceptionListener{
 
-   
-    public static void main(String[] args) {
-	new Test_ExcpList_T().run();
+
+  public static void main(String[] args) {
+    new Test_ExcpList_T().run();
+  }
+
+  public void run() {
+    try {
+      System.out.println("server start");
+      startAgentServer((short)0);
+
+
+      admin();
+      System.out.println("admin config ok");
+
+      Context  ictx = new InitialContext();
+      Topic topic = (Topic) ictx.lookup("topic");
+
+      ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
+      ictx.close();
+
+      Connection cnx = cf.createConnection();
+
+      Session sessionp = cnx.createSession(false,
+          Session.AUTO_ACKNOWLEDGE);
+      Session sessionc = cnx.createSession(false,
+          Session.AUTO_ACKNOWLEDGE);
+      cnx.setExceptionListener(this);
+      cnx.start();
+
+      // create a producer and a consumer
+      MessageProducer producer = sessionp.createProducer(topic);
+      MessageConsumer consumer = sessionc.createConsumer(topic);
+      // create a message send to the topic by the pruducer 
+      Message msg = sessionp.createMessage();
+
+      producer.send(msg);
+
+      // receive the message
+      Message msg1= consumer.receive(3000);
+
+      // We do not close the connection, so we have an exception
+      // cnx.close();
+
+    } catch (Throwable exc) {
+      exc.printStackTrace();
+      error(exc);
     }
-          
-    public void run() {
-	try {
-	    System.out.println("server start");
-	    startAgentServer((short)0);
-	   
-	    
-	    admin();
-	    System.out.println("admin config ok");
-	    
-	    Context  ictx = new InitialContext();
-	    Topic topic = (Topic) ictx.lookup("topic");
-	   
-	    ConnectionFactory cf = (ConnectionFactory) ictx.lookup("cf");
-	    ictx.close();
-
-	    Connection cnx = cf.createConnection();
- 
-	    Session sessionp = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    Session sessionc = cnx.createSession(false,
-						Session.AUTO_ACKNOWLEDGE);
-	    cnx.setExceptionListener(this);
-	    cnx.start();
-	  
-	    // create a producer and a consumer
-	    MessageProducer producer = sessionp.createProducer(topic);
-	    MessageConsumer consumer = sessionc.createConsumer(topic);
-	    // create a message send to the topic by the pruducer 
-	    Message msg = sessionp.createMessage();
-	   
-	    producer.send(msg);
-	    
-	    // receive the message
-	    Message msg1= consumer.receive(3000);
-	   
-	    // We do not close the connection, so we have an exception
-	    // cnx.close();
-	   	    
-	} catch (Throwable exc) {
-	    exc.printStackTrace();
-	    error(exc);
-	}
-	finally {
-	    System.out.println("Server stop ");
-	    stopAgentServer((short)0);
-	    endTest(); 
-	}
+    finally {
+      System.out.println("Server stop ");
+      stopAgentServer((short)0);
+      endTest(); 
     }
+  }
 
-    /**
-     * Admin : Create topic and a user anonymous
-     *   use jndi
-     */
-    public void admin() throws Exception {
-	// conexion 
-	org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
-								 "root", "root", 60);
-	// create a Topic   
-	org.objectweb.joram.client.jms.Topic topic =
-	    (org.objectweb.joram.client.jms.Topic) org.objectweb.joram.client.jms.Topic.create(0); 
-        // create a user
-	org.objectweb.joram.client.jms.admin.User user =
-	    org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
-	// set permissions
-	topic.setFreeReading();
-	topic.setFreeWriting();
+  /**
+   * Admin : Create topic and a user anonymous
+   *   use jndi
+   */
+  public void admin() throws Exception {
+    // conexion 
+    org.objectweb.joram.client.jms.admin.AdminModule.connect("localhost", 2560,
+        "root", "root", 60);
+    // create a Topic   
+    org.objectweb.joram.client.jms.Topic topic =
+        (org.objectweb.joram.client.jms.Topic) org.objectweb.joram.client.jms.Topic.create(0); 
+    // create a user
+    org.objectweb.joram.client.jms.admin.User user =
+        org.objectweb.joram.client.jms.admin.User.create("anonymous", "anonymous");
+    // set permissions
+    topic.setFreeReading();
+    topic.setFreeWriting();
 
-      	javax.jms.ConnectionFactory cf =
-	    org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
+    javax.jms.ConnectionFactory cf =
+        org.objectweb.joram.client.jms.tcp.TcpConnectionFactory.create("localhost", 2560);
 
-	javax.naming.Context jndiCtx = new javax.naming.InitialContext();
-	jndiCtx.bind("cf", cf);
-	jndiCtx.bind("topic", topic);
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.bind("cf", cf);
+    jndiCtx.bind("topic", topic);
 
-	jndiCtx.close();
-	   
-	org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
-    }
+    jndiCtx.close();
+
+    org.objectweb.joram.client.jms.admin.AdminModule.disconnect();
+  }
 
 
-    public void onException(javax.jms.JMSException jexecp) {
-	assertEquals("javax.jms.IllegalStateException",jexecp.toString()); 
-
-    }
+  public void onException(javax.jms.JMSException jexecp) {
+    assertTrue("unexpected exception: " + jexecp, jexecp instanceof javax.jms.IllegalStateException);
+  }
 }
 
