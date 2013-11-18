7ef8a6296582824c95302fbccc413094c9e14f3b
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-11-18 15:03:03 +0000

    Adds sleep to wait the server starting.

--- tests/src/joram/base/Test_T_MText_Delay.java
+++ tests/src/joram/base/Test_T_MText_Delay.java
@@ -59,7 +59,7 @@ public class Test_T_MText_Delay extends TestCase implements javax.jms.MessageLis
     try {
       System.out.println("server start");
       startAgentServer((short)0);
-
+      Thread.sleep(2000L);
       admin();
       System.out.println("admin config ok");
 
