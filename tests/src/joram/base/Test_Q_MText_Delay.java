b38bf8c266124d622882644fa23cf8b5fcb34ee6
feliot <feliot@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2013-12-19 12:12:36 +0000

    JORAM-176: update assertion.

--- tests/src/joram/base/Test_Q_MText_Delay.java
+++ tests/src/joram/base/Test_Q_MText_Delay.java
@@ -90,7 +90,7 @@ public class Test_Q_MText_Delay extends TestCase {
 
       msg = (TextMessage) consumer.receive();
       //System.out.println("msg = " + msg);
-      assertTrue("bad delivery delay", (System.currentTimeMillis() - current) > deliveryDelay);
+      assertTrue("bad delivery delay", (System.currentTimeMillis() - current) >= deliveryDelay);
 
       //test messages
       assertEquals(msg.getJMSMessageID(), msg.getJMSMessageID());
