61626528b39eebfd7e72e7f17da8ac84d0491ba9
afreyssin <afreyssin@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2014-01-06 16:36:14 +0000

    Adds LGPL header.

--- joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/ElasticTopic.java
+++ joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/ElasticTopic.java
@@ -1,3 +1,26 @@
+/*
+ *  JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2013 - 2014 ScalAgent Distributed Technologies
+ * Copyright (C) 2013 - 2014 Université Joseph Fourier
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): Université Joseph Fourier
+ * Contributor(s): ScalAgent Distributed Technologies
+ */
 package org.objectweb.joram.mom.dest;
 
 import java.io.Serializable;
@@ -11,7 +34,6 @@ import org.objectweb.joram.mom.notifications.FwdAdminRequestNot;
 import org.objectweb.joram.mom.notifications.GetClientSubscriptions;
 import org.objectweb.joram.mom.notifications.ReconnectSubscribersNot;
 import org.objectweb.joram.mom.notifications.TopicForwardNot;
-import org.objectweb.joram.mom.notifications.TopicMsgsReply;
 import org.objectweb.joram.shared.admin.AdminReply;
 import org.objectweb.joram.shared.admin.AdminRequest;
 import org.objectweb.joram.shared.admin.GetNumberReply;
