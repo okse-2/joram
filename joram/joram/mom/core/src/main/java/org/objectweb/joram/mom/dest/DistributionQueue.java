b7fb580a8c496f30450fc02b047319c04d0de711
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2015-05-28 08:48:25 +0000

    Tag Joram 5.10.0

--- joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/DistributionQueue.java
+++ joram/joram/mom/core/src/main/java/org/objectweb/joram/mom/dest/DistributionQueue.java
@@ -246,7 +246,6 @@ public class DistributionQueue extends Queue {
       for (Iterator ite = msgs.iterator(); ite.hasNext();) {
         // Bug fix JORAM-232: now counts received messages.
         nbMsgsReceiveSinceCreation++;
-        
         Message msg = (Message) ite.next();
         try {
           distributionModule.processMessage(msg);
@@ -330,9 +329,6 @@ public class DistributionQueue extends Queue {
       List msgs = cm.getMessages();
 
       for (Iterator ite = msgs.iterator(); ite.hasNext();) {
-        // Bug fix JORAM-232: now counts received messages.
-        nbMsgsReceiveSinceCreation++;
-
         Message msg = (Message) ite.next();
 
         // a processMessage exception is normal with async mode.
