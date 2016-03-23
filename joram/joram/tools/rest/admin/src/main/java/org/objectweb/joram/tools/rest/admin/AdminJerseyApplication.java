81faa4f5e0e2ac191a71487ef889e398516168c8
tachker <tachker@49cbe9c5-cf2d-4a1c-8f48-c1b532d97402>
2016-03-23 14:53:28 +0000

    JORAM-245: Add a REST API for the Joram admin.

---
+++ joram/joram/tools/rest/admin/src/main/java/org/objectweb/joram/tools/rest/admin/AdminJerseyApplication.java
@@ -0,0 +1,39 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2016 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
+package org.objectweb.joram.tools.rest.admin;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class AdminJerseyApplication extends Application {
+
+  static private Set<Class<?>> result = new HashSet<Class<?>>();
+  
+    @Override
+    public Set<Class<?>> getClasses() {
+        result.add(AdminService.class);
+        return result;
+    }
+}
